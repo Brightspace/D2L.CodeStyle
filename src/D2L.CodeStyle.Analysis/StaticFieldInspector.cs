f78ccd5c8f927a1a85758ea404b7f64ff52eecc8
Jacob Parker <Jacob.Parker@D2L.com>
2016-08-19 11:36:30 -0400

    Add a utility to check field declarations for multi-tenant safety

---
+++ src/D2L.CodeStyle.Analysis/StaticFieldInspector.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace D2L.CodeStyle.Analysis {
+	internal sealed class StaticFieldInspector {
+		/// <summary>
+		/// Validate that a field is safe for multi-tenant processes
+		/// </summary>
+		/// <param name="model">Semantic model</param>
+		/// <param name="field">The field to inspect</param>
+		/// <param name="concern">A diagnostic message</param>
+		/// <returns>true if the field is safe</returns>
+		public static bool IsMultiTenantSafe( SemanticModel model, FieldDeclarationSyntax field, out string concern ) {
+			if ( !field.Modifiers.Any( SyntaxKind.StaticKeyword) ) {
+				concern = null;
+				return true;
+			}
+
+			var typeInfo = model.GetTypeInfo( field.Declaration.Type );
+
+			if (typeInfo.Type.IsValueType ) {
+				concern = null;
+				return true;
+			}
+
+			concern = "Static reference-type fields are scary";
+			return false;
+		}
+	}
+}
