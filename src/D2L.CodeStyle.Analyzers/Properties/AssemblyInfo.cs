663b0b2211b73e64bfd74d9785f02d5391e7d862
Saquib Mian <Saquib.Mian@D2L.com>
2016-09-06 17:17:44 -0400

    Bumping version to 0.2.0.0

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.1.0.0" )]
-[assembly: AssemblyFileVersion( "0.1.0.0" )]
+[assembly: AssemblyVersion( "0.2.0.0" )]
+[assembly: AssemblyFileVersion( "0.2.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
--- src/D2L.CodeStyle.Annotations/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Annotations/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("35fe7851-3f76-4057-9754-e883231619d0")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
