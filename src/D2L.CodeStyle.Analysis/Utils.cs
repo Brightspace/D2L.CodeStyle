bff834117c9451d0af8551222c2142192fe20c3b
Saquib Mian <Saquib.Mian@D2L.com>
2016-10-27 17:04:50 -0400

    Ignore resource (generated) csharp files.

--- src/D2L.CodeStyle.Analysis/Utils.cs
+++ src/D2L.CodeStyle.Analysis/Utils.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 
 namespace D2L.CodeStyle.Analysis {
 
-    public sealed class Utils {
+	public sealed class Utils {
 
-        private static readonly string GeneratedFolderPathSegment = Path.DirectorySeparatorChar + ".generated" + Path.DirectorySeparatorChar;
+		private static readonly string GeneratedFolderPathSegment = Path.DirectorySeparatorChar + ".generated" + Path.DirectorySeparatorChar;
+		private static readonly string ResourceFileSuffix = ".Designer.cs";
 
-        public bool IsGeneratedCodefile( string path ) {
-            path = Path.GetFullPath( path );
+		public bool IsGeneratedCodefile( string path ) {
+			path = Path.GetFullPath( path );
 
-            if( Path.GetExtension( path ) == ".cs") {
-                if( path.Contains( GeneratedFolderPathSegment ) ) {
-                    return true;
-                }
-            }
+			if( Path.GetExtension( path ) == ".cs") {
+				if( path.Contains( GeneratedFolderPathSegment ) ) {
+					return true;
+				}
+				if( path.EndsWith( ResourceFileSuffix ) ) {
+					return true;
+				}
+			}
 
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 }
