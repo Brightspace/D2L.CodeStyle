048087f4d8d7ec01417eea334a725f5268494551
sawang <sara.wang@d2l.com>
2017-02-14 16:26:16 -0500

    Add Analyzer for named preperty 'Throws' of TestCaseData

---
+++ src/D2L.CodeStyle.TestAnalyzers/TestCaseData/TestCaseDataAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.TestAnalyzers.TestCaseData {
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	internal sealed class TestCaseDataAnalyzer : DiagnosticAnalyzer {
+		public const string DiagnosticId = "D2L0010";
+		private const string Category = "Safety";
+
+		private const string Title = "Ensure test does not contain named property 'Throws' in TestCaseData.";
+		private const string Description = "Named property 'Throws' should not be used in TestCaseData.";
+		internal const string MessageFormat = "Use Assert.Throws or Assert.That in your test case instead if 'Throws' in TestCaseData for NUnit 3 compatibility";
+
+		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+			DiagnosticId,
+			Title,
+			MessageFormat,
+			Category,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( Rule );
+
+		public override void Initialize( AnalysisContext context ) {
+			context.RegisterSyntaxNodeAction(
+				AnalyzeSyntaxNode,
+				SyntaxKind.InvocationExpression
+			);
+		}
+
+		private void AnalyzeSyntaxNode( SyntaxNodeAnalysisContext context ) {
+			var root = context.Node as InvocationExpressionSyntax;
+			if( root == null ) {
+				return;
+			}
+
+			if( root.Expression is MemberAccessExpressionSyntax ) {
+				var memberAccessExpression = root.Expression as MemberAccessExpressionSyntax;
+				var objectCreationExpressions = memberAccessExpression.DescendantNodes().OfType<ObjectCreationExpressionSyntax>().ToImmutableArray();
+				if( objectCreationExpressions.Length == 0 ) {
+					return;
+				}
+
+				if( objectCreationExpressions[0].Type.ToString().Equals( "TestCaseData" ) && memberAccessExpression.Name.ToString().Equals( "Throws" ) ) {
+					var diagnostic = Diagnostic.Create( Rule, memberAccessExpression.Name.GetLocation() );
+					context.ReportDiagnostic( diagnostic );
+				}
+			}
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.TestAnalyzers.Tests/TestCaseData/TestCaseDataAnalyzerTests.cs
@@ -0,0 +1,121 @@
+﻿using D2L.CodeStyle.TestAnalyzers.Test.Verifiers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.TestAnalyzers.TestCaseData {
+
+	internal sealed class TestCaseDataAnalyzerTests : DiagnosticVerifier {
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+			return new TestCaseDataAnalyzer();
+		}
+
+		[Test]
+		public void EmptyDocument_NoDiag() {
+			const string test = @"";
+
+			VerifyCSharpDiagnostic( test );
+		}
+
+		[Test]
+		public void DocumentWithoutThrows_NoDiag() {
+			const string test = @"
+	using System;
+
+	namespace test {
+		class Test {
+
+			[Test]
+			public void TestWithoutThrows() {
+			}
+
+		}
+	}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void DocumentWithThrowsCase1_Diag() {
+			const string test = @"
+	using System;
+
+	namespace test {
+		class Test {
+			private TestCaseData[] UserDetailsTestSource = {
+				new TestCaseData(0,0 ).SetCategory('').SetName( 'NoExistingResult_500Ids_Success' ).Throws( typeof( Exception ) ),
+				new TestCaseData( 0, 0 ).Throws( 'Exception' ),
+				new TestCaseData( 0, 0 )
+			};
+		}
+	}";
+			var diag1 = CreateDiagnosticResult( 7, 89 );
+			var diag2 = CreateDiagnosticResult( 8, 30 );
+			VerifyCSharpDiagnostic( test, diag1, diag2 );
+		}
+
+		[Test]
+		public void DocumentWithThrowsCase2_Diag() {
+			const string test = @"
+	using System;
+
+	namespace test {
+		class Test {
+			static IEnumerable GetTranslateTestCases() {
+				yield return new TestCaseData( 0, 0 ).SetName( 'NoExistingResult_EmptyIdList_ExpectInvalidRequestDataException' ).Throws( typeof( Exception ) );
+				yield return new TestCaseData( 0, 500 ).SetCategory('').Throws( 'Exception' );
+				yield return new TestCaseData( 0, 0 );
+			}
+		}
+	}";
+			var diag1 = CreateDiagnosticResult( 7, 119 );
+			var diag2 = CreateDiagnosticResult( 8, 61 );
+			VerifyCSharpDiagnostic( test, diag1, diag2 );
+		}
+
+		[Test]
+		public void DocumentWithThrowsCase3_Diag() {
+			const string test = @"
+	using System;
+
+	namespace test {
+		class Test {
+			private static TestCaseData[] CheckPassword_TestCases
+			{
+				get
+				{
+					return new TestCaseData[] {
+						new TestCaseData(0,0 ).Throws( typeof( Exception ) ),
+						new TestCaseData(0,0 ).Throws( 'Exception' ),
+						new TestCaseData(0,0 )
+					};
+				}
+			}
+		}
+	}";
+			var diag1 = CreateDiagnosticResult( 11, 30 );
+			var diag2 = CreateDiagnosticResult( 12, 30 );
+			VerifyCSharpDiagnostic( test, diag1, diag2 );
+		}
+
+		private void AssertNoDiagnostic( string file ) {
+			VerifyCSharpDiagnostic( file );
+		}
+
+		private void AssertSingleDiagnostic( string file, int line, int column ) {
+
+			DiagnosticResult result = CreateDiagnosticResult( line, column );
+			VerifyCSharpDiagnostic( file, result );
+		}
+
+		private static DiagnosticResult CreateDiagnosticResult( int line, int column ) {
+			return new DiagnosticResult {
+				Id = TestCaseDataAnalyzer.DiagnosticId,
+				Message = TestCaseDataAnalyzer.MessageFormat,
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] {
+					new DiagnosticResultLocation( "Test0.cs", line, column )
+				}
+			};
+		}
+	}
+}
