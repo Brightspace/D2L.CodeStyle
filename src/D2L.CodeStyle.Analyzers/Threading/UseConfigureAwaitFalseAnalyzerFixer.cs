77c13b256ce0d8bcf8f37b752871e49808d4edbc
David Rose <David.Rose@d2l.com>
2017-02-23 15:13:06 -0500

    Added Unit Tests for Await Fixer

--- src/D2L.CodeStyle.Analyzers/Threading/UseConfigureAwaitFalseAnalyzerFixer.cs
+++ src/D2L.CodeStyle.Analyzers/Threading/UseConfigureAwaitFalseAnalyzerFixer.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace D2L.CodeStyle.Analyzers.Threading {
 
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Threading/UnsafeAwaitFixerTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D2L.CodeStyle.Analyzers.Verifiers;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers.Threading {
+
+	internal sealed class UnsafeAwaitFixerTests : CodeFixVerifier {
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+			return new UseConfigureAwaitFalseAnalyzer();
+		}
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() {
+			return new UseConfigureAwaitFalseAnalyzerFixer();
+		}
+
+		[Test]
+		public void FixAwait_CodeHasMultipleAwaitIssues_ReturnsCodeWithConfigureAwaitFalse() {
+			const string oldCode = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+public static class TaskExtensions {
+    public static ConfiguredTaskAwaitable SafeAsync( this Task @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+
+    public static ConfiguredTaskAwaitable<T> SafeAsync<T>( this Task<T> @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+}
+
+public class Test {
+    Task<int> DoSomethingAsync() {   
+        return Task.FromResult(5);
+    }
+
+    async Task NonGenericDoSomethingAsync() {   
+        await Task.FromResult(5);
+    }
+
+    async Task<string> DoSomethignElseAsync() {
+        int i = await DoSomethingAsync().SafeAsync();
+        await NonGenericDoSomethingAsync();
+        await Task.Factory.StartNew(
+                () => {
+                    for( int i = 0; i < 1000000; i++ ) {
+                        Console.WriteLine(i);
+                    }
+                } );
+        return $""""hello {i}th person"""";
+    }
+}";
+
+			const string newCode = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+public static class TaskExtensions {
+    public static ConfiguredTaskAwaitable SafeAsync( this Task @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+
+    public static ConfiguredTaskAwaitable<T> SafeAsync<T>( this Task<T> @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+}
+
+public class Test {
+    Task<int> DoSomethingAsync() {   
+        return Task.FromResult(5);
+    }
+
+    async Task NonGenericDoSomethingAsync() {
+        await Task.FromResult(5).ConfigureAwait(false);
+    }
+
+    async Task<string> DoSomethignElseAsync() {
+        int i = await DoSomethingAsync().SafeAsync();
+        await NonGenericDoSomethingAsync().ConfigureAwait(false);
+        await Task.Factory.StartNew(
+                () => {
+                    for( int i = 0; i < 1000000; i++ ) {
+                        Console.WriteLine(i);
+                    }
+                } ).ConfigureAwait(false);
+        return $""""hello {i}th person"""";
+    }
+}";
+
+			VerifyFix( oldCode, newCode);
+
+		}
+
+	}
+}
