c685acd6084a0f61de9fcd99af63e8bc65968138
David Rose <David.Rose@d2l.com>
2017-02-23 14:12:03 -0500

    Added unit tests for Await Analyzer. Made analyzer work with SafeAsync extension methods in core

--- src/D2L.CodeStyle.Analyzers/Threading/UseConfigureAwaitFalseAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/Threading/UseConfigureAwaitFalseAnalyzer.cs
@@ -1,141 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Composition;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace D2L.CodeStyle.Analyzers.Threading {
 
-	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseConfigureAwaitFalseAnalyzerFixer)), Shared]
-	public class UseConfigureAwaitFalseAnalyzerFixer : CodeFixProvider {
-		private static readonly string s_title = "Awaitable's should specify a ConfigureAwait";
-		public override async Task RegisterCodeFixesAsync(CodeFixContext context) {
-
-			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
-			var diagnostic = context.Diagnostics.First();
-			var diagnosticSpan = diagnostic.Location.SourceSpan;
-
-			var awaitExpression =
-				root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<AwaitExpressionSyntax>().First();
-
-			context.RegisterCodeFix(
-				CodeAction.Create(s_title, c => AddConfigureAwait(context.Document, awaitExpression, c), s_title),
-				diagnostic);
-
-		}
-
-		private async Task<Document> AddConfigureAwait(
-			Document document,
-			AwaitExpressionSyntax awaitExpression,
-			CancellationToken ct
-		) {
-
-			var configureAwaitExpr = SyntaxFactory.MemberAccessExpression(
-				SyntaxKind.SimpleMemberAccessExpression,
-				awaitExpression.Expression,
-				SyntaxFactory.IdentifierName("ConfigureAwait"));
-
-			var invocExpression =
-				SyntaxFactory.InvocationExpression(configureAwaitExpr)
-							 .WithArgumentList(
-								 SyntaxFactory.ArgumentList(
-									 SyntaxFactory.SingletonSeparatedList<ArgumentSyntax>(
-										 SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)))));
-
-			var newAwait = SyntaxFactory.AwaitExpression(invocExpression);
-
-			var root = await document.GetSyntaxRootAsync(ct);
-			var newRoot = root.ReplaceNode(awaitExpression, newAwait);
-
-			var newDoc = document.WithSyntaxRoot(newRoot);
-
-			return newDoc;
-
-
-		}
-
-		public override FixAllProvider GetFixAllProvider() {
-			return WellKnownFixAllProviders.BatchFixer;
-		}
-
-		public override ImmutableArray<string> FixableDiagnosticIds
-		{
-			get { return ImmutableArray.Create<string>(UseConfigureAwaitFalseAnalyzer.DIAGNOSTIC_ID); }
-		}
-
-	}
-
-	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
 	public class UseConfigureAwaitFalseAnalyzer : DiagnosticAnalyzer {
 
-		public const string DIAGNOSTIC_ID = "D2LAwaitFalse";
-
-		// You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-		// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-		private static readonly LocalizableString s_title = "Awaitable's should specify a ConfigureAwait";
-		private static readonly LocalizableString s_description = "Awaitable's should use ConfigureAwait(false)";
+		public const string DiagnosticId = "D2L0004";
 		private const string CATEGORY = "Naming";
 
-		private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(DIAGNOSTIC_ID, s_title, s_description, CATEGORY, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: s_description);
+		private static readonly LocalizableString s_title = "Awaitable should specify a ConfigureAwait";
+		private static readonly LocalizableString s_description = "Awaitable should use ConfigureAwait(false) if possible";
 
+		private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+			DiagnosticId,
+			s_title,
+			s_description,
+			CATEGORY,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: s_description );
 
-		public override void Initialize(AnalysisContext context) {
-			context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.AwaitExpression);
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics {
+			get { return ImmutableArray.Create<DiagnosticDescriptor>( s_rule ); }
 		}
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-		{
-			get { return ImmutableArray.Create<DiagnosticDescriptor>(s_rule); }
+
+		public override void Initialize( AnalysisContext context ) {
+			context.RegisterSyntaxNodeAction( AnalyzeSymbol, SyntaxKind.AwaitExpression );
 		}
 
-		private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context) {
+		private static void AnalyzeSymbol( SyntaxNodeAnalysisContext context ) {
+			INamedTypeSymbol nonGenericAwaitable =
+				context.Compilation.GetTypeByMetadataName( "System.Runtime.CompilerServices.ConfiguredTaskAwaitable" );
+			INamedTypeSymbol genericAwaitable =
+				context.Compilation.GetTypeByMetadataName( "System.Runtime.CompilerServices.ConfiguredTaskAwaitable`1" );
 
 			bool isConfigured = context.Node.DescendantNodes()
-					.OfType<MemberAccessExpressionSyntax>()
-					.Any(x => IsConfigureAwaitFunction(x, context.SemanticModel));
+			                           .OfType<MemberAccessExpressionSyntax>()
+			                           .Any(
+				                           x => IsConfigureAwaitFunction(
+					                           x,
+					                           context.SemanticModel,
+					                           genericAwaitable,
+					                           nonGenericAwaitable ) );
 
-			if (!isConfigured) {
+			if( !isConfigured ) {
 
-				var diagnostic = Diagnostic.Create(s_rule, context.Node.GetLocation());
+				var diagnostic = Diagnostic.Create( s_rule, context.Node.GetLocation() );
 
-				context.ReportDiagnostic(diagnostic);
+				context.ReportDiagnostic( diagnostic );
 			}
 		}
 
-		private static bool IsConfigureAwaitFunction(MemberAccessExpressionSyntax node, SemanticModel model) {
-			if (!node.IsKind(SyntaxKind.SimpleMemberAccessExpression)) {
+		private static bool IsConfigureAwaitFunction(
+			MemberAccessExpressionSyntax node,
+			SemanticModel model,
+			params INamedTypeSymbol[] awaitableSymbols ) {
+			if( !node.IsKind( SyntaxKind.SimpleMemberAccessExpression ) ) {
 				return false;
 			}
 
-			var symbol = model.GetSymbolInfo(node).Symbol;
+			ISymbol symbol = model.GetSymbolInfo( node ).Symbol;
 
-			if (symbol == null) {
+			if( symbol == null ) {
 				return false;
 			}
 
-			if (!String.Equals(symbol.ContainingAssembly.Identity.Name, "mscorlib", StringComparison.OrdinalIgnoreCase)) {
+			IMethodSymbol methodSymbol = symbol as IMethodSymbol;
+
+			if( methodSymbol == null || methodSymbol.ReturnsVoid ) {
 				return false;
 			}
 
-			if (!String.Equals(symbol.ContainingNamespace.ToString(), "System.Threading.Tasks", StringComparison.OrdinalIgnoreCase)) {
+			var namedType = methodSymbol.ReturnType as INamedTypeSymbol;
+
+			if( namedType == null ) {
 				return false;
 			}
 
-			if (symbol.Name.Equals("ConfigureAwait", StringComparison.OrdinalIgnoreCase)) {
-				return true;
+			foreach( INamedTypeSymbol ns in awaitableSymbols ) {
+				if( namedType.OriginalDefinition.Equals( ns ) ) {
+					return true;
+				}
 			}
 
 			return false;
 		}
+
 	}
+
 }
--- tests/D2L.CodeStyle.Analyzers.Test/Threading/UnsafeAwaitTests.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Threading/UnsafeAwaitTests.cs
@@ -17,6 +17,54 @@ namespace D2L.CodeStyle.Analyzers.Threading {
 		}
 
 		[Test]
+		public void VerifyAnalyzer_HasMultipleUnsafeAwaits_MultipleDiagnostics() {
+			const string code = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+public static class TaskExtensions {
+    public static ConfiguredTaskAwaitable SafeAsync( this Task @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+
+    public static ConfiguredTaskAwaitable<T> SafeAsync<T>( this Task<T> @this ) {
+        return @this.ConfigureAwait( continueOnCapturedContext: false );
+    }
+}
+
+public class Test {
+    Task<int> DoSomethingAsync() {   
+        return Task.FromResult(5);
+    }
+
+    async Task NonGenericDoSomethingAsync() {   
+        await Task.FromResult(5);
+    }
+
+    async Task<string> DoSomethignElseAsync() {
+        int i = await DoSomethingAsync().SafeAsync();
+        await NonGenericDoSomethingAsync();
+
+        await Task.Factory.StartNew(
+                () => {
+                    for( int i = 0; i < 1000000; i++ ) {
+                        Console.WriteLine(i);
+                    }
+                } );
+
+
+
+        return $""""hello {i}th person"""";
+    }
+}";
+			var diag1 = CreateDiagnosticResult(22, 9);
+			var diag2 = CreateDiagnosticResult(27, 9);
+			var diag3 = CreateDiagnosticResult(29, 9);
+			VerifyCSharpDiagnostic(code, diag1, diag2, diag3);
+		}
+
+		[Test]
 		public void VerifyAnalyzer_HasUnsafeAwait_DiagnosticsShowUp() {
 
 			const string code = @"using System;
@@ -30,27 +78,120 @@ public class Test {
 
 	async Task<string> DoSomethignElseAsync() {
 		int i = await DoSomethingAsync();
+		int j = await DoSomethingAsync().ConfigureAwait(false);
 		return $""hello {i}th person"";
 	}
 }";
 
-			AssertSingleDiagnostic(code, 11, 10, "bad", "it");
+			AssertSingleDiagnostic( code, 11, 11 );
+
+		}
+
+		[Test]
+		public void VerifyAnalyzer_HasSafeAwait_DiagnosticsShowDoNotShowUp() {
+
+			const string code = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Test {
+	Task<int> DoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	Task NonGenericDoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	async Task<string> DoSomethignElseAsync() {
+		int i = await DoSomethingAsync().ConfigureAwait(false);
+		await NonGenericDoSomethingAsync().ConfigureAwait(false);
+		return $""hello {i}th person"";
+	}
+}";
+
+			VerifyCSharpDiagnostic( code );
+
+		}
+
+		[Test]
+		public void VerifyAnalyzer_HasSafeAwaitWithTrue_DiagnosticsShowDoNotShowUp() {
+
+			const string code = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Test {
+	Task<int> DoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	Task NonGenericDoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	async Task<string> DoSomethignElseAsync() {
+		int i = await DoSomethingAsync().ConfigureAwait(true);
+		await NonGenericDoSomethingAsync().ConfigureAwait(true);
+		return $""hello {i}th person"";
+	}
+}";
+
+			VerifyCSharpDiagnostic(code);
+
+		}
+
+		[Test]
+		public void VerifyAnalyzer_HasSafeAwaitWithTaskExtensions_DiagnosticsShowDoNotShowUp() {
+
+			const string code = @"using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+public static class TaskExtensions {
+	public static ConfiguredTaskAwaitable SafeAsync( this Task @this ) {
+		return @this.ConfigureAwait( continueOnCapturedContext: false );
+	}
+
+	public static ConfiguredTaskAwaitable<T> SafeAsync<T>( this Task<T> @this ) {
+		return @this.ConfigureAwait( continueOnCapturedContext: false );
+	}
+}
+
+public class Test {
+	Task<int> DoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	Task NonGenericDoSomethingAsync() {   
+		return Task.FromResult(5);
+	}
+
+	async Task<string> DoSomethignElseAsync() {
+		int i = await DoSomethingAsync().SafeAsync();
+		await NonGenericDoSomethingAsync().SafeAsync();
+		return $""hello {i}th person"";
+	}
+}";
+
+			VerifyCSharpDiagnostic(code);
 
 		}
 
 
-		private void AssertSingleDiagnostic(string file, int line, int column, string fieldOrProp, string badFieldOrType) {
+		private void AssertSingleDiagnostic(string file, int line, int column) {
 
-			DiagnosticResult result = CreateDiagnosticResult(line, column, fieldOrProp, badFieldOrType);
+			DiagnosticResult result = CreateDiagnosticResult(line, column);
 			VerifyCSharpDiagnostic(file, result);
 		}
 
-		private static DiagnosticResult CreateDiagnosticResult(int line, int column, string fieldOrProp, string badFieldOrType) {
+		private static DiagnosticResult CreateDiagnosticResult(int line, int column) {
 			return new DiagnosticResult
 			{
-				Id = UnsafeStaticsAnalyzer.DiagnosticId,
-				Message = string.Format(UnsafeStaticsAnalyzer.MessageFormat, fieldOrProp, badFieldOrType),
-				Severity = DiagnosticSeverity.Error,
+				Id = UseConfigureAwaitFalseAnalyzer.DiagnosticId,
+				Message = "Awaitable should use ConfigureAwait(false) if possible",
+				Severity = DiagnosticSeverity.Warning,
 				Locations = new[] {
 					new DiagnosticResultLocation( "Test0.cs", line, column )
 				}
