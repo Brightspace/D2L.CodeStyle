5782b8fef37a4639463df33b3e74c9d2fcfeef5d
Saquib Mian <Saquib.Mian@D2L.com>
2017-03-20 12:06:46 -0400

    Refactor aggregations, and make one for unsafe statics per cause.

--- src/D2L.CodeStyle.UnsafeStaticCounter/AnalyzedResults.cs
+++ src/D2L.CodeStyle.UnsafeStaticCounter/AnalyzedResults.cs
@@ -5,8 +5,9 @@ namespace D2L.CodeStyle.UnsafeStaticCounter {
 
 	internal sealed class AnalyzedResults {
 		public readonly int UnsafeStaticsCount;
-		public readonly IEnumerable<AnalyzedType> UnsafeStaticsPerType;
-		public readonly IEnumerable<AnalyzedProject> UnsafeStaticsPerProject;
+		public readonly IEnumerable<Aggregation> UnsafeStaticsPerCause;
+		public readonly IEnumerable<Aggregation> UnsafeStaticsPerType;
+		public readonly IEnumerable<Aggregation> UnsafeStaticsPerProject;
 		public readonly IEnumerable<AnalyzedStatic> RawResults;
 
 		public AnalyzedResults( AnalyzedStatic[] rawResults ) {
@@ -14,46 +15,47 @@ namespace D2L.CodeStyle.UnsafeStaticCounter {
 
 			UnsafeStaticsCount = rawResults.Length;
 
-			var unsafeStaticsPerProject = new Dictionary<string, AnalyzedProject>();
-			var unsafeStaticsPerType = new Dictionary<string, AnalyzedType>();
+			var unsafeStaticsPerCause = new Dictionary<string, Aggregation>();
+			var unsafeStaticsPerProject = new Dictionary<string, Aggregation>();
+			var unsafeStaticsPerType = new Dictionary<string, Aggregation>();
 
 			foreach( var result in rawResults ) {
 
 				// increment per project
 				var project = unsafeStaticsPerProject.GetOrAdd(
 						result.ProjectName,
-						() => new AnalyzedProject( result.ProjectName )
+						() => new Aggregation( result.ProjectName )
 					);
 				project.UnsafeStaticsCount++;
 
 				// increment per type
 				var analyzedType = unsafeStaticsPerType.GetOrAdd(
 					result.FieldOrPropType,
-					() => new AnalyzedType( result.FieldOrPropType )
+					() => new Aggregation( result.FieldOrPropType )
 				);
 				analyzedType.UnsafeStaticsCount++;
 
+				// increment per cause
+				var analyzedCause = unsafeStaticsPerCause.GetOrAdd(
+					result.Cause,
+					() => new Aggregation( result.Cause )
+				);
+				analyzedCause.UnsafeStaticsCount++;
+
 			}
 
+			UnsafeStaticsPerCause = unsafeStaticsPerCause.Values;
 			UnsafeStaticsPerProject = unsafeStaticsPerProject.Values;
 			UnsafeStaticsPerType = unsafeStaticsPerType.Values;
 		}
 	}
 
-	internal sealed class AnalyzedProject {
+	internal sealed class Aggregation {
 		public readonly string Name;
 		public int UnsafeStaticsCount;
 
-		public AnalyzedProject( string projectName ) {
-			Name = projectName;
-		}
-	}
-	internal sealed class AnalyzedType {
-		public readonly string Name;
-		public int UnsafeStaticsCount;
-
-		public AnalyzedType( string typeName ) {
-			Name = typeName;
+		public Aggregation( string name ) {
+			Name = name;
 		}
 	}
 }
