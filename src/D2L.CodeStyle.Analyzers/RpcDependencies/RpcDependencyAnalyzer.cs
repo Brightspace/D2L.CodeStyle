fa32d918528806ee82a0c3b09837cb7a2f5f4f59
Jacob Parker <Jacob.Parker@D2L.com>
2017-04-11 16:12:12 -0400

    Add analyzer for RPCs

---
+++ src/D2L.CodeStyle.Analyzers/RpcDependencies/RpcDependencyAnalyzer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.RpcDependencies {
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	internal sealed class RpcDependencyAnalyzer : DiagnosticAnalyzer {
+		internal static readonly DiagnosticDescriptor RpcContextRule = new DiagnosticDescriptor(
+			id: "D2L0004",
+			title: "RPCs must take an IRpcContext as their first argument",
+			messageFormat: "RPCs must take an IRpcContext as their first argument",
+			category: "Correctness",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true,
+			description: "RPCs must take an IRpcContext as their first argument"
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( RpcContextRule );
+
+		public override void Initialize( AnalysisContext context ) {
+			context.RegisterSyntaxNodeAction( AnalyzeMethod, SyntaxKind.MethodDeclaration );
+		}
+
+		private static void AnalyzeMethod( SyntaxNodeAnalysisContext context ) {
+			var method = context.Node as MethodDeclarationSyntax;
+
+			if ( method == null ) {
+				return;
+			}
+
+			// Could this be cached per-compilation?
+			var rpcAttributeType = context.Compilation.GetTypeByMetadataName( "D2L.Web.RpcAttribute" );
+
+			bool isRpc = method
+				.AttributeLists
+				.SelectMany( al => al.Attributes )
+				.Any( attr => IsRpcAttribute( rpcAttributeType, attr, context.SemanticModel ) );
+
+			if( !isRpc ) {
+				return;
+			}
+
+			var ps = method.ParameterList.Parameters;
+
+			if ( ps.Count == 0 ) {
+				context.ReportDiagnostic( Diagnostic.Create( RpcContextRule, method.ParameterList.GetLocation() ) );
+			} else {
+				var firstParam = method.ParameterList.Parameters[0];
+
+				var rpcContextType = context.Compilation.GetTypeByMetadataName( "D2L.Web.IRpcContext" );
+
+				if ( !ParameterIsOfTypeIRpcContext( rpcContextType, firstParam, context.SemanticModel ) ) {
+					context.ReportDiagnostic( Diagnostic.Create( RpcContextRule, firstParam.GetLocation() ) );
+				}
+			}
+
+			// other things to check:
+			// - sort order of [Dependency] arguments
+			// - appropriate use of [Dependency]
+		}
+
+		private static bool IsRpcAttribute( INamedTypeSymbol expectedType, AttributeSyntax attr, SemanticModel model ) {
+			var symbol = model.GetSymbolInfo( attr ).Symbol;
+
+			if ( symbol == null || symbol.Kind == SymbolKind.ErrorType ) {
+				return false;
+			}
+
+			return symbol.ContainingType.Equals( expectedType );
+		}
+
+		private static bool ParameterIsOfTypeIRpcContext( INamedTypeSymbol expectedType, ParameterSyntax param, SemanticModel model ) {
+			var symbol = model.GetSymbolInfo( param.Type ).Symbol;
+
+			if ( symbol == null || symbol.Kind == SymbolKind.ErrorType ) {
+				return false;
+			}
+
+			return symbol.Equals( expectedType );
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/RpcDependencies/RpcDependencyAnalyzerTests.cs
@@ -0,0 +1,106 @@
+﻿using D2L.CodeStyle.Analyzers.Test.Verifiers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers.RpcDependencies {
+	[TestFixture]
+	internal sealed class RpcDependencyAnalyzerTests : DiagnosticVerifier {
+		private const string PREAMBLE = @"
+namespace D2L.Web {
+	interface IRpcContext {}
+	class RpcAttribute : System.Attribute {}
+}
+
+namespace D2L.LP.Extensibility.Activation.Domain {
+	class DependencyAttribute : System.Attribute {}
+}";
+
+		[Test]
+		public void NormalMethod_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	class Test {
+		public void Test( int x ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void MethodWithUnrelatedButAnnoyinglyNamedRpcAttribute_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	class RpcAttribute : System.Attribute {} // shadow the parent one
+	class Test {
+		[Rpc]
+		public void Test() {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		[Test]
+		public void MethodWithRpcAttributeButNoArguments_RpcContextDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+
+	class Test {
+		[Rpc]
+		public void Test() {}
+	}
+}";
+			AssertSingleDiagnostic( test, 15, 19 );
+		}
+
+		[Test]
+		public void MethodWithRpcAttributeAndIntFirstArgument_RpcContextDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+
+	class Test {
+		[Rpc]
+		public void Test( int x ) {}
+	}
+}";
+			AssertSingleDiagnostic( test, 15, 21 );
+		}
+
+		[Test]
+		public void MethodWithRpcAttributeAndIRpcContextFirstArg_NoDiag() {
+			const string test = PREAMBLE + @"
+namespace Test {
+	using D2L.Web;
+
+	class Test {
+		[Rpc]
+		public void Test( IRpcContext x ) {}
+	}
+}";
+			AssertNoDiagnostic( test );
+		}
+
+		private void AssertNoDiagnostic( string file ) {
+			VerifyCSharpDiagnostic( file );
+		}
+
+        private void AssertSingleDiagnostic( string file, int line, int column ) {
+			DiagnosticResult result = new DiagnosticResult {
+				Id = RpcDependencyAnalyzer.RpcContextRule.Id,
+				Message = RpcDependencyAnalyzer.RpcContextRule.MessageFormat.ToString(),
+				Severity =  DiagnosticSeverity.Error,
+				Locations = new [] {
+					new DiagnosticResultLocation( "Test0.cs", line, column )
+				}
+			};
+
+			VerifyCSharpDiagnostic( file, result );
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+			return new RpcDependencyAnalyzer();
+		}
+	}
+}
