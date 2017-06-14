04211f8ffc446749866a7cd26855edf18d7a287b
Jacob Parker <Jacob.Parker@D2L.com>
2017-06-15 00:42:14 +0100

    Add spec tests prototype
    This form of testing for the analyzers would be awesome I think.
    
    - We get VS syntax highlighting for the files
    - There is minimal boilerplate to add new test cases
    - The assertions are beside where the diagnostics should be emitted
    - We can delineate where the diagnostic appears so we can assert where
      the squiggle should go without manually having to type line and column
      numbers.
    - We could generate pretty HTML from this: remove the comments, syntax
      highlight, add red-squiggles/error messages.
    
    The biggest win though, in my opinion, is for TDD like development: an
    analyzer author can write a spec file full of evil code that should have
    diagnostics and then write the analyzer with the assistance of the
    VS syntax tree visualizer on the spec file.
    
    This is just a prototype and its got some sketchy code in there. It's
    only asserting that the number of diagnostics matches the number of
    non-empty comments.

---
+++ tests/D2L.CodeStyle.Analyzers.Test/SpecRunner.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers {
+	[TestFixture]
+	internal sealed class SpecRunner {
+		[Test, TestCaseSource(typeof(SpecRunner), nameof(TestCases))]
+		public async Task RunSpec( string testName, string source ) {
+			CompilationUnitSyntax root = (CompilationUnitSyntax)CSharpSyntaxTree.ParseText( source ).GetRoot();
+
+			string rawComment = root.GetLeadingTrivia()
+				.First(
+					t => t.Kind() == SyntaxKind.SingleLineCommentTrivia
+					|| t.Kind() == SyntaxKind.MultiLineCommentTrivia )
+				.ToString();
+
+			var settings =
+				Regex.Split( GetCommentContents( rawComment ), @"\r?\n" )
+				.Select( l => {
+					var parts = l.Split( new[] { ':' }, 2 );
+					return new KeyValuePair<string, string>( parts[0].Trim(), parts[1].Trim() );
+				} )
+				.ToImmutableDictionary( StringComparer.InvariantCultureIgnoreCase );
+
+			Assert.True( settings.ContainsKey( "analyzer" ) );
+
+			var type = Type.GetType( settings["analyzer"] + ", D2L.CodeStyle.Analyzers" );
+
+			Assert.NotNull( type );
+
+			var analyzer = (DiagnosticAnalyzer)Activator.CreateInstance( type );
+
+			Assert.NotNull( analyzer );
+
+			var projectName = "SpecTestsFor" + testName;
+			var filename = testName + ".cs";
+
+			var projectId = ProjectId.CreateNewId( debugName: projectName );
+			var documentId = DocumentId.CreateNewId( projectId, debugName: filename );
+
+			var solution = new AdhocWorkspace()
+				.CurrentSolution
+				.AddProject( projectId, projectName, projectName, LanguageNames.CSharp )
+				.AddMetadataReference( projectId, MetadataReference.CreateFromFile( typeof( object ).Assembly.Location ) ) // mscorlib
+				.AddMetadataReference( projectId, MetadataReference.CreateFromFile( typeof( Enumerable ).Assembly.Location ) ) // system.core
+				.AddDocument( documentId, filename, SourceText.From( source ) );
+
+			var compilation = ( await solution.Projects.First().GetCompilationAsync() )
+				.WithAnalyzers( ImmutableArray.Create( analyzer ));
+
+			var diags = (await compilation.GetAnalyzerDiagnosticsAsync()).ToImmutableArray();
+
+			var multilineComments = root.DescendantTrivia()
+				.Where( t => t.Kind() == SyntaxKind.MultiLineCommentTrivia )
+				.Select( c => GetCommentContents( c.ToString() ) )
+				.Where( c => c != "" ).ToImmutableArray();
+
+			Assert.AreEqual( multilineComments.Length, diags.Length );
+		}
+
+		private static string GetCommentContents( string rawComment ) {
+			// real ghetto
+			return Regex.Replace( rawComment.Trim(), @"^(//|/\*)(?<contents>[^*]*)(\*/)*$", "${contents}" ).Trim();
+		}
+
+		public static IEnumerable TestCases {
+			get {
+				var resourceSet = Specs.SpecTests.ResourceManager.GetResourceSet( CultureInfo.CurrentUICulture, true, true );
+				foreach ( DictionaryEntry resource in resourceSet ) {
+					var testName = resource.Key.ToString();
+					var source = resource.Value;
+
+					yield return new TestCaseData(
+						testName, source
+					).SetCategory( "Unit" ).SetName( testName );
+				}
+			}
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/RpcAnalyzer.cs
@@ -0,0 +1,120 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.RpcDependencies.RpcDependencyAnalyzer
+
+using System;
+using D2L.LP.Extensibility.Activation.Domain;
+using D2L.Web;
+using D2L.Web.RequestContext;
+
+namespace D2L.Web {
+	public interface IRpcContext { }
+	public interface IRpcPostContext { }
+	public class RpcAttribute : Attribute { }
+}
+
+namespace D2L.Web.RequestContext {
+	public interface IRpcPostContextBase { }
+}
+
+namespace D2L.LP.Extensibility.Activation.Domain {
+	public class DependencyAttribute : Attribute { }
+}
+
+namespace D2L.CodeStyle.Analyzers.RpcDependencies.Examples {
+	public sealed class FooDependency { }
+	public sealed class BarDependency { }
+	public sealed class OkayRpcHandler {
+		public void NonRpcMethod( int x ) { }
+
+		public static void NonRpcStaticMethod() { }
+
+		[UndefinedAttribute]
+		public static void WeShouldntEmitDiagnosticsWhenAnAttributeDoesntExist() { }
+
+		[Rpc]
+		public static void BasicIRpcContext( IRpcContext context ) { }
+
+		[Rpc]
+		public static void BasicIRpcPostContext( IRpcPostContext context ) { }
+
+		[Rpc]
+		public static void BasicIRpcPostContextBase( IRpcPostContextBase context ) { }
+
+		[Rpc]
+		public static void RpcWithSingleParameter( IRpcContext context, int x ) { }
+
+		[Rpc]
+		public static void RpcWithMultipleParameters( IRpcContext context, int x, int y ) { }
+		
+		[Rpc]
+		public static void RpcWithDependencyParameter(
+			IRpcContext context,
+			[Dependency] FooDependency x
+		) { }
+
+		[Rpc]
+		public static void RpcWithMultipleDependencyParameters(
+			IRpcContext context,
+			[Dependency] FooDependency x,
+			[Dependency] BarDependency y
+		) { }
+
+		[Rpc]
+		public static void GeneralRpc(
+			IRpcContext context,
+			[Dependency] FooDependency x,
+			[Dependency] BarDependency y,
+			int a,
+			string b
+		) { }
+
+		[Rpc]
+		public static void GeneralRpcWithWeirdParameterAttribute(
+			IRpcContext context,
+			[Dependency] FooDependency x,
+			[Dependency] BarDependency y,
+			int a,
+			[UndefinedAttribute] string b
+		) { }
+	}
+
+	public sealed class BadRpcs {
+		[Rpc]
+		/* RpcContextFirstArgument */ public static void MissingFirstArgument() /**/ { }
+
+		[Rpc]
+		public static void BadFirstArgument( /* RpcContextFirstArgument */ int x /**/ ) { }
+
+		[Rpc]
+		[UndefinedAttribute]
+		public static void BadFirstArgumentWithUnrelatedUndefinedMethodAttribute( /* RpcContextFirstArgument */ int x /**/ ) { }
+
+		[Rpc]
+		public static void BadFirstArgument2(
+			/* RpcContextFirstArgument */ int x /**/,
+			IRpcContext context
+		) { }
+
+		[Rpc]
+		public static void IncorrectDependencySortOrder(
+			IRpcContext context,
+			int x,
+			/* RpcArgumentSortOrder */ [Dependency] FooDependency foo /**/
+		) { }
+
+		[Rpc]
+		public static void IncorrectDependencySortOrderWithMultipleThings(
+			IRpcContext context,
+			int x,
+			int y,
+			/* RpcArgumentSortOrder */ [Dependency] FooDependency foo /**/,
+			/* RpcArgumentSortOrder */ [Dependency] BarDependency bar /**/
+		) { }
+
+		[Rpc]
+		public static void IncorrectDependencySortOrderEvenWithRandomBadAttributeInTheMiddle(
+			IRpcContext context,
+			[UndefinedAttribute] int x,
+			/* RpcArgumentSortOrder */ [Dependency] FooDependency foo /**/
+		) { }
+	}
+}
