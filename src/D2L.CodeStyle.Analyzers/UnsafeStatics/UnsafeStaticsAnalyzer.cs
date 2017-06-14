7ddcafb79974c7df8a8bf1d02028cc655c0cec02
Jacob Parker <Jacob.Parker@D2L.com>
2017-06-14 20:36:49 +0100

    Emit diagnostics for unnecessary and conflicting static annotations
    This required refactoring InspectType (renamed to InspectFieldOrProperty
    because that is more accurate) and the AnalyzeField/Property methods.
    
    In the new design most branches out of analysis are via yield breaks in
    GenerateDiagnostics. This allows InspectFieldOrProperty to implement the
    "if there are no diagnostics but there is an annotation, emit a
    diagnostic" logic. Bailing out earlier (e.g. in AnalyzeField) would
    break this. I've added some more comments, particularly around any
    bailing-out and some overall design notes for future readers.
    
    In the near future I plan to fix the way attributes are compared to use
    types from semantic analysis similar to the RpcAnalyzer.

--- src/D2L.CodeStyle.Analyzers/UnsafeStatics/UnsafeStaticsAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/UnsafeStatics/UnsafeStaticsAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using D2L.CodeStyle.Analyzers.Common;
 using Microsoft.CodeAnalysis;
@@ -13,7 +14,11 @@ namespace D2L.CodeStyle.Analyzers.UnsafeStatics {
 		public const string PROPERTY_FIELDORPROPNAME = "FieldOrProprName";
 		public const string PROPERTY_OFFENDINGTYPE = "OffendingType";
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create( Diagnostics.UnsafeStatic );
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			Diagnostics.UnsafeStatic,
+			Diagnostics.ConflictingStaticAnnotation,
+			Diagnostics.UnnecessaryStaticAnnotation
+		);
 
 		private readonly MutabilityInspector m_immutabilityInspector = new MutabilityInspector();
 		private readonly Utils m_utils = new Utils();
@@ -24,6 +29,7 @@ namespace D2L.CodeStyle.Analyzers.UnsafeStatics {
 				AnalyzeField,
 				SyntaxKind.FieldDeclaration
 			);
+
 			context.RegisterSyntaxNodeAction(
 				AnalyzeProperty,
 				SyntaxKind.PropertyDeclaration
@@ -37,53 +43,34 @@ namespace D2L.CodeStyle.Analyzers.UnsafeStatics {
 			}
 
 			var root = context.Node as FieldDeclarationSyntax;
+
 			if( root == null ) {
+				// This can't happen: this check makes the compiler not worried
 				return;
 			}
 
-			if( !root.Modifiers.Any( SyntaxKind.StaticKeyword ) ) {
-				// ignore non-static
-				return;
-			}
+			bool isStatic = root.Modifiers.Any( SyntaxKind.StaticKeyword );
+			bool isReadOnly = root.Modifiers.Any( SyntaxKind.ReadOnlyKeyword );
 
 			foreach( var variable in root.Declaration.Variables ) {
 				var symbol = context.SemanticModel.GetDeclaredSymbol( variable ) as IFieldSymbol;
 
 				if( symbol == null ) {
+					// Could this happen? We are not emitting diagnostics in this
+					// case even when the fields have an annotation.
 					continue;
 				}
 
-				if( symbol.GetAttributes().Any( a => a.AttributeClass.MetadataName == "Unaudited" ) ) {
-					// anyhing marked unaudited should not break the build, it's temporary
-					return;
-				}
-				if( symbol.GetAttributes().Any( a => a.AttributeClass.MetadataName == "Audited" ) ) {
-					// anything marked audited has been explicitly marked as a safe static
-					return;
-				}
-
-				if( !symbol.IsReadOnly ) {
-					var diagnostic = CreateDiagnostic(
-						variable.GetLocation(),
-						symbol.Name,
-						symbol.Type.GetFullTypeNameWithGenericArguments(),
-						MutabilityInspectionResult.Mutable(
-							symbol.Name,
-							symbol.Type.GetFullTypeNameWithGenericArguments(),
-							MutabilityTarget.Member,
-							MutabilityCause.IsNotReadonly
-						)
-					);
-					context.ReportDiagnostic( diagnostic );
-					return;
-				}
-
-				InspectType(
+				InspectFieldOrProperty(
 					context,
-					symbol.Type,
-					variable.Initializer?.Value,
-					variable.GetLocation(),
-					variable.Identifier.ValueText
+					location: variable.GetLocation(),
+					attributeLists: root.AttributeLists,
+					isStatic: isStatic,
+					isReadOnly: isReadOnly,
+					fieldOrPropertyType: symbol.Type,
+					fieldOrPropertyName: variable.Identifier.ValueText,
+					initializer: variable.Initializer?.Value,
+					isPropertyGetterImplemented: false
 				);
 			}
 		}
@@ -95,95 +82,214 @@ namespace D2L.CodeStyle.Analyzers.UnsafeStatics {
 			}
 
 			var root = context.Node as PropertyDeclarationSyntax;
+
 			if( root == null ) {
+				// This can't happen: this check makes the compiler not worried
 				return;
 			}
 
-			if( !root.Modifiers.Any( SyntaxKind.StaticKeyword ) ) {
-				// ignore non-static
-				return;
-			}
+			bool isStatic = root.Modifiers.Any( SyntaxKind.StaticKeyword );
 
 			var prop = context.SemanticModel.GetDeclaredSymbol( root );
+
 			if( prop == null ) {
+				// Could this happen? We are not emitting diagnostics in this case
+				// even when the property has an annotation.
 				return;
 			}
 
-			if( prop.GetAttributes().Any( a => a.AttributeClass.MetadataName == "Unaudited" ) ) {
-				// anyhing marked unaudited should not break the build, it's temporary
+			bool isPropertyGetterImplemented = root.IsPropertyGetterImplemented();
+
+			InspectFieldOrProperty(
+				context,
+				location: root.GetLocation(),
+				attributeLists: root.AttributeLists,
+				isStatic: isStatic,
+				isReadOnly: prop.IsReadOnly,
+				fieldOrPropertyType: prop.Type,
+				fieldOrPropertyName: prop.Name,
+				initializer: root.Initializer?.Value,
+				isPropertyGetterImplemented: isPropertyGetterImplemented
+			);
+		}
+
+		/// <summary>
+		/// This helper method implements all of the shared logic. We have to
+		/// split this for two reasons:
+		///
+		/// 1. PropertyDeclarationSyntax and FieldDeclarationSyntaxs useful
+		///    members don't come from a base class/interface so we can't do
+		///    this generically.
+		/// 2. Fields can define multiple variables and we may wish to output
+		///    multiple diagnostics (individual ariables in a field declaration
+		///    may be alright because of their initializers.)
+		///
+		/// The arguments are organized roughly based on the common grammar of
+		/// fields and properties: attributes, modifiers, type, name,
+		/// initializer.
+		/// </summary>
+		private void InspectFieldOrProperty(
+			SyntaxNodeAnalysisContext context,
+			Location location,
+			SyntaxList<AttributeListSyntax> attributeLists,
+			bool isStatic,
+			bool isReadOnly,
+			ITypeSymbol fieldOrPropertyType,
+			string fieldOrPropertyName,
+			ExpressionSyntax initializer,
+			bool isPropertyGetterImplemented // only applies to properties
+		) {
+			var diagnostics = GatherDiagnostics(
+				context.SemanticModel,
+				location: location,
+				isStatic: isStatic,
+				isReadonly: isReadOnly,
+				fieldOrPropertyType: fieldOrPropertyType,
+				fieldOrPropertyName: fieldOrPropertyName,
+				initializationExpression: initializer,
+				isPropertyGetterImplemented: isPropertyGetterImplemented
+			);
+
+			var enumerator = diagnostics.GetEnumerator();
+
+			var hasDiagnostics = enumerator.MoveNext();
+
+			var attributes = attributeLists
+				.SelectMany( al => al.Attributes )
+				.ToImmutableArray();
+
+			// TODO: This EndsWith stuff is lame. We should do the same thing
+			// that the RpcAnalyzer does with looking up and caching the types.
+			bool hasUnauditedAnnotation = attributes
+				.Any( a => a.Name.ToFullString().EndsWith( ".Unaudited" ) );
+
+			// TODO: This EndsWith stuff is lame. We should do the same thing
+			// that the RpcAnalyzer does with looking up and caching the types.
+			bool hasAuditedAnnotation = attributes
+				.Any( a => a.Name.ToFullString().EndsWith( ".Audited" ) );
+
+			if ( hasAuditedAnnotation && hasUnauditedAnnotation ) {
+				context.ReportDiagnostic(
+					Diagnostic.Create(
+						Diagnostics.ConflictingStaticAnnotation,
+						location
+					)
+				);
+
+				// Bail out here because it's unclear which of the remaining
+				// diagnostics for this field/property should apply.
 				return;
 			}
 
-			if( prop.GetAttributes().Any( a => a.AttributeClass.MetadataName == "Audited" ) ) {
-				// anything marked audited has been explicitly marked as a safe static
+			bool hasAnnotations = hasAuditedAnnotation || hasUnauditedAnnotation;
+
+			if ( hasAnnotations && !hasDiagnostics ) {
+				context.ReportDiagnostic(
+					Diagnostic.Create(
+						Diagnostics.UnnecessaryStaticAnnotation,
+						location
+					)
+				);
+
+				// this bail-out isn't important because !hasDiagnostics
 				return;
 			}
 
-			if( !prop.IsReadOnly ) {
-				var diagnostic = CreateDiagnostic(
-					root.GetLocation(),
-					prop.Name,
-					prop.Type.GetFullTypeNameWithGenericArguments(),
+			if ( hasAnnotations ) {
+				// the annotations supress remaining diagnostics
+				return;
+			}
+
+			// We're manually using enumerators here.
+			// - if we used IEnumerable directly we'd re-compute the first
+			//   diagnostic in the GatherDiagnostics generator
+			// - if we did .ToArray() early then we would avoid multiple
+			//   enumeration but would compute diagnostics even when we
+			//   ultimately ignore them due to annotations
+			while ( hasDiagnostics ) {
+				context.ReportDiagnostic( enumerator.Current );
+				hasDiagnostics = enumerator.MoveNext();
+			}
+		}
+
+		/// <summary>
+		/// All logic relating to either emitting or not emitting diagnostics other
+		/// than the ones about unnecessary annotations belong in this function.
+		/// This allows InspectMember to implement the logic around the unnecessary
+		/// annotations diagnostic. Any time we bail early in AnalyzeField or
+		/// AnalyzeProperty we risk not emitting unnecessary annotation
+		/// diagnostics.
+		/// </summary>
+		private IEnumerable<Diagnostic> GatherDiagnostics(
+			SemanticModel model,
+			Location location,
+			bool isStatic,
+			bool isReadonly,
+			ITypeSymbol fieldOrPropertyType,
+			string fieldOrPropertyName,
+			ExpressionSyntax initializationExpression,
+			bool isPropertyGetterImplemented
+		) {
+			if ( !isStatic ) {
+				yield break;
+			}
+
+			if( isPropertyGetterImplemented ) {
+				// things with getters are really just functions; they can't
+				// hold state themselves.
+				yield break;
+			}
+
+			if( !isReadonly ) {
+				yield return CreateDiagnostic(
+					location,
+					fieldOrPropertyName,
+					fieldOrPropertyType.GetFullTypeNameWithGenericArguments(),
 					MutabilityInspectionResult.Mutable(
-						prop.Name,
-						prop.Type.GetFullTypeNameWithGenericArguments(),
+						fieldOrPropertyName,
+						fieldOrPropertyType.GetFullTypeNameWithGenericArguments(),
 						MutabilityTarget.Member,
 						MutabilityCause.IsNotReadonly
 					)
 				);
-				context.ReportDiagnostic( diagnostic );
-				return;
-			}
-			if( root.IsPropertyGetterImplemented() ) {
-				// property has getter with body; it is either backed by a field, or is a static function; ignore
-				return;
 			}
 
-			InspectType( context, prop.Type, root.Initializer?.Value, root.GetLocation(), prop.Name );
-		}
-
-		private void InspectType(
-			SyntaxNodeAnalysisContext context,
-			ITypeSymbol type,
-			ExpressionSyntax exp,
-			Location location,
-			string fieldOrPropName
-		) {
-			if( m_immutabilityInspector.IsTypeMarkedImmutable( type ) ) {
-				// if the type is marked immutable, skip checking it, to avoid reporting a diagnostic for each usage of non-immutable types that are marked immutable (another analyzer catches this already)
-				return;
+			if( m_immutabilityInspector.IsTypeMarkedImmutable( fieldOrPropertyType ) ) {
+				// if the type is marked immutable, skip checking it, to avoid reporting
+				// a diagnostic for each usage of non-immutable types that are marked
+				// immutable (another analyzer catches this already.)
+				yield break;
 			}
 
 			var flags = MutabilityInspectionFlags.Default;
 
 			// Always prefer the type from the initializer if it exists because
 			// it may be more specific.
-			if( exp != null ) {
-				var typeInfo = context.SemanticModel.GetTypeInfo( exp );
+			if( initializationExpression != null ) {
+				var initializerType = model.GetTypeInfo( initializationExpression ).Type;
 
 				// Fall back to the declaration type if we can't get a type for
-				// the initializer
-				if( typeInfo.Type != null && !( typeInfo.Type is IErrorTypeSymbol ) ) {
-					type = typeInfo.Type;
+				// the initializer.
+				if( initializerType != null && !( initializerType is IErrorTypeSymbol ) ) {
+					fieldOrPropertyType = initializerType;
 				}
 			}
 
 			// When we know the concrete type as in "new T()" we don't have to
-			// be paranoid about mutable derived classes
-			if ( exp is ObjectCreationExpressionSyntax ) {
+			// be paranoid about mutable derived classes.
+			if ( initializationExpression is ObjectCreationExpressionSyntax ) {
 				flags |= MutabilityInspectionFlags.AllowUnsealed;
 			}
 
-			var result = m_immutabilityInspector.InspectType( type, flags );
+			var result = m_immutabilityInspector.InspectType( fieldOrPropertyType, flags );
 			if ( result.IsMutable ) {
-				result = result.WithPrefixedMember( fieldOrPropName );
-				var diagnostic = CreateDiagnostic( 
+				result = result.WithPrefixedMember( fieldOrPropertyName );
+				yield return CreateDiagnostic( 
 					location, 
-					fieldOrPropName, 
-					type.GetFullTypeNameWithGenericArguments(), 
+					fieldOrPropertyName, 
+					fieldOrPropertyType.GetFullTypeNameWithGenericArguments(), 
 					result 
 				);
-				context.ReportDiagnostic( diagnostic );
 			}
 		}
 
@@ -209,6 +315,5 @@ namespace D2L.CodeStyle.Analyzers.UnsafeStatics {
 			);
 			return diagnostic;
 		}
-
 	}
 }
