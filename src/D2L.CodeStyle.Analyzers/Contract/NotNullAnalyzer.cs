802d1d2b4462b773ddb6ba9406a1464aa9b736d8
Jason Tarka <jason.tarka@d2l.com>
2017-06-20 13:08:21 -0400

    US87654: Initial `NotNull` attribute & analyzer
    - Works across the test cases
    - Starting a check once per invocation should be more efficient than than once per argument passed
      - Will still report one error per argument that has an issue
    - Keep track of whether or not methods & parameters require not-null, so the attribute lists don't have to be fetched & checked for *every* parameter repeatedly
      - Prevents having to look this up every time a common method is encountered
      - I figured the decreased time to compile would be worth the increased memory usage

---
+++ src/D2L.CodeStyle.Analyzers/Contract/NotNullAnalyzer.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using D2L.CodeStyle.Analyzers.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.Contract {
+
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	internal sealed class NotNullAnalyzer : DiagnosticAnalyzer {
+
+		private const string Namespace = "D2L.CodeStyle.Annotations.Contract.";
+		private const string NotNullAttribute = Namespace + "NotNullAttribute";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+			=> ImmutableArray.Create( Diagnostics.NullPassedToNotNullParameter );
+
+		public override void Initialize( AnalysisContext context ) {
+			context.EnableConcurrentExecution();
+			context.RegisterCompilationStartAction( RegisterNotNullAnalyzer );
+		}
+
+		public static void RegisterNotNullAnalyzer( CompilationStartAnalysisContext context ) {
+			// For caching if a method has any not-null parameters, and the which ones are
+			var notNullMethodCache = new ConcurrentDictionary<IMethodSymbol, ImmutableHashSet<IParameterSymbol>>();
+
+			context.RegisterSyntaxNodeAction(
+					ctx => AnalyzeInvocation(
+							ctx,
+							notNullMethodCache
+						),
+					SyntaxKind.InvocationExpression
+				);
+		}
+
+		private static void AnalyzeInvocation(
+			SyntaxNodeAnalysisContext context,
+			IDictionary<IMethodSymbol, ImmutableHashSet<IParameterSymbol>> notNullMethodCache
+		) {
+			var invocation = context.Node as InvocationExpressionSyntax;
+			if( invocation == null ) {
+				// A method isn't being invoked, so there's nothing to look at
+				return;
+			}
+
+			var arguments = invocation.ArgumentList.Arguments;
+			if( arguments.Count == 0 ) {
+				// We don't care about methods that take no arguments
+				return;
+			}
+
+			IList<Tuple<ArgumentSyntax, IParameterSymbol>> notNullArguments;
+			bool isNotNullMethod = TryGetNotNullArguments(
+					context,
+					invocation,
+					arguments,
+					notNullMethodCache,
+					out notNullArguments
+				);
+
+			if( !isNotNullMethod ) {
+				// The called method doesn't have any [NotNull] parameters, so there's nothing more to analyze
+				return;
+			}
+
+			// Start analyzing the arguments
+			foreach( Tuple<ArgumentSyntax, IParameterSymbol> tuple in notNullArguments ) {
+				ArgumentSyntax argument = tuple.Item1;
+				IParameterSymbol parameter = tuple.Item2;
+
+				var literalExpression = argument.Expression as LiteralExpressionSyntax;
+				if( literalExpression != null
+					&& literalExpression.Token.Kind() == SyntaxKind.NullKeyword
+				) {
+					// It is a literal "null" keyword, so mark it as an error
+					Diagnostic diagnostic = Diagnostic.Create(
+							Diagnostics.NullPassedToNotNullParameter,
+							argument.GetLocation(),
+							parameter.Name
+						);
+					context.ReportDiagnostic( diagnostic );
+				}
+			}
+		}
+
+		private static bool TryGetNotNullArguments(
+			SyntaxNodeAnalysisContext context,
+			InvocationExpressionSyntax invocation,
+			SeparatedSyntaxList<ArgumentSyntax> arguments,
+			IDictionary<IMethodSymbol, ImmutableHashSet<IParameterSymbol>> notNullMethodCache,
+			out IList<Tuple<ArgumentSyntax, IParameterSymbol>> notNullArguments
+		) {
+			IMethodSymbol invokedSymbol;
+			if( !TryGetInvokedSymbol( context, invocation, out invokedSymbol ) ) {
+				// There could either by multiple methods that match, in which case we don't know which we should
+				// look at, or the method being called may not actually exist.
+				notNullArguments = null;
+				return false;
+			}
+
+			ImmutableHashSet<IParameterSymbol> notNullParameterCache;
+			if( notNullMethodCache.TryGetValue( invokedSymbol, out notNullParameterCache )
+				&& notNullParameterCache == null
+			) {
+				// We've examined it before, and nothing needs to be not null
+				notNullArguments = null;
+				return false;
+			}
+
+			ImmutableArray<IParameterSymbol> parameters = invokedSymbol.Parameters;
+			if( arguments.Count > parameters.Length ) {
+				// Something is weird, and we can't analyze this
+				notNullArguments = null;
+				return false;
+			}
+
+			notNullArguments = new List<Tuple<ArgumentSyntax, IParameterSymbol>>();
+			for( int i = 0; i < arguments.Count; i++ ) {
+				ArgumentSyntax argument = arguments[i];
+				IParameterSymbol param;
+
+				if( !TryGetParameter( argument, parameters, i, out param ) ) {
+					// If the parameter matching the argument can't be retrieved for whatever reason then
+					// we can't examine the attributes on it
+					continue;
+				}
+
+				if( notNullParameterCache != null ) {
+					if( notNullParameterCache.Contains( param ) ) {
+						notNullArguments.Add( new Tuple<ArgumentSyntax, IParameterSymbol>( arguments[i], param ) );
+					}
+					// We've previously cached whether or not this parameter has [NotNull], so
+					// there's no need to examine it again
+					continue;
+				}
+
+				// Check if the parameter has [NotNull]
+				if( SymbolHasAttribute( param, NotNullAttribute ) ) {
+					notNullArguments.Add( new Tuple<ArgumentSyntax, IParameterSymbol>( arguments[i], param ) );
+				}
+			}
+
+			bool isNotNullMethod = notNullArguments.Count > 0;
+			notNullMethodCache[invokedSymbol] = notNullArguments
+				.Select( x => x.Item2 )
+				.ToImmutableHashSet();
+			return isNotNullMethod;
+		}
+
+		private static bool TryGetParameter(
+			ArgumentSyntax argument,
+			ImmutableArray<IParameterSymbol> parameters,
+			int argumentIndex,
+			out IParameterSymbol parameter
+		) {
+			// Not a named parameter
+			if( argument.NameColon == null ) { // Regular order-based
+				parameter = parameters[argumentIndex];
+				return true;
+			}
+
+			// While parameters with @ are allowed in C#, the compiler strips it from the parameter
+			// name, but not from the named argument
+			string argumentName = argument.NameColon.Name.ToString().TrimStart( '@' );
+			parameter = parameters.SingleOrDefault( p => p.Name == argumentName );
+
+			return parameter != null;
+		}
+
+		private static bool TryGetInvokedSymbol(
+			SyntaxNodeAnalysisContext context,
+			InvocationExpressionSyntax invocation,
+			out IMethodSymbol invokedSymbol
+		) {
+			SymbolInfo invokedSymbolInfo = context.SemanticModel.GetSymbolInfo( invocation );
+
+			// The simple case, where there's no ambiguity
+			invokedSymbol = invokedSymbolInfo.Symbol as IMethodSymbol;
+			if( invokedSymbol != null ) {
+				return true;
+			}
+
+			// Default parameter values might result in a single candidate, but doesn't guarantee
+			// resolution to a method symbol
+			if( invokedSymbolInfo.CandidateReason == CandidateReason.OverloadResolutionFailure
+				&& invokedSymbolInfo.CandidateSymbols.Length == 1
+			) {
+				invokedSymbol = invokedSymbolInfo.CandidateSymbols[0] as IMethodSymbol;
+			}
+
+			return invokedSymbol != null;
+		}
+
+		private static bool SymbolHasAttribute(
+			ISymbol symbol,
+			string attributeClassName,
+			string expectedArgumentValue = null
+		) {
+			ImmutableArray<AttributeData> attributes = symbol.GetAttributes();
+			return attributes.Length > 0
+				&& attributes.Any(
+					x => x.AttributeClass.ToString() == attributeClassName
+						&& (
+							expectedArgumentValue == null 
+							|| (
+								x.ConstructorArguments.Length >= 1
+								&& x.ConstructorArguments[0].Value.ToString() == expectedArgumentValue
+							)
+						)
+				);
+		}
+
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Contracts/NotNullAnalyzerTests.cs
@@ -0,0 +1,432 @@
+﻿using System.Linq;
+using D2L.CodeStyle.Analyzers.Common;
+using D2L.CodeStyle.Analyzers.Contract;
+using D2L.CodeStyle.Analyzers.Test.Verifiers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers.Contracts {
+
+	[TestFixture]
+	internal sealed class NotNullAnalyzerTests : DiagnosticVerifier {
+
+		private const string Attributes = @"
+using System;
+using D2L.CodeStyle.Annotations.Contract;
+
+namespace D2L.CodeStyle.Annotations.Contract {
+	public class NotNullAttribute : System.Attribute {}
+}
+";
+
+		private const string NotNullParamMethod = Attributes + @"
+namespace Test {
+	class TestProvider {
+		public void TestMethod(
+			[NotNull] string testName
+		) {}
+
+		public void TestMethod(
+			object allowedToBeNull,
+			[NotNull] string testName
+		) {}
+
+		public void MultiNotNull(
+			[NotNull] string testName,
+			[NotNull] string anotherName
+		) {}
+
+		public string GetStuff() { return ""Ghostbusters"" };
+
+		public void TestMethodCanTakeNull( string testName ) {}
+
+		public bool ShouldDoStuff => false;
+	}
+}
+";
+
+		private static readonly int NotNullParamMethodLines = NotNullParamMethod.Count( c => c.Equals( '\n' ) ) + 1;
+
+		#region Parameter has [NotNull] directly
+
+		#region Should produce errors
+
+		[Test]
+		public void NotNullParam_NullIsPassed_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( null );
+		}
+	}
+}";
+			AssertProducesError(
+					test,
+					5 + NotNullParamMethodLines,
+					25,
+					"testName"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_NullIsPassed_MethodInSameClass_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			DoStuff( null );
+		}
+
+		public void DoStuff(
+			[D2L.CodeStyle.Annotations.Contract.NotNull] string stuff
+		) {}
+	}
+}";
+			AssertProducesError(
+					test,
+					4 + NotNullParamMethodLines,
+					13,
+					"stuff"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_MultipleParametersWithIssue_ReportsMultipleProblems() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( ""Hello!"" );
+			provider.MultiNotNull(
+					null,
+					null
+				);
+		}
+	}
+}";
+			DiagnosticDescriptor descriptor = Diagnostics.NullPassedToNotNullParameter;
+			DiagnosticResult[] expectedResults = new[] {
+				new DiagnosticResult() {
+					Id = descriptor.Id,
+					Message = string.Format( descriptor.MessageFormat.ToString(), "testName" ),
+					Severity = DiagnosticSeverity.Error,
+					Locations = new[] {
+						new DiagnosticResultLocation( "Test0.cs", 7 + NotNullParamMethodLines, 6 )
+					}
+				},
+				new DiagnosticResult() {
+					Id = descriptor.Id,
+					Message = string.Format( descriptor.MessageFormat.ToString(), "anotherName" ),
+					Severity = DiagnosticSeverity.Error,
+					Locations = new[] {
+						new DiagnosticResultLocation( "Test0.cs", 8 + NotNullParamMethodLines, 6 )
+					}
+				}
+			};
+
+			VerifyCSharpDiagnostic( test, expectedResults );
+		}
+
+		[Test]
+		public void NotNullParam_NamedArguments_OneIsNull_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod(
+				testName: null,
+				allowedToBeNull: ""This is an object""
+			);
+		}
+	}
+}";
+			AssertProducesError(
+					test,
+					6 + NotNullParamMethodLines,
+					5,
+					"testName"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_NamedArgumentsWithAtSign_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			DoSomeStuff(
+				@char: null,
+				myVar: ""A Value""
+			);
+		}
+
+		private void DoSomeStuff(
+			[NotNull] string @char,
+			[NotNull] string myVar
+		) {}
+	}
+}";
+			AssertProducesError(
+					test,
+					5 + NotNullParamMethodLines,
+					5,
+					"char"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_CalledTwice_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( ""This is a value"" );
+			provider.TestMethod( null );
+		}
+	}
+}";
+			AssertProducesError(
+					test,
+					6 + NotNullParamMethodLines,
+					25,
+					"testName"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_CalledMethodHasDefaultValues_NullValuePassed_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			DoSomeStuff( provider, null );
+		}
+
+		private void DoSomeStuff(
+			Provider provider,
+			[NotNull] string value = ""a value"",
+			[NotNull] string anotherValue = ""another value""
+		) {}
+	}
+}";
+			AssertProducesError(
+					test,
+					5 + NotNullParamMethodLines,
+					27,
+					"value"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_CalledMethodHasDefaultValues_NamedArgumentsWithNull_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			DoSomeStuff(
+					provider,
+					anotherValue: null
+				);
+		}
+
+		private void DoSomeStuff(
+			TestProvider provider,
+			[NotNull] string value = ""a value"",
+			[NotNull] string anotherValue = ""another value""
+		) {}
+	}
+}";
+			AssertProducesError(
+					test,
+					7 + NotNullParamMethodLines,
+					6,
+					"anotherValue"
+				);
+		}
+
+		[Test]
+		public void NotNullParam_OverloadedMethodHasDefaultValues_NamedArgumentsWithNull_ReportsProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			DoSomeStuff(
+					value: null,
+					provider: provider
+				);
+		}
+
+		private void DoSomeStuff(
+			string provider = ""a value"",
+			[NotNull] string value = ""another value""
+		) {}
+
+		private void DoSomeStuff(
+			TestProvider provider,
+			[NotNull] string value = ""a value""
+		) {}
+	}
+}";
+			AssertProducesError(
+					test,
+					6 + NotNullParamMethodLines,
+					6,
+					"value"
+				);
+		}
+
+		#endregion
+
+		#region Should not produce errors
+
+		[Test]
+		public void NotNullParam_ValueIsPassed_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( ""My Name"" );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_VariableWithValueAssigned_AtDeclaration_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			string name = ""Hello!"";
+			provider.TestMethod( name );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_MethodParameterPassed_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod( string intakeName ) {
+			var provider = new TestProvider();
+			provider.TestMethod( intakeName );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_NoAttribute_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethodCanTakeNull( null );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_OneParamNotNull_NullPassedToNullable_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( null, ""This is the not-nullable one"" );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_MethodCallResultPassedDirectly_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			provider.TestMethod( provider.GetStuff() );
+		}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+
+		[Test]
+		public void NotNullParam_CalledMethodHasDefaultValues_MarkedAsNotNull_DoesNotReportProblem() {
+			const string test = NotNullParamMethod + @"
+namespace Test {
+	class TestCaller {
+		public void TestMethod() {
+			var provider = new TestProvider();
+			string hello = ""something"";
+			DoSomeStuff( provider, hello );
+		}
+
+		private void DoSomeStuff(
+			TestProvider provider,
+			[NotNull] string value = ""a value"",
+			[NotNull] string anotherValue = ""another value"",
+		) {}
+	}
+}";
+			AssertDoesNotProduceError( test );
+		}
+		
+
+		#endregion
+
+		#endregion
+
+		private void AssertDoesNotProduceError(
+			string file
+		) {
+			VerifyCSharpDiagnostic( file );
+		}
+
+		private void AssertProducesError(
+			string file,
+			int line,
+			int column,
+			string paramName
+		) {
+			DiagnosticDescriptor descriptor = Diagnostics.NullPassedToNotNullParameter;
+			DiagnosticResult result = new DiagnosticResult() {
+				Id = descriptor.Id,
+				Message = string.Format( descriptor.MessageFormat.ToString(), paramName ),
+				Severity = DiagnosticSeverity.Error,
+				Locations = new[] {
+					new DiagnosticResultLocation( "Test0.cs", line, column ),
+				}
+			};
+
+			VerifyCSharpDiagnostic( file, result );
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+			return new NotNullAnalyzer();
+		}
+
+	}
+}
