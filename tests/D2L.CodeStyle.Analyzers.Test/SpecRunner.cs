c3277c72d4c041f571b463e53650ea3e2581f51e
Jacob Parker <Jacob.Parker@D2L.com>
2017-07-04 23:00:18 +0100

    Remove unused using

--- tests/D2L.CodeStyle.Analyzers.Test/SpecRunner.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/SpecRunner.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using D2L.CodeStyle.Analyzers.Common;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
