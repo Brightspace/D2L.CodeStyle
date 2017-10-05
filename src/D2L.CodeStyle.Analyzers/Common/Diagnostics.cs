679c936fadac06769212c2f9c54614cf0b37eac4
Jacob Parker <Jacob.Parker@D2L.com>
2017-10-05 19:32:08 +0100

    Drop severity of ClassShouldBeSealed to info for now

--- src/D2L.CodeStyle.Analyzers/Common/Diagnostics.cs
+++ src/D2L.CodeStyle.Analyzers/Common/Diagnostics.cs
@@ -107,7 +107,7 @@ namespace D2L.CodeStyle.Analyzers.Common {
 			title: "Non-public class should be sealed because it doesn't have any subtypes.",
 			messageFormat: "Non-public class should be sealed because it doesn't have any subtypes.",
 			category: "Style",
-			defaultSeverity: DiagnosticSeverity.Error,
+			defaultSeverity: DiagnosticSeverity.Info,
 			isEnabledByDefault: true,
 			description: "Non-public class should be sealed because it doesn't have any subtypes."
 		);
