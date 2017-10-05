e2b859dafab4be8b4061263ed5c02f3727edbba7
Jacob Parker <Jacob.Parker@D2L.com>
2017-10-05 19:31:37 +0100

    Add integration tests for ClassesShouldBeSealedAnalyzer

---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ClassShouldBeSealedAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.ClassShouldBeSealed.ClassShouldBeSealedAnalyzer
+
+namespace D2L.CodeStyle.Analyzers.Specs {
+	public class PublicClass { } // no diagnostic because its public
+	internal abstract class AbstractClass { } // no diagnostic because its abstract (but it is dead code)
+	interface ISomeInterface { } // interfaces can't be sealed
+	internal struct Struct { } // structs are implicitly sealed
+
+	internal sealed class SealedClass { } // already sealed
+
+	internal class /* ClassShouldBeSealed */ InternalUnsealedClass /**/ {
+		private class /* ClassShouldBeSealed */ PrivateUnsealedClass /**/ { }
+	}
+
+	// internal/private classes can be unsealed if they are actually used as a base somewhere
+	internal class InternalBaseClass {
+		private class PrivateBaseBaseClass { }
+
+		private class PrivateBaseClass : PrivateBaseBaseClass { }
+
+		private sealed class Derived : PrivateBaseClass { }
+	}
+
+	public sealed class Derived : InternalBaseClass {}
+
+	internal class AnotherInternalBaseBaseClass { } // this one is used (albeit not usefully) as a base class
+	internal class /* ClassShouldBeSealed */ AnotherInternalBaseClass /**/ : AnotherInternalBaseBaseClass { }
+
+	// Controversial opinion: there seems to be a significant amount of
+	// inappopriate use of protected in our code base. A large reason is a form
+	// of code-gen that no longer exists that would create derived types for
+	// webpage classes. We'll be ruthless and call this an error too.
+	internal class /* ClassShouldBeSealed */ ThingWithProtectedMembers /**/ {
+		protected int x;
+
+		protected ThingWithProtectedMembers() {}
+
+		public virtual void Foo() {}
+	}
+}
