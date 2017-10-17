331ed8e3f73cffc267cf6d6961bbdca33d2dbf77
Jacob Parker <Jacob.Parker@D2L.com>
2017-10-17 20:06:48 +0100

    Create ISemanticModel to aid in writing tests

---
+++ src/D2L.CodeStyle.Analyzers/Common/Mutability/WrappedSemanticModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.Analyzers.Common.Mutability {
+	internal sealed class WrappedSemanticModel : ISemanticModel {
+		private readonly SemanticModel m_model;
+		
+		public WrappedSemanticModel( SemanticModel model ) {
+			m_model = model;
+		}
+
+		public IAssemblySymbol Assembly() {
+			return m_model.Compilation.Assembly;
+		}
+	}
+}
