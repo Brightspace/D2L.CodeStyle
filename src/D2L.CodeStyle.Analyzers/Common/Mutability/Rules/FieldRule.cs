887b491dfebe40843dadfa92811189de5548a62c
Jacob Parker <Jacob.Parker@D2L.com>
2017-10-23 22:01:37 +0100

    Add note about implicit casts

--- src/D2L.CodeStyle.Analyzers/Common/Mutability/Rules/FieldRule.cs
+++ src/D2L.CodeStyle.Analyzers/Common/Mutability/Rules/FieldRule.cs
@@ -53,6 +53,11 @@ namespace D2L.CodeStyle.Analyzers.Common.Mutability.Rules {
 			foreach( var init in initializers ) {
 				yield return new InitializerGoal( init.Value );
 			}
+
+			// NOTE: we may want to pass the type in the initializer goal.
+			// implicit conversions make looking at the initializer in
+			// isolation theoretically dangerous. See this issue for more info:
+			// https://github.com/Brightspace/D2L.CodeStyle/issues/35
 		}
 	}
 }
