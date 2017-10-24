1db37bf04c0806e55b1a51393b2be5fe39a5b4d4
Jacob Parker <jacob@solidangle.ca>
2017-10-24 11:10:28 -0400

    Add GenericTypeParameterGoal/GenericTypeParameterRule

---
+++ src/D2L.CodeStyle.Analyzers/Common/Mutability/Goals/GenericTypeParameterGoal.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.Analyzers.Common.Mutability.Goals {
+	internal struct GenericTypeParameterGoal : Goal {
+		public GenericTypeParameterGoal(
+			ITypeParameterSymbol type
+		) {
+			Type = type;
+		}
+
+		public ITypeParameterSymbol Type { get; }
+	}
+}
