6df27298c5257ee73b26f3f0705d326f72ff8392
Jacob Parker <jacob@solidangle.ca>
2017-10-24 13:55:04 -0400

    Add ReadOnlyRule

---
+++ src/D2L.CodeStyle.Analyzers/Common/Mutability/Rules/ReadOnlyRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using D2L.CodeStyle.Analyzers.Common.Mutability.Goals;
+
+namespace D2L.CodeStyle.Analyzers.Common.Mutability.Rules {
+	internal static class ReadOnlyRule {
+		public static IEnumerable<Goal> Apply(
+			ISemanticModel model,
+			ReadOnlyGoal goal
+		) {
+			if ( goal.Field != null && goal.Field.IsReadOnly ) {
+				yield break;
+			}
+
+			if ( goal.Property != null && goal.Property.IsReadOnly ) {
+				yield break;
+			}
+
+			yield return goal;
+		}
+	}
+}
