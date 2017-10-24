f536568d447db99a64eb4004fd43409e12864a62
Jacob Parker <jacob@solidangle.ca>
2017-10-24 15:16:46 -0400

    Add ConcreteType rule

---
+++ src/D2L.CodeStyle.Analyzers/Common/Mutability/Rules/ConcreteTypeRule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using D2L.CodeStyle.Analyzers.Common.Mutability.Goals;
+using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.Analyzers.Common.Mutability.Rules {
+	internal static class ConcreteTypeRule {
+		public static IEnumerable<Goal> Apply(
+			ISemanticModel model,
+			ConcreteTypeGoal goal
+		) {
+			switch ( goal.Type.TypeKind ) {
+				case TypeKind.Array:
+				case TypeKind.Delegate:
+				case TypeKind.Dynamic:
+					// These objects are inherently mutable
+					yield return goal;
+					yield break;
+
+				case TypeKind.Enum:
+					// Enums don't hold mutable state
+					yield break;
+
+				case TypeKind.Error:
+					// This only happens if there are other errors in the
+					// compilation. We ignore these types to avoid creating 
+					// additional unhelpful diagnostics.
+					yield break;
+
+				case TypeKind.Class:
+					yield return new ClassGoal( goal.Type );
+					yield break;
+
+				case TypeKind.Struct: // equivalent to TypeKind.Structure (VB)
+					yield return new StructGoal( goal.Type );
+					yield break;
+
+				case TypeKind.TypeParameter:
+					yield return new GenericTypeParameterGoal( goal.Type as ITypeParameterSymbol );
+					yield break;
+
+				case TypeKind.Interface:
+					throw new InvalidOperationException( "Unexpected ConcreteType goal for interface type" );
+
+				case TypeKind.Module:
+				case TypeKind.Pointer:
+				case TypeKind.Submission:
+				case TypeKind.Unknown:
+				default:
+					throw new NotImplementedException( "Unhandled case in ConcreteType rule" );
+			}
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Common/Mutability/Rules/ConcreteTypeRuleTests.cs
@@ -0,0 +1,86 @@
+﻿using D2L.CodeStyle.Analyzers.Common.Mutability.Goals;
+using Microsoft.CodeAnalysis;
+using Moq;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers.Common.Mutability.Rules {
+	[TestFixture]
+	public sealed class ConcreteTypeRuleTests {
+		private readonly ISemanticModel m_model = new Mock<ISemanticModel>( MockBehavior.Strict ).Object;
+
+		[TestCase( TypeKind.Array )]
+		[TestCase( TypeKind.Delegate )]
+		[TestCase( TypeKind.Dynamic )]
+		public void ConcreteTypeForThingsThatDontReduce( TypeKind kind ) {
+			var type = new Mock<ITypeSymbol>( MockBehavior.Strict );
+			type.Setup( t => t.TypeKind ).Returns( kind );
+
+			var goal = new ConcreteTypeGoal( type.Object );
+
+			var subgoals = ConcreteTypeRule.Apply( m_model, goal );
+
+			CollectionAssert.AreEquivalent(
+				new[] { goal },
+				subgoals
+			);
+		}
+
+		[TestCase( TypeKind.Enum )]
+		[TestCase( TypeKind.Error )]
+		public void ConcreteTypeForThingsThatReduceToNothing( TypeKind kind ) {
+			var type = new Mock<ITypeSymbol>( MockBehavior.Strict );
+			type.Setup( t => t.TypeKind ).Returns( kind );
+
+			var goal = new ConcreteTypeGoal( type.Object );
+
+			var subgoals = ConcreteTypeRule.Apply( m_model, goal );
+
+			CollectionAssert.IsEmpty( subgoals );
+		}
+
+		[Test]
+		public void ConcreteTypeForClass() {
+			var type = new Mock<ITypeSymbol>( MockBehavior.Strict );
+			type.Setup( t => t.TypeKind ).Returns( TypeKind.Class );
+
+			var goal = new ConcreteTypeGoal( type.Object );
+
+			var subgoals = ConcreteTypeRule.Apply( m_model, goal );
+
+			CollectionAssert.AreEquivalent(
+				new[] { new ClassGoal( type.Object ) },
+				subgoals
+			);
+		}
+
+		[Test]
+		public void ConcreteTypeForStruct() {
+			var type = new Mock<ITypeSymbol>( MockBehavior.Strict );
+			type.Setup( t => t.TypeKind ).Returns( TypeKind.Struct );
+
+			var goal = new ConcreteTypeGoal( type.Object );
+
+			var subgoals = ConcreteTypeRule.Apply( m_model, goal );
+
+			CollectionAssert.AreEquivalent(
+				new[] { new StructGoal( type.Object ) },
+				subgoals
+			);
+		}
+
+		[Test]
+		public void ConcreteTypeForGenericTypeParameter() {
+			var type = new Mock<ITypeParameterSymbol>( MockBehavior.Strict );
+			type.Setup( t => t.TypeKind ).Returns( TypeKind.TypeParameter );
+
+			var goal = new ConcreteTypeGoal( type.Object as ITypeSymbol );
+
+			var subgoals = ConcreteTypeRule.Apply( m_model, goal );
+
+			CollectionAssert.AreEquivalent(
+				new[] { new GenericTypeParameterGoal( type.Object ) },
+				subgoals
+			);
+		}
+	}
+}
