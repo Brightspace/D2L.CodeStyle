09027564898e56d1bf49f5a5993149536703cea8
Jeff Ashton <Jeff.Ashton@D2L.com>
2017-12-21 00:55:00 -0500

    Restricting IFeature to only existing types

---
+++ src/D2L.CodeStyle.Analyzers/LaunchDarkly/ObsoleteLaunchDarklyApisAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Immutable;
+using D2L.CodeStyle.Analyzers.Common;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.LaunchDarkly {
+
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	internal sealed class ObsoleteLaunchDarklyApisAnalyzer : DiagnosticAnalyzer {
+
+		public const string IFeatureFullName = "D2L.LP.LaunchDarkly.FeatureFlagging.IFeature";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+				Diagnostics.ObsoleteLaunchDarklyFramework
+			);
+
+		public override void Initialize( AnalysisContext context ) {
+
+			context.EnableConcurrentExecution();
+			context.RegisterCompilationStartAction( RegisterAnalysis );
+		}
+
+		private void RegisterAnalysis( CompilationStartAnalysisContext context ) {
+
+			Compilation compilation = context.Compilation;
+
+			context.RegisterSyntaxNodeAction(
+					AnalyzeSimpleBaseType,
+					SyntaxKind.SimpleBaseType
+				);
+		}
+
+		private void AnalyzeSimpleBaseType(
+				SyntaxNodeAnalysisContext context
+			) {
+
+			SimpleBaseTypeSyntax baseTypeSyntax = (SimpleBaseTypeSyntax)context.Node;
+			SymbolInfo baseTypeSymbol = context.SemanticModel.GetSymbolInfo( baseTypeSyntax.Type );
+
+			string baseTypeFullName = baseTypeSymbol.Symbol.ToDisplayString();
+			if( !baseTypeFullName.Equals( IFeatureFullName ) ) {
+				return;
+			}
+
+			SyntaxNode classNode = baseTypeSyntax.Parent.Parent;
+			ISymbol featureSymbol = context.SemanticModel.GetDeclaredSymbol( classNode );
+
+			string featureName = featureSymbol.ToDisplayString();
+			if( LegacyFeatureTypes.Types.Contains( featureName ) ) {
+				return;
+			}
+
+			Diagnostic diagnostic = Diagnostic.Create(
+					Diagnostics.ObsoleteLaunchDarklyFramework,
+					baseTypeSyntax.GetLocation()
+				);
+
+			context.ReportDiagnostic( diagnostic );
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ObsoleteLaunchDarklyApisAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.LaunchDarkly.ObsoleteLaunchDarklyApisAnalyzer
+
+namespace D2L.LP.LaunchDarkly.FeatureFlagging {
+	public interface IFeature { }
+}
+
+namespace D2L.Integration.ParentPortal {
+	using D2L.LP.LaunchDarkly.FeatureFlagging;
+
+	public sealed class AppDynamicsFeature : IFeature { }
+}
+
+namespace SpecTests {
+	using D2L.LP.LaunchDarkly.FeatureFlagging;
+
+	public sealed class SpyCamFeature :/* ObsoleteLaunchDarklyFramework */ IFeature /**/{ }
+
+	public struct SneakyStructFeature :/* ObsoleteLaunchDarklyFramework */ IFeature /**/{ }
+}
