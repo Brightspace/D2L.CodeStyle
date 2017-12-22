7816758d9db757e27f17d1be52fa9432d8146b52
Jeff Ashton <JeffAshton@users.noreply.github.com>
2017-12-22 17:40:44 -0500

    Jashton/obsolete i launch darkly client (#259)
    * Making ILaunchDarklyClient obsolete too
    
    * Updating the Diagnostic to one that matches the use case

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/ObsoleteLaunchDarklyApisAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ObsoleteLaunchDarklyApisAnalyzer.cs
@@ -1,5 +1,11 @@
 ﻿// analyzer: D2L.CodeStyle.Analyzers.LaunchDarkly.ObsoleteLaunchDarklyApisAnalyzer
 
+namespace D2L.LP.LaunchDarkly {
+	public interface ILaunchDarklyClient {
+		void Flush();
+	}
+}
+
 namespace D2L.LP.LaunchDarkly.FeatureFlagging {
 	public interface IFeature { }
 }
@@ -26,4 +32,26 @@ namespace MissingNamespaceUsing {
 namespace UnreleatedInterfaces {
 
 	public sealed class IncompleteFeature : System.ICloneable { }
+}
+
+namespace BannedMethods {
+	using D2L.LP.LaunchDarkly;
+
+	public sealed class NonLegacyILaunchDarklyClientConsumers {
+
+		public void Method( ILaunchDarklyClient client ) {
+			/* ObsoleteILaunchDarklyClientClient */	client.Flush();/**/
+		}
+	}
+}
+
+namespace D2L.ClassStream.FeatureFlag {
+	using D2L.LP.LaunchDarkly;
+
+	public sealed class ClassStreamFeatureToggle {
+
+		public void IsOneDrivePickerEnabled( ILaunchDarklyClient client ) {
+			client.Flush();
+		}
+	}
 }
