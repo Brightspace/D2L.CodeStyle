fc4b4823ac97bb1d381735d28a593e5236283745
Owen Smith <Owen.Smith@d2l.com>
2018-01-11 12:28:35 -0500

    Add TestAnalyzer for [TestCaseSource( "string" )]

---
+++ src/D2L.CodeStyle.TestAnalyzers/Common/AnalysisContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.TestAnalyzers.Common {
+	internal static class AnalysisContextExtensions {
+		
+		internal static void RegisterCompilationStartActionForTestProjects(
+			this AnalysisContext @this,
+			Action<CompilationStartAnalysisContext> action
+		) {
+			@this.RegisterCompilationStartAction(
+				( CompilationStartAnalysisContext context ) => {
+					var references = context.Compilation.ReferencedAssemblyNames;
+					if( !references.Any( r => r.Name.ToUpper().Contains( "NUNIT" ) ) ) {
+						// Compilation is not a test assembly, skip
+						return;
+					}
+
+					action( context );
+				}
+			);
+		}
+
+	}
+}
---
+++ src/D2L.CodeStyle.TestAnalyzers/Common/Diagnostics.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.TestAnalyzers.Common {
+	public static class Diagnostics {
+		public static readonly DiagnosticDescriptor TestCaseSourceStrings = new DiagnosticDescriptor(
+			id: "D2LTESTS001",
+			title: "Use nameof in TestCaseSource attributes.",
+			messageFormat: "String arguments in TestCaseSource not allowed. Use nameof( {0} ) instead.",
+			category: "Cleanliness",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true,
+			description: "Using strings in TestCaseSource attributes creates false positives during dead code analysis. nameof should be used instead."
+		);
+
+	}
+}
