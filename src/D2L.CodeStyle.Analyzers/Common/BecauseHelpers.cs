b78b9eb8b4f053e8789c0212b526658c89e7da57
Matt Jones <Matt.Jones@D2L.com>
2018-01-24 14:16:41 -0500

    Add helper for parsing Because out of Unaudited attribute

---
+++ src/D2L.CodeStyle.Analyzers/Common/BecauseHelpers.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace D2L.CodeStyle.Analyzers.Common {
+	internal static class BecauseHelpers {
+
+		public static bool TryGetUnauditedReason( ISymbol symbol, out string reason ) {
+
+			// If it's an error, don't bother parsing anything more
+			if( symbol is IErrorTypeSymbol ) {
+				reason = null;
+				return false;
+			}
+
+			SyntaxNode syntaxNode = Attributes.Mutability.Unaudited
+				.GetAll( symbol )
+				.FirstOrDefault()?
+				.ApplicationSyntaxReference?
+				.GetSyntax();
+
+			AttributeSyntax attrSyntax = syntaxNode as AttributeSyntax;
+
+			if( attrSyntax == null ) {
+				reason = null;
+				return false;
+			}
+
+			AttributeArgumentSyntax foundArg = attrSyntax
+				.ArgumentList
+				.Arguments
+				.FirstOrDefault( arg => {
+					// Get the first argument that is not defined by a "Name = ..." syntax and either is not named or is named "why"
+					return arg.NameEquals == null && ( arg.NameColon == null || arg.NameColon.Name.Identifier.ValueText == "why" );
+				} );
+
+			if( foundArg == null ) {
+				reason = null;
+				return false;
+			}
+
+			MemberAccessExpressionSyntax expr = foundArg.Expression as MemberAccessExpressionSyntax;
+			if( expr == null ) {
+				reason = null;
+				return false;
+			}
+
+			reason = expr.Name.Identifier.ValueText;
+			return true;
+		}
+
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Common/BecauseHelpersTests.cs
@@ -0,0 +1,163 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using static D2L.CodeStyle.Analyzers.Common.RoslynSymbolFactory;
+
+namespace D2L.CodeStyle.Analyzers.Common {
+	[TestFixture]
+	internal sealed class BecauseHelpersTests {
+
+		private const string s_preamble = @"
+using System;
+using D2L.CodeStyle.Annotations;
+using static D2L.CodeStyle.Annotations.Objects;
+ 
+namespace D2L.CodeStyle.Annotations {
+	public enum Because {
+		ItHasntBeenLookedAt,
+		ItsSketchy,
+		ItsStickyDataOhNooo,
+		WeNeedToMakeTheAnalyzerConsiderThisSafe,
+		ItsUgly,
+		ItsOnDeathRow
+	}
+
+	public enum UndiffBucket {
+		Core
+	}
+ 
+	public static partial class Mutability {
+		public sealed class UnauditedAttribute : Attribute {
+ 
+			public UnauditedAttribute( Because why ) { }
+
+			public UnauditedAttribute( Because why, UndiffBucket bucket ) { }
+		}
+	}
+}
+";
+
+		[Test]
+		public void TryGetUnauditedReason_WhenSymbolDoesNotHaveUnauditedAttribute_ReturnsFalse() {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( @"
+public class Foo {
+	public string foo;
+}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.False );
+		}
+
+		private static readonly string[] BecauseReasons = {
+			"ItHasntBeenLookedAt",
+			"ItsSketchy",
+			"ItsStickyDataOhNooo",
+			"WeNeedToMakeTheAnalyzerConsiderThisSafe",
+			"ItsUgly",
+			"ItsOnDeathRow"
+		};
+
+		[TestCaseSource( nameof( BecauseReasons ) )]
+		public void TryGetUnauditedReason_WhenSymbolIsAnnotatedWithUnauditedReason_ReturnsTrueAndCorrectReason( string expectedReason ) {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( $@"
+public class Foo {{
+	[Mutability.Unaudited( Because.{expectedReason} )]
+	public string foo;
+}}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.True );
+			Assert.That( reason, Is.EqualTo( expectedReason ) );
+		}
+
+		[TestCaseSource( nameof( BecauseReasons ) )]
+		public void TryGetUnauditedReason_WhenSymbolIsAnnotatedWithUnauditedReason_UsingNamedParameters_ReturnsTrueAndCorrectReason( string expectedReason ) {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( $@"
+public class Foo {{
+	[Mutability.Unaudited( why: Because.{expectedReason} )]
+	public string foo;
+}}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.True );
+			Assert.That( reason, Is.EqualTo( expectedReason ) );
+		}
+
+		[TestCaseSource( nameof( BecauseReasons ) )]
+		public void TryGetUnauditedReason_WhenSymbolIsAnnotatedWithUnauditedReasonAndHasBucket_ReturnsTrueAndCorrectReason( string expectedReason ) {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( $@"
+public class Foo {{
+	[Mutability.Unaudited( Because.{expectedReason}, UndiffBucket.Core )]
+	public string foo;
+}}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.True );
+			Assert.That( reason, Is.EqualTo( expectedReason ) );
+		}
+
+		[TestCaseSource( nameof( BecauseReasons ) )]
+		public void TryGetUnauditedReason_WhenSymbolIsAnnotatedWithUnauditedReasonAndHasBucket_UsingNamedParameters_ReturnsTrueAndCorrectReason( string expectedReason ) {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( $@"
+public class Foo {{
+	[Mutability.Unaudited( why: Because.{expectedReason}, bucket: UndiffBucket.Core )]
+	public string foo;
+}}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.True );
+			Assert.That( reason, Is.EqualTo( expectedReason ) );
+		}
+
+		[TestCaseSource( nameof( BecauseReasons ) )]
+		public void TryGetUnauditedReason_WhenSymbolIsAnnotatedWithUnauditedReasonAndHasBucket_UsingNamedParametersWithReversedOrder_ReturnsTrueAndCorrectReason( string expectedReason ) {
+			TestSymbol<IFieldSymbol> field = CompileAndGetFooField( $@"
+public class Foo {{
+	[Mutability.Unaudited( bucket: UndiffBucket.Core, why: Because.{expectedReason} )]
+	public string foo;
+}}
+" );
+
+			string reason;
+			bool result = BecauseHelpers.TryGetUnauditedReason( field.Symbol, out reason );
+
+			Assert.That( result, Is.True );
+			Assert.That( reason, Is.EqualTo( expectedReason ) );
+		}
+
+		private static TestSymbol<IFieldSymbol> CompileAndGetFooField( string source ) {
+			source = $"namespace D2L {{ {source} }}";
+			source = s_preamble + source;
+
+			var compilation = Compile( source );
+			var symbol = compilation.GetSymbolsWithName(
+				predicate: n => n == "Foo",
+				filter: SymbolFilter.Type
+			).OfType<ITypeSymbol>().FirstOrDefault();
+
+			Assert.IsNotNull( symbol );
+			Assert.AreNotEqual( TypeKind.Error, symbol.TypeKind );
+			TestSymbol<ITypeSymbol> fooType = new TestSymbol<ITypeSymbol>( symbol, compilation );
+
+			IFieldSymbol fooField = fooType.Symbol.GetMembers().OfType<IFieldSymbol>().FirstOrDefault( s => s.Name == "foo" );
+
+			return new TestSymbol<IFieldSymbol>( fooField, fooType.Compilation );
+		}
+
+	}
+}
