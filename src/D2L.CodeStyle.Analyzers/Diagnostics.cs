d0c3d6c3af8f4510fb70b8e690eccec6ffe307b7
Matt Jones <Matt.Jones@D2L.com>
2018-01-25 18:58:05 -0500

    Report diagnostic for invalid unaudited reasons

--- src/D2L.CodeStyle.Analyzers/Diagnostics.cs
+++ src/D2L.CodeStyle.Analyzers/Diagnostics.cs
@@ -192,5 +192,15 @@ namespace D2L.CodeStyle.Analyzers {
 			description: "Switch your feature definition to inherit D2L.LP.LaunchDarkly.FeatureDefinition, configure your feature for DI, and use one of the new IInstanceFlag/IOrgFlag/ICurrentOrgFlag interfaces instead"
 		);
 
+		public static readonly DiagnosticDescriptor InvalidUnauditedReasonInImmutable = new DiagnosticDescriptor(
+			id: "D2L0023",
+			title: "Immutability exceptions must be a subset of containing type's",
+			messageFormat: "One or more members on this type have unaudited reasons that are not excepted. Resolve the Unaudited members or relax exceptions on this type to a superset of {{ {0} }}.",
+			category:"Safety",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true,
+			description: "Type's members' allowed and used unaudited reasons must be a subset of the type's immutable exceptions."
+		);
+
 	}
 }
