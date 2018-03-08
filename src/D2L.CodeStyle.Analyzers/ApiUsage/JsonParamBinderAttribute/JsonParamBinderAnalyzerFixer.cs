fe2eff6c73190d6b3bd7391a8626fe3abcfd2ba5
David Rose <David.Rose@d2l.com>
2018-03-08 10:12:08 -0500

    Adding a code fix for ObsoleteJsonParamBinder

---
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/JsonParamBinderAttribute/JsonParamBinderAnalyzerFixer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace D2L.CodeStyle.Analyzers.ApiUsage.JsonParamBinderAttribute {
+
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(JsonParamBinderAnalyzerFixer) )]
+	public class JsonParamBinderAnalyzerFixer : CodeFixProvider {
+
+		public override FixAllProvider GetFixAllProvider() {
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		public override async Task RegisterCodeFixesAsync( CodeFixContext context ) {
+			Diagnostic diagnostic = context.Diagnostics.First();
+			TextSpan sourceSpan = diagnostic.Location.SourceSpan;
+
+			SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+
+			AttributeSyntax oldAttribute = root.FindToken(sourceSpan.Start)
+				.Parent.AncestorsAndSelf().OfType<AttributeSyntax>().First();
+
+			context.RegisterCodeFix( 
+				CodeAction.Create( 
+					Diagnostics.ObsoleteJsonParamBinder.Title.ToString(),
+					ct => SwapAttributes( context.Document, oldAttribute, ct)
+				), 
+				diagnostic
+			);
+		}
+
+		private async Task<Document> SwapAttributes( Document document, AttributeSyntax oldAttribute, CancellationToken ct ) {
+			SyntaxNode root = await document.GetSyntaxRootAsync(ct);
+
+			AttributeSyntax newAttribute = SyntaxFactory.Attribute(
+				SyntaxFactory.IdentifierName( "JsonConvertParameterBinder" ) );
+
+			SyntaxNode newRoot = root.ReplaceNode(oldAttribute, newAttribute );
+
+			return document.WithSyntaxRoot( newRoot );
+		}
+
+		public override ImmutableArray<string> FixableDiagnosticIds =>
+			ImmutableArray.Create<string>( Diagnostics.ObsoleteJsonParamBinder.Id );
+
+	}
+}
