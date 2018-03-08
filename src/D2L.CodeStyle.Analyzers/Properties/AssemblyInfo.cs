8f4f63d1a82e8c23759e2a2c5b719dfa86b01006
Jacob Parker <jacob@solidangle.ca>
2018-03-07 20:49:13 -0500

    Bump D2L.CodeStyle.Analyzers from 0.56 to 0.57

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.56.0.0" )]
-[assembly: AssemblyFileVersion( "0.56.0.0" )]
-[assembly: AssemblyInformationalVersion( "0.56.0.0" )]
+[assembly: AssemblyVersion( "0.57.0.0" )]
+[assembly: AssemblyFileVersion( "0.57.0.0" )]
+[assembly: AssemblyInformationalVersion( "0.57.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
 [assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
