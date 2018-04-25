255f42ee29aea5057ca940900d540d954f4f98de
Jacob Parker <Jacob.Parker@D2L.com>
2018-04-25 16:14:01 +0100

    Add first-pass at RequireNamedArgsAnalyzer
    Over time I'd like to expand this:
    - shrink the limit from 10 to something like 5
    - two args of the same type (or convertable?) => name them
    - literal args must be named

---
+++ src/D2L.CodeStyle.Analyzers/Language/RequireNamedArgumentsAnalyzer.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using D2L.CodeStyle.Analyzers.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.Language {
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	public sealed class RequireNamedArgumentsAnalyzer : DiagnosticAnalyzer {
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			Diagnostics.UseNamedArgsForInvocationWithLotsOfArgs
+		);
+
+		// TODO: shrink this number over time. Maybe 5 would be good?
+		public const int TOO_MANY_UNNAMED_ARGS = 10;
+
+		public override void Initialize( AnalysisContext context ) {
+			context.EnableConcurrentExecution();
+
+			context.RegisterSyntaxNodeAction(
+				AnalyzeInvocation,
+				SyntaxKind.InvocationExpression
+			);
+		}
+
+		private static void AnalyzeInvocation(
+			SyntaxNodeAnalysisContext ctx
+		) {
+			var expr = (InvocationExpressionSyntax)ctx.Node;
+
+			if ( expr.ArgumentList == null ) {
+				return;
+			}
+
+			// Don't complain about single argument functions because they're
+			// very likely to be understandable
+			if ( expr.ArgumentList.Arguments.Count <= 1 ) {
+				return;
+			}
+
+			var unnamedArgs = GetUnnamedArgs(
+				ctx.SemanticModel,
+				expr.ArgumentList
+			).ToImmutableArray();
+
+			if ( unnamedArgs.Length >= TOO_MANY_UNNAMED_ARGS ) {
+				ctx.ReportDiagnostic(
+					Diagnostic.Create(
+						descriptor: Diagnostics.UseNamedArgsForInvocationWithLotsOfArgs,
+						location: expr.GetLocation()
+					)
+				);
+			}
+
+			// TODO: literal args should always be named
+
+			// TODO: if there are duplicate typed args then they should be named
+			// These will create a bit more cleanup. Fix should probably name
+			// all the args instead to avoid craziness with overloading.
+		}
+		
+		/// <summary>
+		/// Get the arguments which are unnamed and not "params"
+		/// </summary>
+		private static IEnumerable<ArgumentSyntax> GetUnnamedArgs(
+			SemanticModel model,
+			ArgumentListSyntax args
+		) {
+			foreach( var arg in args.Arguments ) {
+				// Ignore args that are already named. This will mean that args
+				// can be partially named which is sometimes helpful (the named
+				// args have to be at the end of the list though.)
+				if ( arg.NameColon != null ) {
+					continue;
+				}
+
+				var param = arg.DetermineParameter(
+					model,
+
+					// Don't map params arguments. It's okay that they are
+					// unnamed. Some things like ImmutableArray.Create() could
+					// take a large number of args and we don't want anything to
+					// be named. Named params really suck so we may still
+					// encourage it but APIs that take params and many other
+					// args would suck anyway.
+					allowParams: false
+				);
+
+				// We presumably can't name this param anyway
+				if ( param == null ) {
+					continue;
+				}
+
+				// IParameterSymbol.Name is documented to be possibly empty in
+				// which case it is unnamed, so ignore it.
+				if ( param.Name == "" ) {
+					continue;
+				}
+
+				yield return arg;
+			}
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/RequireNamedArgumentsAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.Language.RequireNamedArgumentsAnalyzer
+
+namespace D2L {
+	public static class Foo {
+		public void _arg0() { }
+		public void _arg1( int a1 ) { }
+		public void _arg2( int a1, int a2 ) { }
+		public void _arg3( int a1, int a2, int a3 ) { }
+		public void _arg4( int a1, int a2, int a3, int a4 ) { }
+		public void _arg5( int a1, int a2, int a3, int a4, int a5 ) { }
+		public void _arg6( int a1, int a2, int a3, int a4, int a5, int a6 ) { }
+		public void _arg7( int a1, int a2, int a3, int a4, int a5, int a6, int a7 ) { }
+		public void _arg8( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8 ) { }
+		public void _arg9( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9 ) { }
+		public void _arg10( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10 ) { }
+		public void _arg11( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11 ) { }
+
+		public static void Test() {
+			_arg0();
+			_arg1( 1 );
+			_arg2( 1, 2 );
+			_arg3( 1, 2, 3 );
+			_arg4( 1, 2, 3, 4 );
+			_arg5( 1, 2, 3, 4, 5 );
+			_arg6( 1, 2, 3, 4, 5, 6 );
+			_arg7( 1, 2, 3, 4, 5, 6, 7 );
+			_arg8( 1, 2, 3, 4, 5, 6, 7, 8 );
+			_arg9( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
+			/* UseNamedArgsForInvocationWithLotsOfArgs */ _arg10( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ) /**/;
+			/* UseNamedArgsForInvocationWithLotsOfArgs */ _arg11( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ) /**/;
+
+			// when there are lots of arguments we'd rather just see
+			_arg10(
+				a1: 1,
+				a2: 2,
+				a3: 3,
+				a4: 4,
+				a5: 5,
+				a6: 6,
+				a7: 7,
+				a8: 8,
+				a9: 9,
+				a10: 10
+			);
+
+			// begruddingly allowed (may make more sense when the limit isn't 10)
+			_arg10( 1, 2, 3, 4, 5, 6, 7, 8, 9, a10: 10 );
+			_arg11( 1, 2, 3, 4, 5, 6, 7, 8, 9, a10: 10, a11: 11 );
+
+			/* UseNamedArgsForInvocationWithLotsOfArgs */ _arg11( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, a11: 11 ) /**/;
+		}
+	}
+}
