448a528aefac5adf10dffdd861975a6340106269
Jacob Parker <jacob@solidangle.ca>
2018-04-26 11:12:46 -0400

    Bump version from 0.62 to 0.63

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.62.0.0" )]
-[assembly: AssemblyFileVersion( "0.62.0.0" )]
-[assembly: AssemblyInformationalVersion( "0.62.0.0" )]
+[assembly: AssemblyVersion( "0.63.0.0" )]
+[assembly: AssemblyFileVersion( "0.63.0.0" )]
+[assembly: AssemblyInformationalVersion( "0.63.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
 [assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
