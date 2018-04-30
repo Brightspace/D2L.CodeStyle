b814a595beea6d44b5e6164aa659abcf88aa992a
Owen Smith <Owen.Smith@d2l.com>
2018-04-30 15:37:36 -0400

    spec test with type conversions (and currently failing)

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/LikelyArgumentMismatchAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/LikelyArgumentMismatchAnalyzer.cs
@@ -11,5 +11,52 @@ namespace D2L.CodeStyle.Analyzers.Specs {
 			);
 		}
 
+
+		struct IdTypeA {
+
+			private readonly long m_value;
+
+			public IdTypeA( long m_value ) {
+				m_value = m_value;
+			}
+
+			public implicit operator long( IdTypeA id ) {
+				return id.m_value;
+			}
+
+			public implicit operator IdTypeA( long value ) {
+				return new IdTypeA( value );
+			}
+		}
+
+		struct IdTypeB {
+
+			private readonly long m_value;
+
+			public IdTypeB( long m_value ) {
+				m_value = m_value;
+			}
+
+			public implicit operator long( IdTypeB id ) {
+				return id.m_value;
+			}
+
+			public implicit operator IdTypeB( long value ) {
+				return new IdTypeB( value );
+			}
+		}
+
+		void Foo( IdTypeA orgId, IdTypeB userId, string foo ) {
+			long orgIdLong = 1234;
+			long userIdLong = 5678;
+
+			Foo( orgIdLong, userIdLong );
+			Foo(
+				/* LikelyArgumentMismatch(userIdLong,userId,orgId) */ userIdLong /**/,
+				/* LikelyArgumentMismatch(orgIdLong,orgId,userId) */ orgIdLong /**/,
+				foo
+			);
+		}
+
 	}
 }
