4b0f38cbc795ae13fc6880dfbc1219086bfa0c1c
Jacob Parker <jacob@solidangle.ca>
2018-05-03 11:43:23 -0400

    Fix merge conflict mistake

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/RequireNamedArgumentsAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/RequireNamedArgumentsAnalyzer.cs
@@ -105,7 +105,7 @@ namespace D2L {
 			new SomeClass();
 			new SomeClass( 1 );
 			new SomeClass( 1, 2 );
-			/* UseNamedArgsWhenTooManyArgs */ new SomeClass( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ) /**/;
+			/* TooManyUnnamedArgs */ new SomeClass( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ) /**/;
 			new SomeClass( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, a31:31 );
 
 			#endregion
