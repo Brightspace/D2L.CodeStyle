3783090a15808360f82665327d71435e39aac774
Ciprian Bortos <Ciprian.Bortos@D2L.com>
2018-05-08 10:21:26 -0400

    added basic analyzer for Assert.IsTrue/IsFalse ... only handles binary expressions for now

---
+++ src/D2L.CodeStyle.TestAnalyzers/NUnit/AssertIsBool/AssertIsBoolDiagnosticProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace D2L.CodeStyle.TestAnalyzers.NUnit.AssertIsBool {
+
+	internal sealed class AssertIsBoolDiagnosticProvider<T> where T : ExpressionSyntax {
+
+		private readonly Func<T, string> m_isTrueDiagnostic;
+		private readonly Func<T, string> m_isFalseDiagnostic;
+
+		public AssertIsBoolDiagnosticProvider(
+			Func<T, string> isTrueDiagnostic,
+			Func<T, string> isFalseDiagnostic
+		) {
+			m_isTrueDiagnostic = isTrueDiagnostic;
+			m_isFalseDiagnostic = isFalseDiagnostic;
+		}
+
+		public AssertIsBoolDiagnosticProvider<T> Opposite() =>
+			new AssertIsBoolDiagnosticProvider<T>( m_isFalseDiagnostic, m_isTrueDiagnostic );
+
+		public Func<T, string> GetDiagnosticFunc( string symbolName ) {
+			Func<T, string> getDiagnosticFunc;
+			switch( symbolName ) {
+				case AssertIsBoolAnalyzer.AssertIsTrue:
+					getDiagnosticFunc = m_isTrueDiagnostic;
+					break;
+				case AssertIsBoolAnalyzer.AssertIsFalse:
+					getDiagnosticFunc = m_isFalseDiagnostic;
+					break;
+				default:
+					throw new InvalidOperationException( $"unknown '{symbolName}' symbol" );
+			}
+
+			return getDiagnosticFunc;
+		}
+	}
+}
