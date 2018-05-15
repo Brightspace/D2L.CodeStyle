e93f615f431e9dba467f8ef576de5e5f529a528e
Ciprian Bortos <Ciprian.Bortos@D2L.com>
2018-05-15 14:18:54 -0400

    added MapPath dangerous methods

--- src/D2L.CodeStyle.Analyzers/ApiUsage/DangerousMethodUsages/DangerousMethods.cs
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/DangerousMethodUsages/DangerousMethods.cs
@@ -2,6 +2,10 @@
 using System.Collections.Immutable;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Hosting;
+using System.Web.UI;
 
 namespace D2L.CodeStyle.Analyzers.ApiUsage.DangerousMethodUsages {
 	internal static class DangerousMethods {
@@ -16,7 +20,48 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage.DangerousMethodUsages {
 			)
 			.Add<Task>(
 				nameof( Task.Run )
-			);
+			)
+			.AddMapPathMethods();
+
+		private static ImmutableDictionary<string, ImmutableArray<string>> AddMapPathMethods( 
+				this ImmutableDictionary<string, ImmutableArray<string>> types 
+			) {
+
+			return types
+				.Add<HttpServerUtility>(
+					nameof( HttpServerUtility.MapPath ) 
+				)
+				.Add<HttpServerUtilityBase>(
+					nameof( HttpServerUtilityBase.MapPath )
+				)
+				.Add<HttpServerUtilityWrapper>(
+					nameof( HttpServerUtilityWrapper.MapPath )
+				)
+				.Add<HttpRequest>(
+					nameof( HttpRequest.MapPath )
+				)
+				.Add<HttpRequestBase>(
+					nameof( HttpRequestBase.MapPath )
+				)
+				.Add<HttpRequestWrapper>(
+					nameof( HttpRequestWrapper.MapPath )
+				)
+				.Add<HttpWorkerRequest>(
+					nameof( HttpWorkerRequest.MapPath )
+				)
+				.Add<HostingEnvironment>(
+					nameof( HostingEnvironment.MapPath )
+				)
+				.Add<UserMapPath>(
+					nameof( UserMapPath.MapPath )
+				)
+				.Add<Page>(
+					nameof( Page.MapPath )
+				)
+				.Add<UserControl>(
+					nameof( UserControl.MapPath )
+				);
+		}
 
 		private static ImmutableDictionary<string, ImmutableArray<string>> Add<T>(
 				this ImmutableDictionary<string, ImmutableArray<string>> types,
