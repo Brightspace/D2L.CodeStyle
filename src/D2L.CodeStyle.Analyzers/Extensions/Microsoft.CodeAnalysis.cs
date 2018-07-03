64d03d6bcf6043c88ddec6d432e69d0583d1c6d1
Todd Lang <Todd.Lang@D2L.com>
2018-07-03 14:25:22 -0400

    Moving immutable container extension methods to their own file.

--- src/D2L.CodeStyle.Analyzers/Extensions/Microsoft.CodeAnalysis.cs
+++ src/D2L.CodeStyle.Analyzers/Extensions/Microsoft.CodeAnalysis.cs
@@ -18,45 +18,6 @@ namespace D2L.CodeStyle.Analyzers.Extensions {
 			"System.IO.Abstractions.IFileSystem"
 		);
 
-		/// <summary>
-		/// A list of immutable container types (i.e., types that hold other types)
-		/// </summary>
-		private static readonly ImmutableDictionary<string, string[]> ImmutableContainerTypes = new Dictionary<string, string[]> {
-			["D2L.LP.Utilities.DeferredInitializer"] = new[] { "Value" },
-			["D2L.LP.Extensibility.Activation.Domain.IPlugins"] = new[] { "[]" },
-			["System.Collections.Immutable.IImmutableSet"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableArray"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableDictionary"] = new[] { "[].Key", "[].Value" },
-			["System.Collections.Immutable.ImmutableHashSet"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableList"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableQueue"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableSortedDictionary"] = new[] { "[].Key", "[].Value" },
-			["System.Collections.Immutable.ImmutableSortedSet"] = new[] { "[]" },
-			["System.Collections.Immutable.ImmutableStack"] = new[] { "[]" },
-			["System.Collections.Generic.IReadOnlyCollection"] = new[] { "[]" },
-			["System.Collections.Generic.IReadOnlyList"] = new[] { "[]" },
-			["System.Collections.Generic.IReadOnlyDictionary"] = new[] { "[].Key", "[].Value" },
-			["System.Collections.Generic.IEnumerable"] = new[] { "[]" },
-			["System.Lazy"] = new[] { "Value" },
-			["System.Nullable"] = new[] { "Value" },
-			["System.Tuple"] = new[] { "Item1", "Item2", "Item3", "Item4", "Item5", "Item6" }
-		}.ToImmutableDictionary();
-
-		public static bool IsAnImmutableContainerType( this ITypeSymbol type ) {
-			return ImmutableContainerTypes.ContainsKey( type.GetFullTypeName() );
-		}
-
-		public static string[] GetImmutableContainerTypePrefixes( this ITypeSymbol type ) {
-			string fullTypeName = type.GetFullTypeName();
-
-			if( ImmutableContainerTypes.ContainsKey( fullTypeName ) ) {
-				return ImmutableContainerTypes[fullTypeName];
-			}
-
-			// Not found, so return an empty array
-			return new string[] { };
-		}
-
 		public static ImmutabilityScope GetImmutabilityScope( this ITypeSymbol type ) {
 			if( type.IsTypeMarkedImmutable() ) {
 				return ImmutabilityScope.SelfAndChildren;
