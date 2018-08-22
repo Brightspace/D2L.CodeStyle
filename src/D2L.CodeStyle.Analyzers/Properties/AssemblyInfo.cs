f026414ff94e19b4509bae0fa2d7192ae35fd199
Todd Lang <todd.lang@d2l.com>
2018-08-22 10:38:20 -0400

    Update AssemblyInfo.cs (#409)

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.77.0.0" )]
-[assembly: AssemblyFileVersion( "0.77.0.0" )]
-[assembly: AssemblyInformationalVersion( "0.77.0.0" )]
+[assembly: AssemblyVersion( "0.78.0.0" )]
+[assembly: AssemblyFileVersion( "0.78.0.0" )]
+[assembly: AssemblyInformationalVersion( "0.78.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
 [assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
