735fbfd14aa560459d950d6f4f4e8fd70cade61b
Jacob Parker <jacob@solidangle.ca>
2018-09-06 17:16:10 -0400

    Bumping version from 0.79 to 0.80

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible( false )]
 
-[assembly: AssemblyVersion( "0.79.0.0" )]
-[assembly: AssemblyFileVersion( "0.79.0.0" )]
-[assembly: AssemblyInformationalVersion( "0.79.0.0" )]
+[assembly: AssemblyVersion( "0.80.0.0" )]
+[assembly: AssemblyFileVersion( "0.80.0.0" )]
+[assembly: AssemblyInformationalVersion( "0.80.0.0" )]
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
 [assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
