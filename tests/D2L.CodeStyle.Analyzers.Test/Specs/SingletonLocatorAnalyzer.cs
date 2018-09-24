03ae9de7e962850c62cba2ccdd633f17fef36c45
Ciprian Bortos <Ciprian.Bortos@D2L.com>
2018-09-24 11:01:23 -0400

    handle the both IPlugins interfaces (with and without the extension point)

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/SingletonLocatorAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/SingletonLocatorAnalyzer.cs
@@ -15,6 +15,9 @@ namespace D2L.LP.Extensibility.Activation.Domain {
 	public sealed class SingletonAttribute : Attribute { }
 
 	public interface IPlugins<out T> : System.Collections.Generic.IEnumerable<T> { }
+	public interface IPlugins<TExtensionPoint, out T> : System.Collections.Generic.IEnumerable<T> { }
+
+	public interface IPlugins<TExtensionPoint, TOther, out T> : System.Collections.Generic.IEnumerable<T> { }
 }
 
 namespace SingletonSpecTests {
@@ -24,6 +27,10 @@ namespace SingletonSpecTests {
 	public interface INotMarkedSingleton {
 		internal void SomeOtherMethod() { }
 	}
+
+	public interface ISomeExtensionPoint { }
+
+	public interface IInterface { }
 
 	public sealed class BadClass {
 
@@ -63,8 +70,24 @@ namespace SingletonSpecTests {
 			INotMarkedSingleton problem = /* SingletonLocatorMisuse(SingletonSpecTests.INotMarkedSingleton) */ SingletonLocator.Get<IPlugins<INotMarkedSingleton>>() /**/;
 		}
 
+		public void UsesSingletonLocatorUnmarkedPluginsForExtensionPoint() {
+			INotMarkedSingleton problem = /* SingletonLocatorMisuse(SingletonSpecTests.INotMarkedSingleton) */ SingletonLocator.Get<IPlugins< ISomeExtensionPoint, INotMarkedSingleton >>() /**/;
+		}
+
+		public void UsesSingletonLocatorUnmarkedUnknownPlugins() {
+			INotMarkedSingleton problem = /* SingletonLocatorMisuse(D2L.LP.Extensibility.Activation.Domain.IPlugins) */ SingletonLocator.Get<IPlugins<ISomeExtensionPoint, IInterface, INotMarkedSingleton>>() /**/;
+		}
+
 		public void UsesSingletonLocatorMarkedPlugins() {
 			IMarkedSingleton ok = SingletonLocator.Get<IPlugins<IMarkedSingleton>>();
+		}
+
+		public void UsesSingletonLocatorMarkedPluginsForExtensionPoint() {
+			IMarkedSingleton ok = SingletonLocator.Get<IPlugins<ISomeExtensionPoint, IMarkedSingleton>>();
+		}
+
+		public void UsesSingletonLocatorMarkedUnknownPlugins() {
+			IMarkedSingleton problem = /* SingletonLocatorMisuse(D2L.LP.Extensibility.Activation.Domain.IPlugins) */ SingletonLocator.Get<IPlugins<ISomeExtensionPoint, IInterface, IMarkedSingleton>>() /**/;
 		}
 	}
 }
