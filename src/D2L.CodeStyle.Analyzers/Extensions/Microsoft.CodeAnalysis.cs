881f80fe719a73748ed5354fe9aaa175a10015a6
Jeff Ashton <Jeff.Ashton@D2L.com>
2018-11-06 10:29:59 -0500

    Making event types immutable

--- src/D2L.CodeStyle.Analyzers/Extensions/Microsoft.CodeAnalysis.cs
+++ src/D2L.CodeStyle.Analyzers/Extensions/Microsoft.CodeAnalysis.cs
@@ -270,5 +270,15 @@ namespace D2L.CodeStyle.Analyzers.Extensions {
 
 			return -1;
 		}
+
+		public static bool TryGetTypeByMetadataName(
+				this Compilation compilation,
+				string fullyQualifiedMetadataName,
+				out INamedTypeSymbol type
+			) {
+
+			type = compilation.GetTypeByMetadataName( fullyQualifiedMetadataName );
+			return ( type != null );
+		}
 	}
 }
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/EventTypesAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿
+// analyzer: D2L.CodeStyle.Analyzers.ApiUsage.Events.EventTypesAnalyzer
+
+namespace D2L.CodeStyle.Annotations {
+
+	using System;
+
+	public static class Objects {
+		public sealed class ImmutableAttribute : Attribute {}
+	}
+}
+
+namespace D2L.LP.Distributed.Events.Domain {
+
+	using System;
+
+	[AttributeUsage( validOn: AttributeTargets.Class, AllowMultiple = false, Inherited = false )]
+	public sealed class EventAttribute : Attribute { }
+}
+
+namespace Tests {
+
+	using System;
+	using D2L.LP.Distributed.Events.Domain;
+	using static D2L.CodeStyle.Annotations.Objects;
+
+	[Event]
+	public sealed class/* EventTypeMissingImmutableAttribute(Tests.MutableEvent) */ MutableEvent /**/{ }
+
+	[Event]
+	[Immutable]
+	public sealed class ImmutableEvent { }
+
+	[System.Diagnostics.Tracing.Event]
+	public sealed class WrongEventAttribute { }
+
+	[Wacky.Event]
+	public sealed class InvalidAttributeType { }
+}
