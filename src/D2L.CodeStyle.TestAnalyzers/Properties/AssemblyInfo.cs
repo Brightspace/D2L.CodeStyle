7e1c9b523582346c1add2b41eea2602028318dc8
Owen Smith <Owen.Smith@d2l.com>
2018-12-03 20:14:15 -0500

    testanalyzers: newstyle csproj. target 4.7.2

--- src/D2L.CodeStyle.TestAnalyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.TestAnalyzers/Properties/AssemblyInfo.cs
@@ -1,16 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle( "D2L.CodeStyle.TestAnalyzers" )]
-[assembly: AssemblyDescription( "D2L.CodeStyle test analyzers" )]
-[assembly: AssemblyCompany( "D2L" )]
-[assembly: AssemblyProduct( "D2L.CodeStyle.TestAnalyzers" )]
-[assembly: AssemblyCopyright( "Copyright © D2L 2016" )]
-
-[assembly: ComVisible( false )]
-
-[assembly: AssemblyVersion( "0.11.0.0" )]
-[assembly: AssemblyFileVersion( "0.11.0.0" )]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.TestAnalyzers.Tests" )]
