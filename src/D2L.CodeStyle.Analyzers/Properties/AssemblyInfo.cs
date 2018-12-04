02f9fa75fd25fa6006299fefc43e6df40910cc74
Owen Smith <Owen.Smith@d2l.com>
2018-12-03 19:50:02 -0500

    analyzers: newstyle csproj. target 4.7.2

--- src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
+++ src/D2L.CodeStyle.Analyzers/Properties/AssemblyInfo.cs
@@ -1,18 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle( "D2L.CodeStyle.Analyzers" )]
-[assembly: AssemblyDescription( "D2L.CodeStyle analyzers" )]
-[assembly: AssemblyCompany( "D2L" )]
-[assembly: AssemblyProduct( "D2L.CodeStyle.Analyzers" )]
-[assembly: AssemblyCopyright( "Copyright © D2L 2018" )]
-
-[assembly: ComVisible( false )]
-
-[assembly: AssemblyVersion( "0.92.0.0" )]
-[assembly: AssemblyFileVersion( "0.92.0.0" )]
-[assembly: AssemblyInformationalVersion( "0.92.0.0" )]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo( "D2L.CodeStyle.Analyzers.Tests" )]
-[assembly: InternalsVisibleTo( "DynamicProxyGenAssembly2" )]
