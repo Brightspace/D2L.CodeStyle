e48b9fc97c02ae8657a5cf4871d828aea53dc227
Jim Walkoski <Jim.Walkoski@D2L.com>
2019-04-10 16:43:23 -0400

    Reduce TOO_MANY_UNNAMED_ARGS to 7 (#475)

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/RequireNamedArgumentsAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/RequireNamedArgumentsAnalyzer.cs
@@ -14,23 +14,23 @@ namespace D2L {
 		public static void _arg2( int a1, int a2 ) { }
 		public static void _arg3( int a1, int a2, int a3 ) { }
 		public static void _arg4( int a1, int a2, int a3, int a4 ) { }
+		public static void _arg6( int a1, int a2, int a3, int a4, int a5, int a6 ) { }
 
 		// These will shrink as we shrink the max *blush*
+		public static void _arg7( int a1, int a2, int a3, int a4, int a5, int a6, int a7 ) { }
 		public static void _arg8( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8 ) { }
-		public static void _arg9( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9 ) { }
-		public static void _arg10( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10 ) { }
 
 		public static void funcWithParams( int a, int b, int c, params int[] ps ) { }
 
 		public delegate void delegate0Args();
 		public delegate void delegate1Args( int a1 );
-		public delegate void delegate9Args( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9 );
+		public delegate void delegate7Args( int a1, int a2, int a3, int a4, int a5, int a6, int a7);
 
 		public sealed class SomeClass {
 			public SomeClass() { }
 			public SomeClass( int a1 ) { }
 			public SomeClass( int a1, int a2 ) { }
-			public SomeClass( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9 ) { }
+			public SomeClass( int a1, int a2, int a3, int a4, int a5, int a6, int a7 ) { }
 		}
 
 		public static void Test() {
@@ -40,16 +40,16 @@ namespace D2L {
 			_arg2( 1, 2 );
 			_arg3( 1, 2, 3 );
 			_arg4( 1, 2, 3, 4 );
-			_arg8( 1, 2, 3, 4, 5, 6, 7, 8 );
+			_arg6( 1, 2, 3, 4, 5, 6 );
 			#endregion
 
 			#region diagnostic for too many unnamed args
-			/* TooManyUnnamedArgs */ _arg9( 1, 2, 3, 4, 5, 6, 7, 8, 9 ) /**/;
-			/* TooManyUnnamedArgs */ _arg10( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ) /**/;
+			/* TooManyUnnamedArgs */ _arg7( 1, 2, 3, 4, 5, 6, 7 ) /**/;
+			/* TooManyUnnamedArgs */ _arg8( 1, 2, 3, 4, 5, 6, 7, 8 ) /**/;
 			#endregion
 
 			#region all named args is usually preferred if there are lots of args
-			_arg9(
+			_arg7(
 				a1: 1,
 				a2: 2,
 				a3: 3,
@@ -57,36 +57,34 @@ namespace D2L {
 				a5: 5,
 				a6: 6,
 				a7: 7,
-				a8: 8,
-				a9: 9
 			);
 			#endregion
 
 			#region named args don't count against the unnamed args budget
-			_arg9( 1, 2, 3, 4, 5, 6, 7, 8, a9: 9 );
-			_arg10( 1, 2, 3, 4, 5, 6, 7, 8, a9: 9, a10: 10 );
+			_arg7( a1: 1, 2, 3, 4, 5, 6, 7 );
+			_arg8( a1: 1, a2: 2, 3, 4, 5, 6, 7, 8 );
 			#endregion
 
 			#region arguments that are literals with the correct name don't count against the budget
-			int a6 = 16;
-			int a9 = 20;
-			int A10 = 10; // upper case doesn't matter
-			_arg9( 1, 2, 3, 4, 5, a6, 7, 8, 9 );
-			_arg9( 1, 2, 3, 4, 5, 6, 7, 8, a9 );
-			_arg10( 1, 2, 3, 4, 5, 6, 7, 8, a9, A10 );
+			int a1 = 11;
+			int a3 = 13;
+			int A1 = 101; // upper case doesn't matter
+			_arg7( a1, 2, 3, 4, 5, 6, 7 );
+			_arg7( 1, 2, a3, 4, 5, 6, 7 );
+			_arg8( A1, 2, a3, 4, 5, 6, 7, 8 );
 			#endregion
 
 			#region member accesses can also serve as psuedo-names
 			var thing = new Thing();
-			_arg9( 1, 2, 3, 4, 5, thing.a6, 7, 8, 9 );
-			_arg9( 1, 2, 3, 4, 5, thing.nested.a6, 7, 8, 9 );
-			_arg9( 1, 2, 3, 4, 5, thing.m_a6, 7, 8, 9 );
-			_arg9( 1, 2, 3, 4, 5, thing.nested._a6, 7, 8, 9 );
+			_arg7( 1, 2, 3, 4, 5, thing.a6, 7 );
+			_arg7( 1, 2, 3, 4, 5, thing.nested.a6, 7 );
+			_arg7( 1, 2, 3, 4, 5, thing.m_a6, 7 );
+			_arg7( 1, 2, 3, 4, 5, thing.nested._a6, 7 );
 			#endregion
 
 			#region need to have enough named args, though
-			/* TooManyUnnamedArgs */ _arg10( 1, 2, 3, 4, 5, 6, 7, 8, 9, a10: 10 ) /**/;
-			/* TooManyUnnamedArgs */ _arg10( 1, a2: 2, 3, 4, 5, 6, 7, 8, 9, 10 ) /**/;
+			/* TooManyUnnamedArgs */ _arg8( a1: 1, 2, 3, 4, 5, 6, 7, 8 ) /**/;
+			/* TooManyUnnamedArgs */ _arg8( 1, a2: 2, 3, 4, 5, 6, 7, 8 ) /**/;
 			#endregion
 
 			#region params don't count against the unnamed args budget
@@ -98,8 +96,8 @@ namespace D2L {
 			#region delegates
 			((delegate0Args)null)();
 			((delegate1Args)null)( 1 );
-			/* TooManyUnnamedArgs */ ((delegate9Args)null)( 1, 2, 3, 4, 5, 6, 7, 8, 9 ) /**/;
-			((delegate9Args)null)( 1, 2, 3, 4, 5, 6, 7, 8, a9: 9 );
+			/* TooManyUnnamedArgs */ ((delegate7Args)null)( 1, 2, 3, 4, 5, 6, 7 ) /**/;
+			((delegate7Args)null)( a1: 1, 2, 3, 4, 5, 6, 7 );
 			#endregion
 
 			#region class constructors should behave the same way
@@ -108,8 +106,8 @@ namespace D2L {
 			new SomeClass();
 			new SomeClass( 1 );
 			new SomeClass( 1, 2 );
-			/* TooManyUnnamedArgs */ new SomeClass( 1, 2, 3, 4, 5, 6, 7, 8, 9 ) /**/;
-			new SomeClass( 1, 2, 3, 4, 5, 6, 7, 8, a9: 9 );
+			/* TooManyUnnamedArgs */ new SomeClass( 1, 2, 3, 4, 5, 6, 7 ) /**/;
+			new SomeClass( a1: 1, 2, 3, 4, 5, 6, 7 );
 			#endregion
 		}
 	}
