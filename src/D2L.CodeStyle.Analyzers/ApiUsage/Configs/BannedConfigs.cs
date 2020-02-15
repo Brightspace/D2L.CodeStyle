4018285611be332934d142de2d7b8909cd29a23e
Owen Smith <owen@omsmith.ca>
2020-02-15 13:03:46 -0500

    BannedConfigs: fixup comparer of commented future code (#537)

--- src/D2L.CodeStyle.Analyzers/ApiUsage/Configs/BannedConfigs.cs
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/Configs/BannedConfigs.cs
@@ -8,7 +8,7 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage.Configs {
 
 		internal static readonly IReadOnlyDictionary<string, IReadOnlyDictionary<string, string>> Definitions = ImmutableDictionary
 			.Create<string, IReadOnlyDictionary<string, string>>()
-			// .Add( "GetInstance", ImmutableDictionary.Create<string, string>() )
+			// .Add( "GetInstance", ImmutableDictionary.Create<string, string>( StringComparer.OrdinalIgnoreCase ) )
 			.Add( "GetOrg", ImmutableDictionary.Create<string, string>( StringComparer.OrdinalIgnoreCase )
 				.Add( "d2l.Settings.WebServerName", "WebServerName is being moved to Hiera data. Use IUrlFormatter or IWebServerNameProvider instead." )
 				.Add( "d2l.System.Aws.Region", "Use IOrgAwsRegionProvider instead." )
