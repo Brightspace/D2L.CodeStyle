42d014ca11cf280938ef788e6ccc940d18d99259
Owen Smith <owen@omsmith.ca>
2020-02-15 22:34:29 -0500

    Keep TestServiceLocatorFactory whitelist clean (#539)
    * D2LTESTS007: Error if a whitelist entry is unnecessary
    
    * Add assembly names to TestServiceLocatorFactory whitelist
    * Emit D2lTESTS007 for TestServiceLocatorFactory whitelist entries

--- src/D2L.CodeStyle.TestAnalyzers/ServiceLocator/CustomTestServiceLocatorAnalyzer.cs
+++ src/D2L.CodeStyle.TestAnalyzers/ServiceLocator/CustomTestServiceLocatorAnalyzer.cs
@@ -19,7 +19,7 @@ namespace D2L.CodeStyle.TestAnalyzers.ServiceLocator {
 		private const string WhitelistFileName = "CustomTestServiceLocatorWhitelist.txt";
 
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-			=> ImmutableArray.Create( Diagnostics.CustomServiceLocator );
+			=> ImmutableArray.Create( Diagnostics.CustomServiceLocator, Diagnostics.UnnecessaryWhitelistEntry );
 
 		private readonly bool _excludeKnownProblems;
 
@@ -58,6 +58,15 @@ namespace D2L.CodeStyle.TestAnalyzers.ServiceLocator {
 				),
 				SyntaxKind.InvocationExpression
 			);
+
+			context.RegisterSymbolAction(
+				ctx => PreventUnnecessaryWhitelisting(
+					ctx,
+					factoryType,
+					whitelistedClasses
+				),
+				SymbolKind.NamedType
+			);
 		}
 
 		// Prevent static usage of TestServiceLocator.Create() methods.
@@ -66,45 +75,15 @@ namespace D2L.CodeStyle.TestAnalyzers.ServiceLocator {
 			INamedTypeSymbol disallowedType,
 			ImmutableHashSet<string> whitelistedClasses
 		) {
-			ExpressionSyntax root = context.Node as InvocationExpressionSyntax;
-			if( root == null ) {
+			if( !( context.Node is InvocationExpressionSyntax invocationExpression ) ) {
 				return;
 			}
 
-			SymbolInfo symbolInfo = context.SemanticModel.GetSymbolInfo( root );
-
-			IMethodSymbol method = symbolInfo.Symbol as IMethodSymbol;
-
-			if( method == null ) {
-				if( symbolInfo.CandidateSymbols == null ) {
-					return;
-				}
-
-				if( symbolInfo.CandidateSymbols.Length != 1 ) {
-					return;
-				}
-
-				// This happens on method groups, such as
-				// Func<IServiceLocator> fooFunc = TestServiceLocatorFactory.Create( ... );
-				method = symbolInfo.CandidateSymbols.First() as IMethodSymbol;
-
-				if( method == null ) {
-					return;
-				}
-			}
-
-			// If we're a Create method on a class that isn't
-			// TestServiceLocatorFactory, we're safe.
-			if( !IsTestServiceLocatorFactory(
-					actualType: method.ContainingType,
-					disallowedType: disallowedType
-				)
-			) {
-				return;
-			}
-
-			// If we're not a Create method, we're safe.
-			if( !IsCreateMethod( method ) ) {
+			if( !IsTestServiceLocatorFactoryCreate(
+				context.SemanticModel,
+				disallowedType,
+				invocationExpression
+			) ) {
 				return;
 			}
 
@@ -134,6 +113,97 @@ namespace D2L.CodeStyle.TestAnalyzers.ServiceLocator {
 			);
 		}
 
+		private void PreventUnnecessaryWhitelisting(
+			SymbolAnalysisContext context,
+			INamedTypeSymbol factoryType,
+			ImmutableHashSet<string> whitelistedClasses
+		) {
+			if( !( context.Symbol is INamedTypeSymbol namedType ) ) {
+				return;
+			}
+
+			if( !IsClassWhitelisted( whitelistedClasses, namedType ) ) {
+				return;
+			}
+
+			Location diagnosticLocation = null;
+			foreach( var syntaxRef in namedType.DeclaringSyntaxReferences ) {
+				var syntax = syntaxRef.GetSyntax( context.CancellationToken );
+
+				diagnosticLocation = diagnosticLocation ?? syntax.GetLocation();
+
+				SemanticModel model = context.Compilation.GetSemanticModel( syntax.SyntaxTree );
+
+				var testServiceLocatorFactoryCreates = syntax
+					.DescendantNodes()
+					.OfType<InvocationExpressionSyntax>()
+					.Where( i => IsTestServiceLocatorFactoryCreate(
+						model,
+						factoryType,
+						i
+					) );
+
+				if( testServiceLocatorFactoryCreates.Any() ) {
+					return;
+				}
+			}
+
+			if( diagnosticLocation != null ) {
+				context.ReportDiagnostic(
+					Diagnostic.Create(
+						Diagnostics.UnnecessaryWhitelistEntry,
+						diagnosticLocation,
+						GetWhitelistName( namedType ), WhitelistFileName
+					)
+				);
+			}
+		}
+
+		private static bool IsTestServiceLocatorFactoryCreate(
+			SemanticModel model,
+			INamedTypeSymbol factoryType,
+			InvocationExpressionSyntax invocationExpression
+		) {
+			SymbolInfo symbolInfo = model.GetSymbolInfo( invocationExpression );
+
+			IMethodSymbol method = symbolInfo.Symbol as IMethodSymbol;
+
+			if( method == null ) {
+				if( symbolInfo.CandidateSymbols == null ) {
+					return false;
+				}
+
+				if( symbolInfo.CandidateSymbols.Length != 1 ) {
+					return false;
+				}
+
+				// This happens on method groups, such as
+				// Func<IServiceLocator> fooFunc = TestServiceLocatorFactory.Create( ... );
+				method = symbolInfo.CandidateSymbols.First() as IMethodSymbol;
+
+				if( method == null ) {
+					return false;
+				}
+			}
+
+			// If we're a Create method on a class that isn't
+			// TestServiceLocatorFactory, we're safe.
+			if( !IsTestServiceLocatorFactory(
+					actualType: method.ContainingType,
+					disallowedType: factoryType
+				)
+			) {
+				return false;
+			}
+
+			// If we're not a Create method, we're safe.
+			if( !IsCreateMethod( method ) ) {
+				return false;
+			}
+
+			return true;
+		}
+
 		private static bool IsTestServiceLocatorFactory(
 			INamedTypeSymbol actualType,
 			INamedTypeSymbol disallowedType
@@ -158,10 +228,16 @@ namespace D2L.CodeStyle.TestAnalyzers.ServiceLocator {
 			ISymbol classSymbol
 		) {
 			bool isWhiteListed = _excludeKnownProblems
-				&& whitelistedClasses.Contains( classSymbol.ToString() );
+				&& whitelistedClasses.Contains( GetWhitelistName( classSymbol ) );
 
 			return isWhiteListed;
 		}
+
+		private static string GetWhitelistName( ISymbol classSymbol ) =>
+			classSymbol.ToString()
+			+ ", "
+			+ classSymbol.ContainingAssembly.ToDisplayString( SymbolDisplayFormat.MinimallyQualifiedFormat )
+		;
 
 		private ImmutableHashSet<string> GetWhitelist(
 			ImmutableArray<AdditionalText> additionalFiles
