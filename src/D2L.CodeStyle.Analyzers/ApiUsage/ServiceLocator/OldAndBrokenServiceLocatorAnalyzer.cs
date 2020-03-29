58686ab1b265f5ebda79147b2fc19f986ded9d9d
Owen Smith <owen@omsmith.ca>
2020-03-29 11:55:13 -0400

    OldAndBrokenServiceLocatorAnalyzer: switch to in-repo, type-based whitelist using WhitelistHelper

--- src/D2L.CodeStyle.Analyzers/ApiUsage/ServiceLocator/OldAndBrokenServiceLocatorAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/ServiceLocator/OldAndBrokenServiceLocatorAnalyzer.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections.Immutable;
 using System.Linq;
+using D2L.CodeStyle.Analyzers.Helpers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace D2L.CodeStyle.Analyzers.ApiUsage.ServiceLocator {
 	[DiagnosticAnalyzer( LanguageNames.CSharp )]
 	public sealed class OldAndBrokenServiceLocatorAnalyzer : DiagnosticAnalyzer {
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-			=> ImmutableArray.Create( Diagnostics.OldAndBrokenLocatorIsObsolete );
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			Diagnostics.OldAndBrokenLocatorIsObsolete,
+			Diagnostics.UnnecessaryWhitelistEntry
+		);
+
+		private readonly WhitelistHelper<INamedTypeSymbol> m_whitelistHelper = WhitelistHelper.TypeLevel(
+			"OldAndBrokenServiceLocatorWhitelist.txt"
+		);
 
 		private readonly bool _excludeKnownProblems;
 
-		public OldAndBrokenServiceLocatorAnalyzer() : this(true) { }
+		public OldAndBrokenServiceLocatorAnalyzer() : this( true ) { }
 
-		public OldAndBrokenServiceLocatorAnalyzer( bool excludeKnownProblemDlls ) {
+		public OldAndBrokenServiceLocatorAnalyzer(
+			bool excludeKnownProblemDlls
+		) {
 			_excludeKnownProblems = excludeKnownProblemDlls;
 		}
 
@@ -41,120 +52,124 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage.ServiceLocator {
 				return;
 			}
 
-			if( IsAssemblyWhitelisted( context.Compilation.AssemblyName ) ) {
-				return;
-			}
+			ImmutableArray<INamedTypeSymbol> disallowedTypes = ImmutableArray.Create(
+				locatorType,
+				factoryType,
+				activatorType,
+				customActivatorType
+			);
+
+			Func<INamedTypeSymbol, bool> classIsWhitelisted = m_whitelistHelper.LoadWhitelist( context.Options );
 
 			//Prevent static usage of OldAndBrokenServiceLocator
 			//For example, OldAndBrokenServiceLocator.Instance.Get<IFoo>()
 			context.RegisterSyntaxNodeAction(
 				ctx => PreventOldAndBrokenUsage(
 					ctx,
-					new List<INamedTypeSymbol> { locatorType, factoryType, activatorType, customActivatorType }
+					disallowedTypes,
+					classIsWhitelisted
 				),
 				SyntaxKind.IdentifierName
+			);
+
+			context.RegisterSymbolAction(
+				ctx => PreventUnnecessaryWhitelisting(
+					ctx,
+					disallowedTypes,
+					classIsWhitelisted
+				),
+				SymbolKind.NamedType
 			);
 		}
 
 		//Prevent static usage of OldAndBrokenServiceLocator
 		//For example, OldAndBrokenServiceLocator.Instance.Get<IFoo>()
-		private static void PreventOldAndBrokenUsage(
+		private void PreventOldAndBrokenUsage(
 			SyntaxNodeAnalysisContext context,
-			List<INamedTypeSymbol> disallowedTypes
+			ImmutableArray<INamedTypeSymbol> disallowedTypes,
+			Func<INamedTypeSymbol, bool> classIsWhitelisted
 		) {
-			var actualType = context.SemanticModel.GetTypeInfo( context.Node ).Type as INamedTypeSymbol;
-			
-			if ( actualType == null ) {
+			if( !( context.Node is IdentifierNameSyntax syntax ) ) {
 				return;
 			}
 
-			if( !disallowedTypes.Contains( actualType ) ) {
+			if( !IdentifierIsOfDisallowedType( context.SemanticModel, disallowedTypes, syntax ) ) {
 				return;
 			}
 
-			var parentClasses = context.Node.Ancestors().Where( a => a.IsKind( SyntaxKind.ClassDeclaration ) );
-			var parentSymbols = parentClasses.Select( c => context.SemanticModel.GetDeclaredSymbol( c ) );
+			var parentClasses = context.Node.Ancestors().OfType<TypeDeclarationSyntax>();
+			var parentSymbols = parentClasses.Select( c => context.SemanticModel.GetDeclaredSymbol( c ) ).ToImmutableArray();
+
 			if( parentSymbols.Any( s => Attributes.DIFramework.IsDefined( s ) ) ) {
 				//Classes in the DI Framework are allowed to use locators and activators
 				return;
 			}
 
+			if( _excludeKnownProblems && parentSymbols.Any( classIsWhitelisted ) ) {
+				return;
+			}
+
 			context.ReportDiagnostic(
-				Diagnostic.Create( Diagnostics.OldAndBrokenLocatorIsObsolete, context.Node.GetLocation() )
+				Diagnostic.Create( Diagnostics.OldAndBrokenLocatorIsObsolete, syntax.GetLocation() )
 			);
 		}
 
+		private void PreventUnnecessaryWhitelisting(
+			SymbolAnalysisContext context,
+			ImmutableArray<INamedTypeSymbol> disallowedTypes,
+			Func<INamedTypeSymbol, bool> classIsWhitelisted
+		) {
+			if( !( context.Symbol is INamedTypeSymbol namedType ) ) {
+				return;
+			}
 
-		/// <summary>
-		/// A list of assemblies that already contain OldAndBrokenServiceLocator references
-		/// </summary>
-		private static readonly ImmutableHashSet<string> WhitelistedAssemblies = new HashSet<string> {
-			"D2L",
-			"D2L.Core.JobManagement",
-			"D2L.Core.Metadata",
-			"D2L.Core.ReleaseConditions",
-			"D2L.Core.Workflow",
-			"D2L.Custom.BannerBatchCrosslist",
-			"D2L.Custom.ContentLicenseCheck",
-			"D2L.Custom.ExternalHomepageResolver",
-			"D2L.Custom.IPASDelimitedUserMapping",
-			"D2L.Custom.IPASUserCreateUpdate",
-			"D2L.Custom.ParticipationReport",
-			"D2L.Custom.PDGG.Core",
-			"D2L.Custom.SessionCourseCopy",
-			"D2L.Custom.SpecialAccessAPIs",
-			"D2L.Custom.StudentOrientationSIS.Task",
-			"D2L.Custom.UserActivityRFReport",
-			"D2L.Custom.UserSyncTool",
-			"D2L.IM.GradesExport.Banner",
-			"D2L.IM.GradesExport.Domain",
-			"D2L.IM.GradesExport.Service",
-			"D2L.IM.IPSCT",
-			"D2L.IM.IPSCT.SyncService",
-			"D2L.IM.IPSCT.WebEx",
-			"D2L.IM.IPSIS.Default",
-			"D2L.IM.IPSIS.LIS",
-			"D2L.IM.IPSIS.Security",
-			"D2L.IM.Platform.Data",
-			"D2L.IM.Platform.DI",
-			"D2L.IM.Platform.Enrollments",
-			"D2L.IM.Platform.OrgUnits",
-			"D2L.IM.Platform.Users",
-			"D2L.IM.Platform.Web.MVC",
-			"D2L.LE.Authorization",
-			"D2L.LE.Competencies",
-			"D2L.LE.Conversion.ClientDataConverters.BB",
-			"D2L.LE.Discussions",
-			"D2L.LE.Grades",
-			"D2L.LE.Lti",
-			"D2L.Lms.AuthorizationSchemes.CommonCartridge",
-			"D2L.Lms.Competencies.Web",
-			"D2L.Lms.Content.Implementation",
-			"D2L.Lms.CourseExport",
-			"D2L.Lms.Crosslistings.Implementation",
-			"D2L.Lms.Discussions.Implementation",
-			"D2L.Lms.Domain",
-			"D2L.Lms.Domain.DataAccess",
-			"D2L.Lms.Dropbox",
-			"D2L.Lms.Dropbox.Integration",
-			"D2L.Lms.Dropbox.Web",
-			"D2L.Lms.Question",
-			"D2L.Lms.Quizzing.Implementation",
-			"D2L.LP",
-			"D2L.LP.AppLoader",
-			"D2L.LP.Diagnostics.Web",
-			"D2L.LP.Services.Framework",
-			"D2L.LP.Tools",
-			"D2L.LP.Tools.Extensibility",
-			"D2L.LP.Web",
-			"D2L.LP.Web.UI",
-			"D2L.PlatformTools.DataPurgeArchive",
-			"D2L.PlatformTools.ErrorPages",
-			"D2L.Web"
-		}.ToImmutableHashSet();
+			if( !classIsWhitelisted( namedType ) ) {
+				return;
+			}
 
-		private bool IsAssemblyWhitelisted( string assemblyName ) {
-			return _excludeKnownProblems && WhitelistedAssemblies.Contains( assemblyName );
+			Location diagnosticLocation = null;
+			foreach( var syntaxRef in namedType.DeclaringSyntaxReferences ) {
+				var typeSyntax = syntaxRef.GetSyntax( context.CancellationToken ) as TypeDeclarationSyntax;
+
+				diagnosticLocation = diagnosticLocation ?? typeSyntax.Identifier.GetLocation();
+
+				SemanticModel model = context.Compilation.GetSemanticModel( typeSyntax.SyntaxTree );
+
+				bool usesDisallowedTypes = typeSyntax
+					.DescendantNodes()
+					.OfType<IdentifierNameSyntax>()
+					.Any( syntax => IdentifierIsOfDisallowedType( model, disallowedTypes, syntax ) );
+
+				if( usesDisallowedTypes ) {
+					return;
+				}
+			}
+
+			if( diagnosticLocation != null ) {
+				m_whitelistHelper.EntryIsUnnecessary(
+					entry: namedType,
+					location: diagnosticLocation,
+					report: context.ReportDiagnostic
+				);
+			}
+		}
+
+		private static bool IdentifierIsOfDisallowedType(
+			SemanticModel model,
+			ImmutableArray<INamedTypeSymbol> disallowedTypes,
+			IdentifierNameSyntax syntax
+		) {
+			var actualType = model.GetTypeInfo( syntax ).Type as INamedTypeSymbol;
+
+			if( actualType == null ) {
+				return false;
+			}
+
+			if( !disallowedTypes.Contains( actualType ) ) {
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
