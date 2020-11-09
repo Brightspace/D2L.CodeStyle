fe74c97376b42dd940184ec05dfd4efc53e49b43
Jacob Parker <jacob@solidangle.ca>
2020-11-09 10:22:00 -0500

    Add support for querying if a type is known to be immutable.
    This organizes the logic to ask "do we know if this type is immutable?".
    This is a different question than "is it OK for this declaration to have
    [Immutable]?", or "does this declaration need to have [Immutable]?".
    
    This is missing support for "generic container types" like
    ImmutableArray<T>, where the type is conditionally immutable.

---
+++ src/D2L.CodeStyle.Analyzers/Immutability/ImmutableTypeKind.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace D2L.CodeStyle.Analyzers.Immutability {
+	/// <summary>
+	/// Our "immutability" is a property of values. However, we can also
+	/// generalize this to types as a whole to varying degrees.
+	/// </summary>
+	[Flags]
+	internal enum ImmutableTypeKind {
+		/// <summary>
+		/// We have nothing to say about the immutability of this type.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// A type T has immutable instances if "new T( ... )" always produces
+		/// an immutable value.
+		/// </summary>
+		Instance = 1,
+
+
+		/// <summary>
+		/// A type T is totally immutable if all objects of type T are
+		/// immutable. This is a stronger condition than instance immutability.
+		/// </summary>
+		Total = 2 | Instance,
+	}
+}
