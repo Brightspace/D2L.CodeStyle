fb6de9567f0fa02771559a437ea7f7d3455a50bd
Jacob Parker <jacob@solidangle.ca>
2020-11-19 13:05:11 -0500

    Some prep work for analyzing static members (#625)

---
+++ src/D2L.CodeStyle.Analyzers/Immutability/MutabilityAuditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.Analyzers.Immutability {
+	internal sealed class MutabilityAuditor {
+		public static bool IsAudited( ISymbol symbol, out Location location ) {
+			AttributeData attr;
+
+			if( symbol.IsStatic ) {
+				attr = Attributes.Statics.Audited.GetAll( symbol ).FirstOrDefault()
+				    ?? Attributes.Statics.Unaudited.GetAll( symbol ).FirstOrDefault();
+			} else {
+				attr = Attributes.Mutability.Audited.GetAll( symbol ).FirstOrDefault()
+				    ?? Attributes.Mutability.Unaudited.GetAll( symbol ).FirstOrDefault();
+			}
+
+			if( attr != null ) {
+				location = GetLocation( attr );
+				return true;
+			}
+
+			location = null;
+			return false;
+		}
+
+		private static Location GetLocation( AttributeData attr )
+			=> attr.ApplicationSyntaxReference.GetSyntax().GetLocation();
+	}
+}
