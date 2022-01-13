a4be7fb704c9a3d9b89a712b6701821303e0b823
Jeff Ashton <JeffAshton@users.noreply.github.com>
2022-01-13 10:23:01 -0500

    Adding analyzer to ensure InterfaceBinder<T> is only used with interfaces (#776)

---
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/DataRecordConverters/InterfaceBinderAnalyzer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Immutable;
+using D2L.CodeStyle.Analyzers.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using static D2L.CodeStyle.Analyzers.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.ApiUsage.DataRecordConverters {
+
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	internal sealed class InterfaceBinderAnalyzer : DiagnosticAnalyzer {
+
+		private const string InterfaceBinderFullName = "D2L.LP.LayeredArch.Data.InterfaceBinder`1";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			InterfaceBinder_InterfacesOnly
+		);
+
+		public override void Initialize( AnalysisContext context ) {
+			context.EnableConcurrentExecution();
+			context.ConfigureGeneratedCodeAnalysis( GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics );
+			context.RegisterCompilationStartAction( CompilationStart );
+		}
+
+		private void CompilationStart( CompilationStartAnalysisContext context ) {
+
+			Compilation comp = context.Compilation;
+			if( !comp.TryGetTypeByMetadataName( InterfaceBinderFullName, out INamedTypeSymbol interfaceBinderType ) ) {
+				return;
+			}
+
+			context.RegisterSyntaxNodeAction(
+					c => AnalyzeGenericName( c, interfaceBinderType, (GenericNameSyntax)c.Node ),
+					SyntaxKind.GenericName
+				);
+		}
+
+		private void AnalyzeGenericName(
+				SyntaxNodeAnalysisContext context,
+				INamedTypeSymbol interfaceBinderType,
+				GenericNameSyntax genericName
+			) {
+
+			TypeArgumentListSyntax argumentList = genericName.TypeArgumentList;
+			if( argumentList.Arguments.Count != 1 ) {
+				return;
+			}
+
+			ITypeSymbol genericType = context.SemanticModel
+				.GetTypeInfo( genericName, context.CancellationToken )
+				.Type;
+
+			if( genericType == null ) {
+				return;
+			}
+
+			if( !SymbolEqualityComparer.Default.Equals( genericType.OriginalDefinition, interfaceBinderType ) ) {
+				return;
+			}
+
+			TypeSyntax interfaceArgument = argumentList.Arguments[ 0 ];
+
+			ITypeSymbol interfaceType = context.SemanticModel
+				.GetTypeInfo( interfaceArgument, context.CancellationToken )
+				.Type;
+
+			if( interfaceType == null ) {
+				return;
+			}
+
+			if( interfaceType.TypeKind == TypeKind.Interface ) {
+				return;
+			}
+
+			Diagnostic diagnostic = Diagnostic.Create(
+					InterfaceBinder_InterfacesOnly,
+					interfaceArgument.GetLocation(),
+					messageArgs: interfaceType.Name
+				);
+
+			context.ReportDiagnostic( diagnostic );
+		}
+	}
+}
