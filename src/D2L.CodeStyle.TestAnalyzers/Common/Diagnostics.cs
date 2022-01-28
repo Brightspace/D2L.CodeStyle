92ab68c8e1f5136a44dc710fdc95ce549b9686b3
Matt Jones <Matt.Jones@D2L.com>
2022-01-28 14:53:36 -0500

    Remove test analyzers covered by nunit.analyzers (1st pass)
    This removed the D2LTESTS001 and D2LTESTS002 diagnostics and their associated analyzers/codefixes. These are covered by the `nunit.analyzers`[^0] package we are bringing in instead.
    
    [^0]: https://github.com/nunit/nunit.analyzers

--- src/D2L.CodeStyle.TestAnalyzers/Common/Diagnostics.cs
+++ src/D2L.CodeStyle.TestAnalyzers/Common/Diagnostics.cs
@@ -2,25 +2,10 @@
 
 namespace D2L.CodeStyle.TestAnalyzers.Common {
 	public static class Diagnostics {
-		public static readonly DiagnosticDescriptor TestCaseSourceStrings = new DiagnosticDescriptor(
-			id: "D2LTESTS001",
-			title: "Use nameof in TestCaseSource attributes.",
-			messageFormat: "String arguments in TestCaseSource not allowed. Use nameof( {0} ) instead.",
-			category: "Cleanliness",
-			defaultSeverity: DiagnosticSeverity.Error,
-			isEnabledByDefault: true,
-			description: "Using strings in TestCaseSource attributes creates false positives during dead code analysis. nameof should be used instead."
-		);
 
-		public static readonly DiagnosticDescriptor ValueSourceStrings = new DiagnosticDescriptor(
-			id: "D2LTESTS002",
-			title: "Use nameof in ValueSource attributes.",
-			messageFormat: "String arguments in ValueSource not allowed. Use nameof( {0} ) instead.",
-			category: "Cleanliness",
-			defaultSeverity: DiagnosticSeverity.Error,
-			isEnabledByDefault: true,
-			description: "Using strings in ValueSource attributes creates false positives during dead code analysis. nameof should be used instead."
-		);
+		// Retired:
+		// D2LTESTS001 (TestCaseSourceStrings): "Use nameof in TestCaseSource attributes"
+		// D2LTESTS002 (ValueSourceStrings): "Use nameof in ValueSource attributes."
 
 		public static readonly DiagnosticDescriptor ConfigTestSetupStrings = new DiagnosticDescriptor(
 			id: "D2LTESTS003",
