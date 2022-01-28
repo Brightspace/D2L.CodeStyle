726010a7eed8114004a322fc2ecedd34633c70dd
Jacob Parker <jacob@solidangle.ca>
2022-01-28 13:32:11 -0500

    Add SourceFilePathHintNameGenerator

---
+++ tests/D2L.CodeStyle.Analyzers.Test/Async/Generator/SourceFilePathHintNameGeneratorTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Immutable;
+using NUnit.Framework;
+
+namespace D2L.CodeStyle.Analyzers.Async.Generator;
+
+[TestFixture]
+internal class SourceFilePathHintNameGeneratorTests {
+	[Test]
+	public void Empty() {
+		var gen = SourceFilePathHintNameGenerator.Create(
+			ImmutableArray<string>.Empty
+		);
+
+		CollectionAssert.IsEmpty( gen.GetHintNames() );
+	}
+
+	[Test]
+	public void Single() {
+		var gen = SourceFilePathHintNameGenerator.Create(
+			ImmutableArray.Create(
+				@"C:\foo\bar.cs"
+			)
+		);
+
+		CollectionAssert.AreEquivalent(
+			ImmutableArray.Create(
+				"bar"
+			),
+			gen.GetHintNames()
+		);
+	}
+
+	[Test]
+	public void Two() {
+		var gen = SourceFilePathHintNameGenerator.Create(
+			ImmutableArray.Create(
+				@"C:\foo\bar.cs",
+				@"C:\foo\baz.cs"
+			)
+		);
+
+		CollectionAssert.AreEquivalent(
+			ImmutableArray.Create(
+				"bar",
+				"baz"
+			),
+			gen.GetHintNames()
+		);
+	}
+
+	[Test]
+	public void Three() {
+		var gen = SourceFilePathHintNameGenerator.Create(
+			ImmutableArray.Create(
+				//subdir
+				@"C:\foo\sub\bar.cs",
+				@"C:\foo\sub\baz.cs",
+
+				@"C:\foo\quux.cs"
+			)
+		);
+
+		CollectionAssert.AreEquivalent(
+			ImmutableArray.Create(
+				"sub_bar",
+				"sub_baz",
+				"quux"
+			),
+			gen.GetHintNames()
+		);
+	}
+
+	[Test]
+	public void ThreeAmbiguous() {
+		var gen = SourceFilePathHintNameGenerator.Create(
+			ImmutableArray.Create(
+				//subdir
+				@"C:\foo\sub\bar.cs",
+				@"C:\foo\sub\baz.cs",
+
+				@"C:\foo\sub_bar.cs"
+			)
+		);
+
+		CollectionAssert.AreEquivalent(
+			ImmutableArray.Create(
+				"sub_bar",
+				"sub_baz",
+				"sub_bar0"
+			),
+			gen.GetHintNames()
+		);
+	}
+}
