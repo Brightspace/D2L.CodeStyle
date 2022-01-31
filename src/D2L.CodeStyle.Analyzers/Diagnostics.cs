0166bc43a85b6494e5d904f81033e0c5127c54d2
Jacob Parker <jacob@solidangle.ca>
2022-01-31 14:05:02 -0500

    Add SyntaxTransformer base class (#785)

--- src/D2L.CodeStyle.Analyzers/Diagnostics.cs
+++ src/D2L.CodeStyle.Analyzers/Diagnostics.cs
@@ -735,5 +735,14 @@ namespace D2L.CodeStyle.Analyzers {
 			defaultSeverity: DiagnosticSeverity.Info, // Only used for the suggested edit
 			isEnabledByDefault: true
 		);
+
+		public static readonly DiagnosticDescriptor GenericGeneratorError = new DiagnosticDescriptor(
+			id: "D2L0095",
+			title: "A source generator encountered an unexpected situation. This is a bug in D2L.CodeStyle.Analyzers.",
+			messageFormat: "The source generator {0} encountered an unexpected situation: {1}",
+			category: "Generators",
+			defaultSeverity: DiagnosticSeverity.Error,
+			isEnabledByDefault: true
+		);
 	}
 }
