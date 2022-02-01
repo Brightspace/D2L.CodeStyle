9abde2cae0b8844be5254593ac45df3dc9198111
Jeff Ashton <Jeff.Ashton@D2L.com>
2022-02-01 09:13:48 -0500

    Cleanup using

--- src/D2L.CodeStyle.Analyzers/Language/AttributeAliasesAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/Language/AttributeAliasesAnalyzer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using D2L.CodeStyle.Analyzers.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
