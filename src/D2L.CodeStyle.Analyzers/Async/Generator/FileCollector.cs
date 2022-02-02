8a9cd7e8140095d8c9640a242c506ecafd5f388a
Jacob Parker <jacob@solidangle.ca>
2022-02-02 17:52:57 -0500

    Add FileCollector (for the async generator)

---
+++ src/D2L.CodeStyle.Analyzers/Async/Generator/FileCollector.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace D2L.CodeStyle.Analyzers.Async.Generator;
+
+internal partial class SyncGenerator {
+	/// <summary>
+	/// Creates a source file containing generated methods and the necessary
+	/// namespaces, using directives, and type declarations for them to live in
+	/// (but only the necessary ones).
+	/// </summary>
+	internal sealed class FileCollector {
+		private static readonly SyntaxToken NoneToken
+			= SyntaxFactory.Token( SyntaxKind.None );
+
+		private static readonly SyntaxTriviaList EmptyTrivia
+			= SyntaxFactory.TriviaList();
+
+		private readonly CompilationUnitSyntax m_root;
+
+		// This is a mutable dictionary because we will remove from it as we go
+		// and check that it is empty at the end, just to be safe.
+		private readonly Dictionary<SyntaxNode, IEnumerable<string>> m_methods;
+
+		// When we recurse into a namespace or type, we're not sure if the
+		// preamble ("namespace foo {", "\t\tpartial class Bar<T> {", etc.) is
+		// necessary, because maybe there are no generated methods.
+		// Using a LinkedList<string> as a double-ended queue.
+		private readonly LinkedList<string> m_preambles = new();
+
+		private readonly StringBuilder m_out = new();
+
+		private FileCollector(
+			CompilationUnitSyntax root,
+			Dictionary<SyntaxNode, IEnumerable<string>> methods
+		) {
+			m_root = root;
+			m_methods = methods;
+		}
+
+		public static FileCollector Create(
+			CompilationUnitSyntax root,
+			ImmutableArray<(SyntaxNode Parent, string Source)> methods
+		) {
+			var groupedMethods = methods
+				.GroupBy( static m => m.Parent )
+				.ToDictionary(
+					static g => g.Key,
+					static g => g.Select( static m => m.Source )
+				);
+
+			return new FileCollector( root, groupedMethods );
+		}
+
+		public string CollectSource() {
+			// File-scoped usings:
+			m_out.Append( m_root.Usings.ToFullString() );
+
+			WriteChildren( m_root );
+
+			if( m_preambles.Count != 0 ) {
+				throw new Bug( "left over preambles" );
+			}
+
+			if( m_methods.Count != 0 ) {
+				throw new Bug( "left over methods" );
+			}
+
+			return m_out.ToString();
+		}
+
+		private bool WriteChildren( SyntaxNode node ) {
+			// Early-out if we have no methods left to place.
+			if( m_methods.Count == 0 ) {
+				return false;
+			}
+
+			var preambleCountBefore = m_preambles.Count;
+
+			bool wroteSomething = false;
+
+			// First recurse into child types
+			foreach( var child in node.ChildNodes() ) {
+				bool wroteSomethingHere = child switch {
+					ClassDeclarationSyntax @class => WriteType( @class ),
+					StructDeclarationSyntax @struct => WriteType( @struct ),
+					InterfaceDeclarationSyntax @interface => WriteType( @interface ),
+
+					// Covers record + record struct:
+					RecordDeclarationSyntax @record => WriteType( @record, @record.ClassOrStructKeyword ),
+
+					// Covers file-scoped and block namespaces
+					BaseNamespaceDeclarationSyntax @namespace => WriteNamespace( @namespace ),
+
+					// Ignore any other child node
+					_ => false
+				};
+
+				if( wroteSomethingHere ) {
+					wroteSomething = true;
+				}
+
+				CheckPremableInvariant( wroteSomething, preambleCountBefore );
+			}
+
+			return wroteSomething;
+		}
+
+		private bool WriteNamespace( BaseNamespaceDeclarationSyntax @namespace ) {
+			SyntaxToken? closeBrace;
+
+			switch( @namespace ) {
+				case FileScopedNamespaceDeclarationSyntax ns:
+					m_preambles.AddLast(
+						ns.NamespaceKeyword.ToFullString()
+						+ ns.Name.ToFullString()
+						+ ns.SemicolonToken.ToFullString()
+					);
+
+					closeBrace = null;
+					break;
+				case NamespaceDeclarationSyntax ns:
+					m_preambles.AddLast(
+						ns.NamespaceKeyword.ToFullString()
+						+ ns.Name.ToFullString()
+						+ ns.OpenBraceToken.ToFullString()
+						+ ns.Usings.ToFullString()
+					);
+
+					closeBrace = ns.CloseBraceToken;
+					break;
+				default:
+					throw new Bug( "unexpected namespace style: " + @namespace.Kind() );
+			}
+
+			bool wroteSomething = WriteChildren( @namespace );
+
+			CloseBraceIfNecessary( wroteSomething, closeBrace );
+
+			return wroteSomething;
+		}
+
+		private bool WriteType( TypeDeclarationSyntax decl )
+			=> WriteType( decl, NoneToken );
+
+		private bool WriteType(
+			TypeDeclarationSyntax decl,
+			SyntaxToken secondaryKeyword
+		) {
+			var preambleCountBefore = m_preambles.Count;
+
+			// This should either get used inside a call to WriteMethods either
+			// somewhere down the call to WriteChildren here or the direct call
+			// to WriteMethods here, or it will be discarded inside
+			// CloseBraceIfNecessary.
+			AddPartialTypeToPreamble(
+				decl,
+				keyword1: decl.Keyword,
+				// for "record struct" etc.
+				keyword2: secondaryKeyword,
+				identifier: decl.Identifier,
+				typeParameterList: decl.TypeParameterList,
+				constraintClauses: decl.ConstraintClauses,
+				openBrace: decl.OpenBraceToken
+			);
+
+			var wroteSomethingInChild = WriteChildren( decl );
+			var wroteMethodsForMe = WriteMethods( decl );
+
+			var wroteSomething = wroteSomethingInChild || wroteMethodsForMe;
+
+			CloseBraceIfNecessary( wroteSomething, decl.CloseBraceToken );
+
+			CheckPremableInvariant( wroteSomething, preambleCountBefore );
+
+			return wroteSomething;
+		}
+
+		private void AddPartialTypeToPreamble(
+			SyntaxNode decl,
+			SyntaxToken keyword1,
+			SyntaxToken keyword2,
+			SyntaxToken identifier,
+			TypeParameterListSyntax? typeParameterList,
+			SyntaxList<TypeParameterConstraintClauseSyntax> constraintClauses,
+			SyntaxToken openBrace
+		) {
+			// Create the partial keyword and grab all leading trivia from the node
+			var partialKeyword = CreatePartialKeyword( decl );
+
+			// Clear any leading trivia from the keyword, if it exists.
+			// Only the first token on the line should have leading trivia, and
+			// we've moved that onto partialKeyword.
+			// This could get ugly if someone did
+			//
+			//   sealed class
+			//      Foo {
+			keyword1 = keyword1.WithLeadingTrivia( EmptyTrivia );
+			keyword2 = keyword2.WithLeadingTrivia( EmptyTrivia );
+			identifier = identifier.WithLeadingTrivia( EmptyTrivia );
+
+			m_preambles.AddLast(
+				partialKeyword.ToFullString()
+				+ keyword1.ToFullString()
+				+ keyword2.ToFullString()
+				+ identifier.ToFullString()
+				+ (typeParameterList?.ToFullString() ?? "")
+				+ constraintClauses.ToFullString()
+				+ openBrace.ToFullString()
+			);
+		}
+
+		private static SyntaxToken CreatePartialKeyword( SyntaxNode decl )
+			=> SyntaxFactory.Token( SyntaxKind.PartialKeyword )
+				.WithLeadingTrivia( decl.GetLeadingTrivia() )
+				.WithTrailingTrivia( SyntaxFactory.Space );
+
+		private void CloseBraceIfNecessary( bool wroteSomething, SyntaxToken? closeBrace ) {
+			if( wroteSomething ) {
+				if( closeBrace.HasValue ) {
+					m_out.Append( closeBrace.Value.ToFullString() );
+				}
+			} else {
+				// Throw away the start of the decl that we never wrote
+				m_preambles.RemoveLast();
+			}
+		}
+
+		private bool WriteMethods( SyntaxNode parent ) {
+			if( !m_methods.TryGetValue( parent, out var methods ) ) {
+				return false;
+			}
+
+			m_methods.Remove( parent );
+
+			// Flush any preambles in the queue
+			while( m_preambles.Count != 0 ) {
+				m_out.Append( m_preambles.First() );
+				m_preambles.RemoveFirst();
+			}
+
+			foreach( var method in methods ) {
+				m_out.Append( method );
+			}
+
+			return true;
+		}
+
+		// Inline assertions to catch some obvious bugs quickly
+		private void CheckPremableInvariant( bool wroteSomething, int countBefore ) {
+			if( wroteSomething && m_preambles.Count != 0 ) {
+				throw new Bug( "Left over preambles" );
+			}
+
+			if( !wroteSomething && m_preambles.Count != countBefore ) {
+				throw new Bug( "Incorrect number of preambles" );
+			}
+		}
+
+		public sealed class Bug : Exception {
+			private readonly string m_message;
+
+			public Bug( string message ) {
+				m_message = message;
+			}
+
+			public override string Message => $"Bug in {nameof( SyncGenerator )}: {m_message}";
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Async/Generator/FileCollectorTests.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using static D2L.CodeStyle.Analyzers.Async.Generator.SyncGenerator;
+
+namespace D2L.CodeStyle.Analyzers.Tests.Async.Generator;
+
+[TestFixture]
+internal class FileCollectorTests {
+	[Test]
+	public void Empty() {
+		var root = CSharpSyntaxTree.ParseText( @"
+namespace X;
+
+public sealed class Y {
+	void SomeMethod() {}
+}" ).GetCompilationUnitRoot();
+
+		var collector = FileCollector.Create(
+			root,
+			ImmutableArray<(SyntaxNode, string)>.Empty 
+		);
+
+		Assert.AreEqual( "", collector.CollectSource() );
+	}
+
+	[Test]
+	public void Basic() {
+		var root = CSharpSyntaxTree.ParseText( @"
+using Foo;
+
+namespace X;
+
+public sealed class Y {
+	internal class Z {}
+
+	void MyMethodBefore() {
+		Console.WriteLine( ""Hello"" );
+	}
+}" ).GetCompilationUnitRoot();
+
+		SyntaxNode myMethodBefore = root.DescendantNodes().OfType<MethodDeclarationSyntax>().Single();
+
+		var collector = FileCollector.Create(
+			root,
+			ImmutableArray.Create(
+				(myMethodBefore.Parent, "\tany text\r\n")
+			)
+		);
+
+		Assert.AreEqual( @"
+using Foo;
+
+namespace X;
+
+partial class Y {
+	any text
+}",
+			collector.CollectSource()
+		);
+	}
+
+	[TestCase( "class" )] // static/selaed come before partial and don't need to show up in the other partials
+	[TestCase( "struct" )] // ditto for readonly
+	[TestCase( "record" )]
+	[TestCase( "interface" )]
+	[TestCase( "record class" )]
+	[TestCase( "record struct" )]
+	public void AllTypes( string kind ) {
+		var root = CSharpSyntaxTree.ParseText( @$"
+public partial {kind} X {{
+	void MyMethodBefore() {{
+		Console.WriteLine( ""Hello"" );
+	}}
+}}" ).GetCompilationUnitRoot();
+
+		SyntaxNode myMethodBefore = root.DescendantNodes().OfType<MethodDeclarationSyntax>().Single();
+
+		var collector = FileCollector.Create(
+			root,
+			ImmutableArray.Create(
+				(myMethodBefore.Parent, "\tany text\r\n")
+			)
+		);
+
+		Assert.AreEqual( @$"
+partial {kind} X {{
+	any text
+}}",
+			collector.CollectSource()
+		);
+	}
+
+	[Test]
+	public void Nesting() {
+		var root = CSharpSyntaxTree.ParseText( @"
+using Foo;
+
+namespace A.B.C {
+	namespace X {
+		public sealed partial class Y {
+			struct Ignored1 {}
+
+			internal partial class Z {
+				void MyMethodBefore() {
+					Console.WriteLine( ""Hello"" );
+				}
+			}
+
+			class Ignored2 {}
+		}
+	}
+}" ).GetCompilationUnitRoot();
+
+		SyntaxNode myMethodBefore = root.DescendantNodes().OfType<MethodDeclarationSyntax>().Single();
+
+		var collector = FileCollector.Create(
+			root,
+			ImmutableArray.Create(
+				(myMethodBefore.Parent, "\t\t\t\tany text\r\n")
+			)
+		);
+
+		Assert.AreEqual( @"
+using Foo;
+
+namespace A.B.C {
+	namespace X {
+		partial class Y {
+
+			partial class Z {
+				any text
+			}
+		}
+	}
+}",
+			collector.CollectSource()
+		);
+	}
+
+	[Test]
+	public void Complicated() {
+		// A mix of nesting and unused things with multiple methods
+
+		var root = CSharpSyntaxTree.ParseText( @"
+using Foo;
+
+namespace A.B.C {
+	using Bar;
+
+	namespace X {
+		using Quux;
+		using System;
+
+		interface Ignored {}
+
+		public sealed partial class Y {
+			struct Ignored1 {}
+
+			internal partial class Z {
+				void MyMethodBefore() {
+					Console.WriteLine( ""Hello"" );
+				}
+
+				void MyMethodBefore2() {
+					Console.WriteLine( ""Bye"" );
+				}
+			}
+
+			class Ignored2 {}
+
+			private static partial class W {
+				void MyBeforeMethod3() {}
+			}
+		}
+	}
+}
+
+namespace Ignored {
+	namespace Ignored {
+		class Ignored {}
+	}
+}
+
+namespace Q {
+	public partial record struct ZZZ {
+		public static partial class WWW {
+			private partial interface IWhat {
+				void MyBeforeMethod4() { }
+			}
+		}
+	}
+}
+" ).GetCompilationUnitRoot();
+
+		var myMethodsBefore = root.DescendantNodes().OfType<MethodDeclarationSyntax>()
+			.Select( ( node, idx ) => (node.Parent, $"\t\t\t\tany text{idx}\r\n") );
+
+		var collector = FileCollector.Create(
+			root,
+			myMethodsBefore.ToImmutableArray()
+		);
+
+		Assert.AreEqual( @"
+using Foo;
+
+namespace A.B.C {
+	using Bar;
+
+	namespace X {
+		using Quux;
+		using System;
+
+		partial class Y {
+
+			partial class Z {
+				any text0
+				any text1
+			}
+
+			partial class W {
+				any text2
+			}
+		}
+	}
+}
+
+namespace Q {
+	partial record struct ZZZ {
+		partial class WWW {
+			partial interface IWhat {
+				any text3
+			}
+		}
+	}
+}
+",
+			collector.CollectSource()
+		);
+	}
+}
