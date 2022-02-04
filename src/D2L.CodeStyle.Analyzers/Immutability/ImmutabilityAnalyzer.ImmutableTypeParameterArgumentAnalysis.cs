4e8879be9ab8408deb754c3c29879c66d491895a
Owen Smith <owen@omsmith.ca>
2022-02-04 17:00:10 -0500

    maybe faster immutability who knows

---
+++ src/D2L.CodeStyle.Analyzers/Immutability/ImmutabilityAnalyzer.ImmutableTypeParameterArgumentAnalysis.cs
@@ -0,0 +1,483 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace D2L.CodeStyle.Analyzers.Immutability;
+public sealed partial class ImmutabilityAnalyzer {
+
+	private static class ImmutableTypeParameterArgumentAnalysis {
+
+		public static void Register(
+			CompilationStartAnalysisContext context,
+			AnnotationsContext annotationsContext,
+			ImmutabilityContext immutabilityContext
+		) {
+			// Type Argument on Methods
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IInvocationOperation)ctx.Operation;
+
+					SyntaxNodeOrToken getSyntax() => operation.Syntax switch {
+						InvocationExpressionSyntax invocation => invocation.Expression,
+						_ => operation.Syntax
+					};
+
+					AnalyzeTypeArgumentsRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.TargetMethod.TypeArguments,
+						operation.TargetMethod.TypeParameters,
+						SelectRightSyntaxRecursive( getSyntax )
+					);
+
+					if( operation.TargetMethod.IsStatic ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							operation.TargetMethod.ContainingType,
+							SelectLeftSyntax( getSyntax )
+						);
+					}
+				},
+				OperationKind.Invocation
+			);
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IMethodReferenceOperation)ctx.Operation;
+
+					AnalyzeTypeArgumentsRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.Method.TypeArguments,
+						operation.Method.TypeParameters,
+						() => SelectRightSyntaxRecursive( operation.Syntax )
+					);
+
+					if( operation.Method.IsStatic ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							operation.Method.ContainingType,
+							() => SelectLeftSyntax( operation.Syntax )
+						);
+					}
+				},
+				OperationKind.MethodReference
+			);
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IPropertyReferenceOperation)ctx.Operation;
+
+					if( operation.Property.IsStatic ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							operation.Property.ContainingType,
+							() => SelectLeftSyntax( operation.Syntax )
+						);
+					}
+				},
+				OperationKind.PropertyReference
+			);
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IFieldReferenceOperation)ctx.Operation;
+
+					if( operation.Field.IsStatic ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							operation.Field.ContainingType,
+							() => SelectLeftSyntax( operation.Syntax )
+						);
+					}
+				},
+				OperationKind.FieldReference
+			);
+
+			// Type Arguments when creating objects
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IObjectCreationOperation)ctx.Operation;
+
+					SyntaxNodeOrToken getSyntax() => operation.Syntax switch {
+						ObjectCreationExpressionSyntax objectCreation => objectCreation.Type,
+						ImplicitObjectCreationExpressionSyntax implicitObjectCreation => implicitObjectCreation.NewKeyword,
+						_ => operation.Syntax
+					};
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.Type!,
+						SelectRightSyntaxRecursive( getSyntax )
+					);
+				},
+				OperationKind.ObjectCreation
+			);
+
+			// Type arguments when defining variables
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IVariableDeclarationOperation)ctx.Operation;
+
+					SyntaxNodeOrToken getSyntax() => operation.Syntax switch {
+						VariableDeclarationSyntax variableDeclaration => variableDeclaration.Type,
+						_ => operation.Syntax
+					};
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.GetDeclaredVariables()[ 0 ].Type,
+						SelectRightSyntaxRecursive( getSyntax )
+					);
+				},
+				OperationKind.VariableDeclaration
+			);
+
+			// Type arguments of field types
+			context.RegisterSymbolAction(
+				ctx => {
+					var symbol = (IFieldSymbol)ctx.Symbol;
+
+					if( symbol.Type is not INamedTypeSymbol type ) {
+						return;
+					}
+
+					SyntaxNodeOrToken getSyntax() {
+						SyntaxNode syntaxNode = symbol.DeclaringSyntaxReferences[ 0 ].GetSyntax( ctx.CancellationToken );
+
+						return syntaxNode switch {
+							FieldDeclarationSyntax fieldDeclaration => fieldDeclaration.Declaration.Type,
+							VariableDeclaratorSyntax variableDeclarator => ( (VariableDeclarationSyntax)variableDeclarator.Parent! ).Type,
+							_ => throw new Exception( $"{syntaxNode.GetLocation().GetLineSpan().StartLinePosition.Line + 1} {syntaxNode.ToFullString()}" ),
+						};
+					}
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						type,
+						SelectRightSyntaxRecursive( getSyntax )
+					);
+				},
+				SymbolKind.Field
+			);
+
+			// Type arguments of property types
+			context.RegisterSymbolAction(
+				ctx => {
+					var symbol = (IPropertySymbol)ctx.Symbol;
+
+					if( symbol.Type is not INamedTypeSymbol type ) {
+						return;
+					}
+
+					SyntaxNodeOrToken getSyntax() {
+						SyntaxNode syntaxNode = symbol.DeclaringSyntaxReferences[ 0 ].GetSyntax( ctx.CancellationToken );
+
+						return syntaxNode switch {
+							PropertyDeclarationSyntax propertyDeclaration => propertyDeclaration.Type,
+							IndexerDeclarationSyntax indexer => indexer.Type,
+							ParameterSyntax parameter => ( parameter.Type ?? (SyntaxNode)parameter ),
+							_ => throw new Exception( $"{syntaxNode.GetLocation().GetLineSpan().StartLinePosition.Line + 1} {syntaxNode.ToFullString()}" ),
+						};
+					}
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						type,
+						SelectRightSyntaxRecursive( getSyntax )
+					);
+				},
+				SymbolKind.Property
+			);
+
+			// Type arguments on implemented types
+			context.RegisterSymbolAction(
+				ctx => {
+					var symbol = (INamedTypeSymbol)ctx.Symbol;
+
+					foreach( INamedTypeSymbol @interface in symbol.Interfaces ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							@interface,
+							() => FindBaseTypeSyntax( symbol, @interface, ctx.CancellationToken ).Type
+						);
+					}
+
+					if( symbol.BaseType != null ) {
+						AnalyzeTypeRecursive(
+							ctx.ReportDiagnostic,
+							annotationsContext,
+							immutabilityContext,
+							symbol.BaseType,
+							() => FindBaseTypeSyntax( symbol, symbol.BaseType, ctx.CancellationToken ).Type
+						);
+					}
+
+					BaseTypeSyntax FindBaseTypeSyntax(
+						INamedTypeSymbol typeSymbol,
+						INamedTypeSymbol baseTypeSymbol,
+						CancellationToken cancellationToken
+					) {
+						foreach( var reference in typeSymbol.DeclaringSyntaxReferences ) {
+							var syntax = (TypeDeclarationSyntax)reference.GetSyntax( cancellationToken );
+
+							var baseTypes = syntax.BaseList?.Types;
+							if( baseTypes == null ) {
+								continue;
+							}
+
+							SemanticModel model = ctx.Compilation.GetSemanticModel( syntax.SyntaxTree );
+							foreach( var baseTypeSyntax in baseTypes ) {
+								TypeSyntax typeSyntax = baseTypeSyntax.Type;
+
+								ITypeSymbol? thisTypeSymbol = model.GetTypeInfo( typeSyntax, cancellationToken ).Type;
+
+								if( baseTypeSymbol.Equals( thisTypeSymbol, SymbolEqualityComparer.Default ) ) {
+									return baseTypeSyntax;
+								}
+							}
+						}
+
+						throw new Exception();
+					}
+				},
+				SymbolKind.NamedType
+			);
+
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (ITypeOfOperation)ctx.Operation;
+
+					var syntax = ( (TypeOfExpressionSyntax)operation.Syntax ).Type;
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.TypeOperand,
+						() => syntax
+					);
+				},
+				OperationKind.TypeOf
+			);
+
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IIsTypeOperation)ctx.Operation;
+
+					var syntax = (BinaryExpressionSyntax)operation.Syntax;
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.TypeOperand,
+						() => syntax.Right
+					);
+				},
+				OperationKind.IsType
+			);
+
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (ITypePatternOperation)ctx.Operation;
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.NarrowedType,
+						() => ( (TypePatternSyntax)operation.Syntax ).Type
+					);
+				},
+				OperationKind.TypePattern
+			);
+
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IDeclarationPatternOperation)ctx.Operation;
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.NarrowedType,
+						() => ( (DeclarationPatternSyntax)operation.Syntax ).Type
+					);
+				},
+				OperationKind.DeclarationPattern
+			);
+
+			context.RegisterOperationAction(
+				ctx => {
+					var operation = (IConversionOperation)ctx.Operation;
+
+					if( operation.IsImplicit ) {
+						return;
+					}
+
+					SyntaxNodeOrToken getSyntax() => operation.Syntax switch {
+						CastExpressionSyntax castExpression => castExpression.Type,
+						BinaryExpressionSyntax binaryExpression => binaryExpression.Right,
+						_ => operation.Syntax
+					};
+
+					AnalyzeTypeRecursive(
+						ctx.ReportDiagnostic,
+						annotationsContext,
+						immutabilityContext,
+						operation.Type!,
+						getSyntax
+					);
+				},
+				OperationKind.Conversion
+			);
+		}
+
+		private static Func<SyntaxNodeOrToken> SelectRightSyntaxRecursive( Func<SyntaxNodeOrToken> getSyntax ) =>
+			() => SelectRightSyntaxRecursive( getSyntax() );
+
+		private static SyntaxNodeOrToken SelectRightSyntaxRecursive( SyntaxNodeOrToken syntax ) {
+			if( syntax.IsToken ) {
+				return syntax;
+			}
+
+			return syntax.AsNode() switch {
+				GenericNameSyntax genericName => genericName,
+				QualifiedNameSyntax qualifiedName => SelectRightSyntaxRecursive( qualifiedName.Right ),
+				MemberAccessExpressionSyntax memberAccess => SelectRightSyntaxRecursive( memberAccess.Name ),
+				_ => syntax,
+			};
+		}
+
+		private static Func<SyntaxNodeOrToken> SelectLeftSyntax( Func<SyntaxNodeOrToken> getSyntax ) =>
+			() => SelectLeftSyntax( getSyntax() );
+
+		private static SyntaxNodeOrToken SelectLeftSyntax( SyntaxNodeOrToken syntax ) {
+			if( syntax.IsToken ) {
+				return syntax;
+			}
+
+			return syntax.AsNode() switch {
+				QualifiedNameSyntax qualifiedName => qualifiedName.Left,
+				MemberAccessExpressionSyntax memberAccess => memberAccess.Expression,
+				_ => syntax,
+			};
+		}
+
+		private static SyntaxNodeOrToken GetTypeArgumentSyntax( SyntaxNodeOrToken syntax, int n ) {
+			if( syntax.IsToken ) {
+				return syntax;
+			}
+
+			return syntax.AsNode() switch {
+				GenericNameSyntax genericName => genericName.TypeArgumentList.Arguments[ n ],
+				_ => syntax,
+			};
+		}
+
+		private static void AnalyzeTypeRecursive(
+			Action<Diagnostic> reportDiagnostic,
+			AnnotationsContext annotationsContext,
+			ImmutabilityContext immutabilityContext,
+			ITypeSymbol type,
+			Func<SyntaxNodeOrToken> getAnalyzedSyntax
+		) {
+			INamedTypeSymbol? namedType = GetNamedTypeRecursive( type );
+			if( namedType is null ) {
+				return;
+			}
+
+			getAnalyzedSyntax = SelectRightSyntaxRecursive( getAnalyzedSyntax );
+
+			if( namedType.ContainingType != null ) {
+				AnalyzeTypeRecursive(
+					reportDiagnostic,
+					annotationsContext,
+					immutabilityContext,
+					namedType.ContainingType,
+					SelectLeftSyntax( getAnalyzedSyntax )
+				);
+			}
+
+			AnalyzeTypeArgumentsRecursive(
+				reportDiagnostic,
+				annotationsContext,
+				immutabilityContext,
+				namedType.TypeArguments,
+				namedType.TypeParameters,
+				getAnalyzedSyntax
+			);
+
+			static INamedTypeSymbol? GetNamedTypeRecursive( ITypeSymbol type ) => type switch {
+				INamedTypeSymbol namedType => namedType,
+				IArrayTypeSymbol arrayType => GetNamedTypeRecursive( arrayType.ElementType ),
+				_ => null
+			};
+		}
+
+		private static void AnalyzeTypeArgumentsRecursive(
+			Action<Diagnostic> reportDiagnostic,
+			AnnotationsContext annotationsContext,
+			ImmutabilityContext immutabilityContext,
+			ImmutableArray<ITypeSymbol> typeArguments,
+			ImmutableArray<ITypeParameterSymbol> typeParameters,
+			Func<SyntaxNodeOrToken> getAnalyzedSyntax
+		) {
+			for( var i = 0; i < typeArguments.Length; ++i ) {
+				ITypeSymbol argument = typeArguments[ i ];
+				ITypeParameterSymbol typeParameter = typeParameters[ i ];
+
+				SyntaxNodeOrToken getThisArgument() => GetTypeArgumentSyntax( getAnalyzedSyntax(), i );
+
+				AnalyzeTypeRecursive(
+					reportDiagnostic,
+					annotationsContext,
+					immutabilityContext,
+					argument,
+					getThisArgument
+				);
+
+				// TODO: this should eventually use information from ImmutableTypeInfo
+				// however the current information about immutable type parameters
+				// includes [Immutable] filling for what will instead be the upcoming
+				// [OnlyIf] (e.g. it would be broken for IEnumerable<>)
+				if( !annotationsContext.Objects.Immutable.IsDefined( typeParameter ) ) {
+					continue;
+				}
+
+				if( !immutabilityContext.IsImmutable(
+					new ImmutabilityQuery(
+						ImmutableTypeKind.Total,
+						argument
+					),
+					getLocation: () => getThisArgument().GetLocation(),
+					out Diagnostic diagnostic
+				) ) {
+					// TODO: not necessarily a good diagnostic for this use-case
+					reportDiagnostic( diagnostic );
+				}
+			}
+		}
+
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ImmutabilityAnalyzer_TypeArguments.cs
@@ -0,0 +1,172 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.Immutability.ImmutabilityAnalyzer
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using D2L.CodeStyle.Annotations;
+using static D2L.CodeStyle.Annotations.Objects;
+
+namespace Z;
+
+#pragma warning disable D2L0066
+
+public class TestImmutableT<[Immutable] T> { }
+public class TestImmutableU<T, [Immutable] U> { }
+
+[Immutable] public class MyImmutable { }
+public class MyMutable { }
+
+[Immutable] public class MyImmutable<[Immutable] T> { }
+
+public static class Holder {
+	public static MyImmutable MyImmutable { get; }
+	public static MyImmutable<MyImmutable> MyImmutable_MyImmutable { get; }
+	public static MyImmutable<MyMutable> MyImmutable_MyMutable { get; }
+
+	public static MyMutable MyMutable { get; }
+
+	public static Receiver<MyMutable> Receiver_MyMutable { get; }
+}
+
+public class Receiver {
+	public Receiver Invoke<[Immutable] T>( T _ ) { }
+	public static Receiver StaticInvoke<[Immutable] T>( T _ ) { }
+}
+
+public class Receiver<[Immutable] T> {
+
+	public static readonly Receiver<T> Instance;
+
+	public Receiver<T> Invoke( T _ ) { }
+	public static Receiver<T> StaticInvoke( T _ ) { }
+}
+
+#pragma warning restore D2L0066
+
+public class Tester<[Immutable] T, U> {
+
+	// SymbolKind.Field
+	TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> mutableField;
+
+	// SymbolKind.Property
+	TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> MutableProp { get; }
+
+	void Statements<[Immutable] R, S>( U _ ) {
+		// Variable Declaration
+		TestImmutableT<MyImmutable> test_myImmutable;
+		TestImmutableT<T> test_immutableClassTP;
+		TestImmutableT<R> test_immutableMethodTP;
+		TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> test_myMutable;
+		TestImmutableT</* TypeParameterIsNotKnownToBeImmutable(U) */ U /**/> test_mutableClassTP;
+		TestImmutableT</* TypeParameterIsNotKnownToBeImmutable(S) */ S /**/> test_mutableMethodTP;
+
+		// Variable Declaration, nested immutable
+		TestImmutableT<MyImmutable<MyImmutable>> test_myImmutable_myImmutable;
+		TestImmutableT<MyImmutable</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>> test_myImmutable_myMutable;
+
+		// OperationKind.ObjectCreation
+		test_myImmutable = new();
+		test_immutableClassTP = new();
+		test_immutableMethodTP = new();
+		test_myMutable = /* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ new /**/ ();
+		test_mutableClassTP = /* TypeParameterIsNotKnownToBeImmutable(U) */ new /**/ ();
+		test_mutableMethodTP = /* TypeParameterIsNotKnownToBeImmutable(S) */ new /**/ ();
+		test_myImmutable_myImmutable = new();
+		test_myImmutable_myMutable = /* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ new /**/ ();
+		new TestImmutableT<MyImmutable>();
+		new TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>();
+		new TestImmutableT<MyImmutable<MyImmutable>>();
+		new TestImmutableT<MyImmutable</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>>();
+
+		// OperationKind.Invocation
+		Receiver
+			.StaticInvoke</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>( Holder.MyMutable )
+			.Invoke</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>( Holder.MyMutable );
+
+		Receiver
+			./* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ StaticInvoke /**/ ( Holder.MyMutable )
+			./* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ Invoke /**/ ( Holder.MyMutable );
+
+		Receiver</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>
+			.StaticInvoke( Holder.MyMutable );
+
+		Holder.Receiver_MyMutable.Invoke( Holder.MyMutable );
+
+
+		// OperationKind.MethodReference
+		var _ = Receiver.StaticInvoke</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>;
+
+
+		// OperationKind.VariableDeclaration && OperationKind.MethodReference
+		/* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ var /**/ _ =
+			Receiver</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>.StaticInvoke;
+
+
+		// OperationKind.FieldReference
+		Receiver</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>.Instance;
+
+		(TestImmutableT<MyImmutable>)null;
+		(TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>)null;
+
+		null as TestImmutableT<MyImmutable>;
+		null as TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>;
+
+		// OperationKind.TypeOf
+		Type _ = typeof( TestImmutableT<MyImmutable> );
+		Type _ = typeof( TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> );
+
+
+		// OperationKind.IsType
+		if( new object() is TestImmutableT<MyImmutable> ) { }
+		if( new object() is TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> ) { }
+
+
+		// OperationKind.TypePattern
+		if( new object() is not TestImmutableT<MyImmutable> ) { }
+		if( new object() is not TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> ) { }
+
+
+		// OperationKind.DeclarationPattern
+		if( new object() is TestImmutableT<MyImmutable> _ ) { }
+		if( new object() is TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> _ ) { }
+		if( new object() is not TestImmutableT<MyImmutable> _ ) { }
+		if( new object() is not TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> _ ) { }
+
+
+		switch( new object() ) {
+
+			// OperationKind.TypePattern
+			case TestImmutableT<MyImmutable>: break;
+			case TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/>: break;
+
+
+			// OperationKind.DeclarationPattern
+			case TestImmutableT<MyImmutable> _: break;
+			case TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> _: break;
+
+			default: break;
+		}
+
+		var _ = new object() switch {
+
+			// OperationKind.TypePattern
+			TestImmutableT<MyImmutable> => null,
+			TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> => null,
+
+
+			// OperationKind.DeclarationPattern
+			TestImmutableT<MyImmutable> _ => null,
+			TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> _ => null,
+
+			_ => null
+		};
+	}
+
+	// SymbolKind.NamedType
+	public class ImplementerA : TestImmutableT<MyImmutable> { }
+	public class ImplementerB : TestImmutableT<T> { }
+	public class ImplementerC : TestImmutableT</* NonImmutableTypeHeldByImmutable(class, Z.MyMutable, ) */ MyMutable /**/> { }
+	public class ImplementerD : TestImmutableT</* TypeParameterIsNotKnownToBeImmutable(U) */ U /**/> { }
+}
