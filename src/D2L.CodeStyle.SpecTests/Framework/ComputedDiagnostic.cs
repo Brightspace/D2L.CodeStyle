08f41935fb26227d52238bbc3a79e146c1f3eefc
Jeff Ashton <Jeff.Ashton@D2L.com>
2022-02-06 15:47:30 -0500

    Formatting ComputedDiagnostic output

--- src/D2L.CodeStyle.SpecTests/Framework/ComputedDiagnostic.cs
+++ src/D2L.CodeStyle.SpecTests/Framework/ComputedDiagnostic.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.Text;
+﻿using System.CodeDom.Compiler;
+using Microsoft.CodeAnalysis.Text;
 
 namespace D2L.CodeStyle.SpecTests.Framework {
 
@@ -7,5 +8,41 @@ namespace D2L.CodeStyle.SpecTests.Framework {
 		string Id,
 		LinePositionSpan LinePosition,
 		string Message
-	);
+	) {
+
+		/// <remarks>
+		/// Formatting output for Visual Studio Test Explorer
+		/// </remarks>
+		public override string ToString() {
+
+			using StringWriter sw = new StringWriter();
+			using( IndentedTextWriter writer = new IndentedTextWriter( sw, tabString: "  " ) ) {
+				writer.Indent = 3;
+
+				writer.WriteLine( "{" );
+				writer.Indent++;
+
+				writer.Write( "Alias = " );
+				writer.Write( Alias ?? "<<unknown>>" );
+				writer.WriteLine( "," );
+
+				writer.Write( "Id = " );
+				writer.Write( Id );
+				writer.WriteLine( "," );
+
+				writer.Write( "LinePosition = " );
+				writer.Write( LinePosition );
+				writer.WriteLine( "," );
+
+				writer.Write( "Message = " );
+				writer.Write( Message );
+				writer.WriteLine( "," );
+
+				writer.Indent--;
+				writer.WriteLine( "}" );
+			}
+
+			return sw.ToString();
+		}
+	}
 }
