6c03ec264b9d3890b21d22aeca19c3cb5ed07ca1
Jacob Parker <jacob@solidangle.ca>
2022-02-07 15:14:42 -0500

    Add AsyncToSyncMethodTransformer (#838)

---
+++ src/D2L.CodeStyle.Annotations/AsyncSupport/GenerateSyncAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace D2L.CodeStyle.Annotations {
+	/// <summary>
+	/// Indicates that a blocking/synchronous version of the method should be
+	/// generated.
+	/// </summary>
+	[AttributeUsage(
+		AttributeTargets.Method,
+		AllowMultiple = false,
+		Inherited = false
+	)]
+	public sealed class GenerateSyncAttribute : Attribute {}
+}
