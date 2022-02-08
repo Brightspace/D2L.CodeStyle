cfb843386b4616d71e7217e54b726aa07b833e3c
Jeff Ashton <JeffAshton@users.noreply.github.com>
2022-02-08 16:23:05 -0500

    Pulling out re-usable pieces from SpecTestsProvider (#855)

---
+++ src/D2L.CodeStyle.SpecTests/AdditionalFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace D2L.CodeStyle.SpecTests {
+
+	public sealed class AdditionalFile : AdditionalText {
+
+		private readonly string m_text;
+
+		public AdditionalFile(
+			string path,
+			string text
+		) {
+			Path = path;
+			m_text = text;
+		}
+
+		public override string Path { get; }
+		public override SourceText GetText( CancellationToken cancellationToken = default ) => SourceText.From( m_text, Encoding.UTF8 );
+	}
+}
