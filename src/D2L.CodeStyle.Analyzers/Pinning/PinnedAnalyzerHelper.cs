702e3aff5221e7b601e6397b0caaa04139dda8f6
Al Lamarre <alamarre@gmail.com>
2023-09-27 13:07:17 -0400

    add pinned analyzer

---
+++ src/D2L.CodeStyle.Analyzers/Pinning/PinnedAnalyzerHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace D2L.CodeStyle.Analyzers.Pinning {
+	public static class PinnedAnalyzerHelper {
+
+		public const string PinnedAttributeName = "D2L.CodeStyle.Annotations.Pinning.PinnedAttribute";
+
+		public static bool TryGetPinnedAttribute( ISymbol classSymbol, INamedTypeSymbol pinnedAttributeSymbol, out AttributeData? attribute ) {
+			attribute = null;
+
+			foreach( AttributeData? attributeData in classSymbol.GetAttributes() ) {
+				INamedTypeSymbol? attributeSymbol = attributeData.AttributeClass;
+				if( pinnedAttributeSymbol.Equals( attributeSymbol, SymbolEqualityComparer.Default ) ) {
+					attribute = attributeData;
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
