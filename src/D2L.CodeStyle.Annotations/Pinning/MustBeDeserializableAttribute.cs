ee04d8aa34778c8787a7628bda3eebd840097808
Al Lamarre <Al.Lamarre@D2L.com>
2023-10-11 11:14:22 -0400

    replace must be recursively pinned with must be deserializable based on recent discussion

--- src/D2L.CodeStyle.Annotations/Pinning/MustBeDeserializableAttribute.cs
+++ src/D2L.CodeStyle.Annotations/Pinning/MustBeDeserializableAttribute.cs
@@ -2,9 +2,9 @@
 
 namespace D2L.CodeStyle.Annotations.Pinning {
 	/// <summary>
-	/// Represents a parameter in a dangerous call chain (e.g. serialization using the assembly qualified name) that must be pinned recursively
+	/// Represents a parameter in the call chain of serialization in order to enforce pinning or otherwise set limitations on serialized types
 	/// </summary>
 	[AttributeUsage( AttributeTargets.Parameter | AttributeTargets.GenericParameter, AllowMultiple = false, Inherited = false )]
-	public sealed class MustBeRecursivelyPinnedAttribute : Attribute {
+	public sealed class MustBeDeserializableAttribute : Attribute {
 	}
 }
