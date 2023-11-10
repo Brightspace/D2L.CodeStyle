8cccb49855d69d8103dd12926bc9049398e54160
TJ Cowen <TJ.Cowen@D2L.com>
2023-11-10 11:21:11 -0500

    Added PoC tests for adding OnlyVisibleToType to interfaces.

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/OnlyVisibleToAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/OnlyVisibleToAnalyzer.cs
@@ -9,13 +9,22 @@ using Targets;
 namespace D2L.CodeStyle.Annotations.Contract {
 
 	[AttributeUsage(
-		validOn: AttributeTargets.Method | AttributeTargets.Property,
+		validOn: AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Property,
 		AllowMultiple = true,
 		Inherited = false
 	)]
 	public sealed class OnlyVisibleToTypeAttribute : Attribute {
 		public OnlyVisibleToTypeAttribute( Type type ) { }
 		public OnlyVisibleToTypeAttribute( string fullyQualifiedTypeName, string assemblyName ) { }
+	}
+
+	[AttributeUsage(
+		validOn: AttributeTargets.Class,
+		AllowMultiple = false,
+		Inherited = false
+	)]
+	public sealed class ReleaseVisibilityRestrictions : Attribute {
+		public ReleaseVisibilityRestrictions() { }
 	}
 }
 
@@ -358,6 +367,97 @@ namespace TestCases {
 		public static void Test() {
 			/* MemberNotVisibleToCaller(VisibleByTypeOf) */ GenericTargetTypes<int>.VisibleByTypeOf() /**/;
 			/* MemberNotVisibleToCaller(VisibleByQualifiedTypeName) */ GenericTargetTypes<int>.VisibleByQualifiedTypeName() /**/;
+		}
+	}
+}
+
+// ===============================================================================
+
+namespace Targets {
+	[OnlyVisibleToType( typeof( RestrictedWithInheritance ) )]
+	[OnlyVisibleToType( typeof( RestrictedWithoutInheritance ) )]
+	[OnlyVisibleToType( typeof( TestCases.AllowedInheritanceCaller ) )]
+	public interface IInheritanceInterface {
+		string SomeProperty;
+		string SomeMethod();
+	}
+
+	public class RestrictedWithInheritance : IInheritanceInterface {
+		public RestrictedWithInheritance() { }
+		string SomeProperty => "SomeProperty";
+		string SomeMethod() {
+			return "SomeMethod";
+		}
+	}
+
+	[ReleaseVisibilityRestrictions]
+	public class RestrictedWithoutInheritance : IInheritanceInterface {
+		public RestrictedWithoutInheritance() { }
+		string SomeProperty => "SomeProperty";
+		string SomeMethod() {
+			return "SomeMethod";
+		}
+	}
+}
+
+namespace TestCases {
+	public static class AllowedInheritanceCaller {
+		public static void DirectInstantiation() {
+			RestrictedWithInheritance extendedObject1 = new RestrictedWithInheritance();
+			RestrictedWithoutInheritance extendedObject2 = new RestrictedWithoutInheritance();
+			IInheritanceInterface interfaceObject1 = new RestrictedWithInheritance();
+			IInheritanceInterface interfaceObject2 = new RestrictedWithoutInheritance();
+
+			string extendedProp1 = extendedObject1.SomeProperty;
+			string extendedMethod1 = extendedObject1.SomeMethod();
+			string extendedProp2 = extendedObject2.SomeProperty;
+			string extendedMethod2 = extendedObject2.SomeMethod();
+			string interfaceProp1 = interfaceObject1.SomeProperty;
+			string interfaceMethod1 = interfaceObject1.SomeMethod();
+			string interfaceProp2 = interfaceObject2.SomeProperty;
+			string interfaceMethod2 = interfaceObject2.SomeMethod();
+		}
+
+		public static void GenericInterface<T>() where T : IInheritanceInterface, new() {
+			T genericObject = new T();
+		}
+
+		public static void GenericExtendedWithInheritance<T>() where T : RestrictedWithInheritance, new() {
+			T genericObject = new T();
+		}
+
+		public static void GenericExtendedWithoutInheritance<T>() where T : RestrictedWithoutInheritance, new() {
+			T genericObject = new T();
+		}
+	}
+
+	public static class DisallowedInheritanceCaller {
+		public static void DirectInstantiation() {
+			/* TypeNotVisibleToCaller(RestrictedWithInheritance) */ RestrictedWithInheritance /**/ extendedObject1 = new /* TypeNotVisibleToCaller(RestrictedWithInheritance) */ RestrictedWithInheritance() /**/;
+			RestrictedWithoutInheritance extendedObject2 = new RestrictedWithoutInheritance();
+			/* TypeNotVisibleToCaller(IInheritanceInterface) */ IInheritanceInterface /**/ interfaceObject1 = new /* TypeNotVisibleToCaller(RestrictedWithInheritance) */ RestrictedWithInheritance() /**/;
+			/* TypeNotVisibleToCaller(IInheritanceInterface) */ IInheritanceInterface /**/ interfaceObject2 = new RestrictedWithoutInheritance();
+
+			string extendedProp1 = extendedObject1.SomeProperty;
+			string extendedMethod1 = extendedObject1.SomeMethod();
+			string extendedProp2 = extendedObject2.SomeProperty;
+			string extendedMethod2 = extendedObject2.SomeMethod();
+			string interfaceProp1 = interfaceObject1.SomeProperty;
+			string interfaceMethod1 = interfaceObject1.SomeMethod();
+			string interfaceProp2 = interfaceObject2.SomeProperty;
+			string interfaceMethod2 = interfaceObject2.SomeMethod();
+		}
+
+		public static void GenericInterface<T>() where T : /* TypeNotVisibleToCaller(IInheritanceInterface) */ IInheritanceInterface /**/, new() {
+			T genericObject = new T();
+		}
+
+		public static void GenericExtendedWithInheritance<T>() where T : /* TypeNotVisibleToCaller(RestrictedWithInheritance) */ RestrictedWithInheritance /**/, new() {
+			T genericObject = new T();
+		}
+
+		public static void GenericExtendedWithoutInheritance<T>() where T : RestrictedWithoutInheritance, new() {
+			T genericObject = new T();
 		}
 	}
 }
