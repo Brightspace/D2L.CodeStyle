1ea68a64dce9a9ed0d6bfe65d4b62e3f47bc6c81
Jacob Parker <jacob@solidangle.ca>
2024-08-08 15:57:59 -0400

    .

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/ImmutabilityAnalyzer.defect956.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ImmutabilityAnalyzer.defect956.cs
@@ -4,28 +4,36 @@
 
 using static D2L.CodeStyle.Annotations.Objects;
 
-namespace Defect956;
+#if NETFRAMEWORK
+namespace System.Runtime.CompilerServices {
+  // Polyfill for .NET framework
+  internal static class IsExternalInit {}
+}
+#endif
 
-[ConditionallyImmutable]
-interface IFoo<[ConditionallyImmutable.OnlyIf]A> {}
+namespace Defect956 {
+  [ConditionallyImmutable]
+  interface IFoo<[ConditionallyImmutable.OnlyIf]A> {}
 
-[ConditionallyImmutable]
-record Foo<[ConditionallyImmutable.OnlyIf]A, [ConditionallyImmutable.OnlyIf]B>(
-  A A
-  B B
-) : IFoo<A>;
+  [ConditionallyImmutable]
+  record Foo<[ConditionallyImmutable.OnlyIf]A, [ConditionallyImmutable.OnlyIf]B>(
+    A A
+    B B
+  ) : IFoo<A>;
 
-static class Demo {
-  public static readonly Foo<int, int> ThisIsFine = new( 1, 2 );
+  static class Demo {
+    public static readonly Foo<int, int> ThisIsFine = new( 1, 2 );
 
-  public static readonly
-    /* NonImmutableTypeHeldByImmutable(class, Foo<int<Dictionary<int, int>>, ) */ Foo<int, Dictionary<int, int>> /**/
-    ObviouslyBad = new( 3, new() );
+    public static readonly
+      /* NonImmutableTypeHeldByImmutable(class, Foo<int<Dictionary<int, int>>, ) */ Foo<int, Dictionary<int, int>> /**/
+      ObviouslyBad = new( 3, new() );
 
-  // That this is allowed is a problem: we can cast back to Foo<int, Dictionary<int, int>>
-  // and poke at the dictionary, or maybe the dictionary got smuggled in from elsehwere
-  // where its still accessible etc.
-  // The point is that IFoo<A> looks immutable if A does, and Foo<A, B> was able to implement
-  // it for any B
-  public static readonly IFoo<int> Defect = (IFoo<int>)(new Foo<int, Dictionary<int, int>>( 4, new() ));
+    // That this is allowed is a problem: we can cast back to Foo<int, Dictionary<int, int>>
+    // and poke at the dictionary, or maybe the dictionary got smuggled in from elsehwere
+    // where its still accessible etc.
+    // The point is that IFoo<A> looks immutable if A does, and Foo<A, B> was able to implement
+    // it for any B
+    public static readonly IFoo<int> Defect = (IFoo<int>)(new Foo<int, Dictionary<int, int>>( 4, new() ));
+  }
+
 }
