39c2c7118c49a781477a0c4b122c09598029ceee
Muaaz Mahmud <mmahmud@desire2learn.com>
2026-04-02 13:50:11 -0400

    SPLA-3311: Allow string.Empty for ConstantAttributeAnalyzer

--- src/D2L.CodeStyle.Analyzers/ApiUsage/ConstantAttributeAnalyzer.cs
+++ src/D2L.CodeStyle.Analyzers/ApiUsage/ConstantAttributeAnalyzer.cs
@@ -118,8 +118,8 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage {
 				return;
 			}
 
-			// Argument is a constant value, so do nothing
-			if( argument.Value.ConstantValue.HasValue ) {
+			// Argument is a constant value or string.Empty, so do nothing
+			if( argument.Value.ConstantValue.HasValue || IsStringEmpty( argument.Value ) ) {
 				return;
 			}
 
@@ -157,9 +157,9 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage {
 				return;
 			}
 
-			// Operand is a constant value, so trust it
+			// Operand is a constant value or string.Empty, so trust it
 			IOperation operand = conversion.Operand;
-			if( operand.ConstantValue.HasValue ) {
+			if( operand.ConstantValue.HasValue || IsStringEmpty( operand ) ) {
 				return;
 			}
 
@@ -214,6 +214,14 @@ namespace D2L.CodeStyle.Analyzers.ApiUsage {
 						attr.AttributeClass
 					)
 				);
+		}
+
+		private static bool IsStringEmpty( IOperation operation ) {
+			if( operation is IFieldReferenceOperation fieldRef ) {
+				return fieldRef.Field.ContainingType.SpecialType == SpecialType.System_String
+					&& fieldRef.Field.Name == "Empty";
+			}
+			return false;
 		}
 
 		private static bool TypeCanBeConstant( SpecialType specialType ) {
