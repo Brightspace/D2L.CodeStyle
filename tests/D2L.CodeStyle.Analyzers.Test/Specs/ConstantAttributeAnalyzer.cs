83026994c09e5ac12a4b160db84ede679016ad6f
Todd Lang <todd.lang@d2l.com>
2026-07-16 16:41:34 -0400

    SPLA-4322 - Sharing the constant analyzer between regular and test analyzers (#981)
    * Sharing the constant analyzer between regular and test analyzers
    -Adding logic to support constant usages in test-only contexts (rhino mocks, for example)

--- tests/D2L.CodeStyle.Analyzers.Test/Specs/ConstantAttributeAnalyzer.cs
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/ConstantAttributeAnalyzer.cs
@@ -1,4 +1,4 @@
-﻿// analyzer: D2L.CodeStyle.Analyzers.ApiUsage.ConstantAttributeAnalyzer, D2L.CodeStyle.Analyzers
+﻿// analyzer: D2L.CodeStyle.Analyzers.ApiUsage.ConstantAttributeAnalyzer, D2L.CodeStyle.Analyzers.Rule.Constant
 
 using System;
 
